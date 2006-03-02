0eedfd8b8e325e02a64827851c71f12e577d9f51
Dag Erling Smørgrav <des@des.no>
2006-03-02 10:31:17 +0000

    Untested & undocumented login code.  I don't have time to continue working on it right now, but I want it in the tree so phk doesn't start duplicating my effort.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@41 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/varnishapi.h
+++ include/varnishapi.h
@@ -5,6 +5,23 @@
 #ifndef VARNISHAPI_H_INCLUDED
 #define VARNISHAPI_H_INCLUDED
 
-/* ... */
+#define V_DEAD __attribute__ ((noreturn))
+
+/* varnish_debug.c */
+void		 vdb_panic(const char *, ...) V_DEAD;
+
+/* varnish_log.c */
+typedef struct vlo_buffer vlo_buffer_t;
+vlo_buffer_t	*vlo_open(const char *, size_t, int);
+ssize_t		 vlo_write(vlo_buffer_t *, const void *, size_t);
+vlo_buffer_t	*vlo_attach(const char *);
+ssize_t		 vlo_read(vlo_buffer_t *, const void *, size_t);
+#if 0
+uuid_t		 vlo_get_uuid(vlo_buffer_t *);
+#endif
+int		 vlo_close(vlo_buffer_t *);
+
+/* varnish_util.c */
+int		 vut_open_lock(const char *, int, int, int);
 
 #endif
