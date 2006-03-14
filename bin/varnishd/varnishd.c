daf316853fbbefff03f245754986d9b209eaf3de
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-03-14 09:31:15 +0000

    Add some trivial bits while I think about the hard ones
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@48 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/varnishd.c
+++ bin/varnishd/varnishd.c
@@ -10,7 +10,19 @@
 static void
 usage(void)
 {
-	fprintf(stderr, "usage: varnishd\n");
+	fprintf(stderr, "usage: varnishd [options]\n");
+	fprintf(stderr, "    %-20s # %s\n", "-d", "debug");
+	fprintf(stderr, "    %-20s # %s\n", "-p number", "TCP listen port");
+#if 0
+	-c clusterid@cluster_controller
+	-f config_file
+	-m memory_limit
+	-s kind[,storage-options]
+	-l logfile,logsize
+	-b backend ip...
+	-u uid
+	-a CLI_port
+#endif
 	exit(1);
 }
 
@@ -18,9 +30,17 @@ int
 main(int argc, char *argv[])
 {
 	int o;
+	unsigned portnumber = 8080;
+	unsigned dflag = 1;	/* XXX: debug=on for now */
 
-	while ((o = getopt(argc, argv, "")) != -1)
+	while ((o = getopt(argc, argv, "dp:")) != -1)
 		switch (o) {
+		case 'd':
+			dflag++;
+			break;
+		case 'p':
+			portnumber = strtoul(optarg, NULL, 0);
+			break;
 		default:
 			usage();
 		}
