a9dbba290d37f8e81e18555acd41008dc28110be
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-04-18 07:34:24 +0000

    Add trivial malloc backed storage backend.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@140 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ bin/varnishd/storage_malloc.c
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ *
+ * Storage method based on malloc(3)
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+#include <pthread.h>
+
+#include "vcl_lang.h"
+#include "cache.h"
+
+struct sma {
+	struct storage		s;
+};
+
+static void
+sma_init(void)
+{
+}
+
+static struct storage *
+sma_alloc(unsigned size)
+{
+	struct sma *sma;
+
+	sma = calloc(sizeof *sma, 1);
+	assert(sma != NULL);
+	sma->s.priv = sma;
+	sma->s.ptr = malloc(size);
+	assert(sma->s.ptr != NULL);
+	sma->s.len = size;
+	return (&sma->s);
+}
+
+static void
+sma_free(struct storage *s)
+{
+	struct sma *sma;
+
+	sma = s->priv;
+	free(sma->s.ptr);
+	free(sma);
+}
+
+struct stevedore sma_stevedore = {
+	"Malloc",
+	sma_init,
+	sma_alloc,
+	sma_free
+};
