df2c3d3ebb278f5eff69ec6ba665fb6c80addc61
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-06-20 09:28:00 +0000

    Rename the VCL compilers public functions to VCC prefix
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@209 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/libvcl.h
+++ include/libvcl.h
@@ -2,8 +2,8 @@
  * $Id$
  */
 
-char *VCL_Compile(struct sbuf *sb, const char *b, const char *e);
-char *VCL_CompileFile(struct sbuf *sb, const char *fn);
-void VCL_InitCompile(void);
+char *VCC_Compile(struct sbuf *sb, const char *b, const char *e);
+char *VCC_CompileFile(struct sbuf *sb, const char *fn);
+void VCC_InitCompile(void);
 
 
