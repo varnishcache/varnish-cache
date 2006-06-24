5dc7f0bc906881f2bc0270ac4dc895067397e36e
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-06-24 21:54:54 +0000

    Use ttl=0 as a "invalid TTL" flag.
    Mark objects with ttl=0 uncachable.
    
    Add cacheable objects to the expiry heap
    
    Start an expiry thread which polls the root element once per second
    
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk@231 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_expire.c
+++ bin/varnishd/cache_expire.c
@@ -4,7 +4,73 @@
  * Expiry of cached objects and execution of prefetcher
  */
 
+#include <pthread.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "libvarnish.h"
+#include "binary_heap.h"
+#include "cache.h"
+
+static pthread_t exp_thread;
+static struct binheap *exp_heap;
+static pthread_mutex_t expmtx;
+
+/*--------------------------------------------------------------------*/
+
+void
+EXP_Insert(struct object *o)
+{
+
+	AZ(pthread_mutex_lock(&expmtx));
+	binheap_insert(exp_heap, o);
+	AZ(pthread_mutex_unlock(&expmtx));
+}
+
+/*--------------------------------------------------------------------*/
+
+static void *
+exp_main(void *arg)
+{
+	struct object *o;
+	time_t t;
+
+	while (1) {
+		time(&t);
+		AZ(pthread_mutex_lock(&expmtx));
+		o = binheap_root(exp_heap);
+		AZ(pthread_mutex_unlock(&expmtx));
+		if (o != NULL) {
+			printf("Root: %p %d (%d)\n",
+			    (void*)o, o->ttl, o->ttl - t);
+		}
+		sleep(1);
+	}
+
+	return ("FOOBAR");
+}
+
+/*--------------------------------------------------------------------*/
+
+static int
+object_cmp(void *priv, void *a, void *b)
+{
+	struct object *aa, *bb;
+
+	aa = a;
+	bb = b;
+	return (aa->ttl < bb->ttl);
+}
+
+/*--------------------------------------------------------------------*/
+
 void
 EXP_Init(void)
 {
+
+	AZ(pthread_create(&exp_thread, NULL, exp_main, NULL));
+	AZ(pthread_mutex_init(&expmtx, NULL));
+	exp_heap = binheap_new(NULL, object_cmp, NULL);
+	assert(exp_heap != NULL);
 }
