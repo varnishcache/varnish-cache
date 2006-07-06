3b9b632f386d76b8d444c46d0c9a88260995445b
Dag Erling Smørgrav <des@des.no>
2006-07-06 08:43:53 +0000

    Portability: don't use non-portable mmap(2) flags.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@339 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/storage_file.c
+++ bin/varnishd/storage_file.c
@@ -25,6 +25,14 @@
 #include "shmlog.h"
 #include "cache.h"
 
+#ifndef MAP_NOCORE
+#define MAP_NOCORE 0 /* XXX Linux */
+#endif
+
+#ifndef MAP_NOSYNC
+#define MAP_NOSYNC 0 /* XXX Linux */
+#endif
+
 #define MINPAGES		128
 
 /*--------------------------------------------------------------------*/
