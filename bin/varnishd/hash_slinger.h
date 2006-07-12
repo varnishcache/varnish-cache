ec4eb07cb48cf6aee8646785f77e11847d601087
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-07-12 12:04:28 +0000

    Hash on both URL and Host header.  If no host header, hash on URL twice.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@454 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/hash_slinger.h
+++ bin/varnishd/hash_slinger.h
@@ -4,7 +4,7 @@
 
 typedef int hash_init_f(const char *);
 typedef void hash_start_f(void);
-typedef struct objhead *hash_lookup_f(const char *key, struct objhead *nobj);
+typedef struct objhead *hash_lookup_f(const char *key1, const char *key2, struct objhead *nobj);
 typedef int hash_deref_f(struct objhead *obj);
 
 struct hash_slinger {
