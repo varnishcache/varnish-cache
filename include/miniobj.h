0c60cc3b1eed7cda446e3c193049a1728e47b39e
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-07-19 12:36:42 +0000

    Add miniobj.h for debugging
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@499 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ include/miniobj.h
@@ -0,0 +1,33 @@
+/* $Id$ */
+
+#define FREE_OBJ(to)							\
+	do {								\
+		(to)->magic = (0);					\
+		free(to);						\
+	} while (0)
+
+#define CHECK_OBJ(ptr, type_magic)					\
+	do {								\
+		assert((ptr)->magic == type_magic);			\
+	} while (0)
+
+#define CHECK_OBJ_NOTNULL(ptr, type_magic)				\
+	do {								\
+		assert((ptr) != NULL);					\
+		assert((ptr)->magic == type_magic);			\
+	} while (0)
+
+#define CAST_OBJ(to, from, type_magic)					\
+	do {								\
+		(to) = (from);						\
+		if ((to) != NULL)					\
+			CHECK_OBJ((to), (type_magic));			\
+	} while (0);
+
+#define CAST_OBJ_NOTNULL(to, from, type_magic)					\
+	do {								\
+		(to) = (from);						\
+		assert((to) != NULL);					\
+		CHECK_OBJ((to), (type_magic));				\
+	} while (0);
+
