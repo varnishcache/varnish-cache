60d01510f1ac017b558d5e849959f4df6f17fbb2
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-07-21 21:01:45 +0000

    Update VRT to minimal functional level again
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@549 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/vrt.h
+++ include/vrt.h
@@ -24,8 +24,6 @@ struct vrt_acl {
 };
 
 void VRT_count(struct sess *, unsigned);
-void VRT_no_cache(struct sess *);
-void VRT_no_new_cache(struct sess *);
 #if 0
 int ip_match(unsigned, struct vcl_acl *);
 int string_match(const char *, const char *);
@@ -35,14 +33,9 @@ void VRT_error(struct sess *, unsigned, const char *);
 int VRT_switch_config(const char *);
 
 char *VRT_GetHdr(struct sess *, const char *);
-char *VRT_GetReq(struct sess *);
 void VRT_handling(struct sess *sp, unsigned hand);
-int VRT_obj_valid(struct sess *);
-int VRT_obj_cacheable(struct sess *);
 
 void VRT_set_backend_name(struct backend *, const char *);
-void VRT_set_backend_hostname(struct backend *, const char *);
-void VRT_set_backend_portname(struct backend *, const char *);
 
 void VRT_alloc_backends(struct VCL_conf *cp);
 
