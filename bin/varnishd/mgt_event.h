b208b9790bdaa5880c0bd7ac11e15a129c3ed447
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-08-05 08:19:41 +0000

    Add a miniature event engine based on poll(2).
    It's general enough to find other uses, but right now it's only for
    the manager process.
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk@645 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ bin/varnishd/mgt_event.h
@@ -0,0 +1,41 @@
+
+#include "queue.h"
+
+struct ev;
+struct evbase;
+
+typedef int ev_cb_f(struct ev *, unsigned what);
+
+struct ev {
+	unsigned	magic;
+#define EV_MAGIC	0x15c8134b
+
+	/* pub */
+	int		fd;
+	unsigned	flags;
+#define		EV_RD	POLLIN
+#define		EV_WR	POLLOUT
+#define		EV_ERR	POLLERR
+#define		EV_HUP	POLLHUP
+	double		timeout;
+	ev_cb_f		*callback;
+	void		*priv;
+
+	/* priv */
+	double		__when;
+	TAILQ_ENTRY(ev)	__list;
+	unsigned	__binheap_idx;
+	unsigned	__privflags;
+	struct evbase	*__evb;
+	int		__poll_idx;
+};
+
+struct evbase;
+
+struct evbase *ev_new_base(void);
+void ev_destroy_base(struct evbase *evb);
+int ev_add(struct evbase *evb, struct ev *e);
+void ev_del(struct evbase *evb, struct ev *e);
+
+int ev_schedule_one(struct evbase *evb);
+int ev_schedule(struct evbase *evb);
