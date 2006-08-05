ddd0e22d3769dd0e324af36e9125ae5479ed4aa6
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-08-05 12:19:33 +0000

    Cleanup unused stuff
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@660 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/common_cli.h
+++ bin/varnishd/common_cli.h
@@ -4,14 +4,9 @@
 
 struct cli {
 	struct sbuf		*sb;
-	unsigned		verbose;
-	unsigned		suspend;
 	enum cli_status_e	result;
-	struct cli_proto	*cli_proto;
 };
 
-void cli_suspend(struct cli *cli);
-void cli_resume(struct cli *cli);
 int cli_writeres(int fd, struct cli *cli);
 int cli_readres(int fd, unsigned *status, char **ptr);
 extern struct cli_proto CLI_cmds[];
