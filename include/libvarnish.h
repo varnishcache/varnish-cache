d3dc645238b308b4db70edfae97386998d1bcd80
Dag Erling Smørgrav <des@des.no>
2006-08-07 15:00:55 +0000

    TIM_{format,parse}() are used unconditionally, so declare them unconditionally.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@718 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/libvarnish.h
+++ include/libvarnish.h
@@ -6,11 +6,9 @@
 void FreeArgv(char **argv);
 char **ParseArgv(const char *s, int comment);
 
-#ifdef CLOCK_MONOTONIC
 /* from libvarnish/time.c */
 void TIM_format(time_t t, char *p);
 time_t TIM_parse(const char *p);
-#endif
 
 /* from libvarnish/version.c */
 void varnish_version(const char *);
