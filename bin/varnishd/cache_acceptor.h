50aac9535ebfd8337f6991a67ebb6a48ef2755b5
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-08-21 11:11:02 +0000

    Add file
    git-svn-id: http://www.varnish-cache.org/svn/trunk@864 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ bin/varnishd/cache_acceptor.h
@@ -0,0 +1,31 @@
+/* 
+ * $Id$ 
+ */
+
+struct sess;
+
+typedef void acceptor_init_f(void);
+typedef void acceptor_recycle_f(struct sess *);
+
+struct acceptor {
+	const char 		*name;
+	acceptor_init_f		*init;
+	acceptor_recycle_f	*recycle;
+};
+
+#if defined(HAVE_EPOLL_CTL)
+extern struct acceptor acceptor_epoll;
+#endif
+
+#if defined(HAVE_KQUEUE)
+extern struct acceptor acceptor_kqueue;
+#endif
+
+#if defined(HAVE_POLL)
+extern struct acceptor acceptor_poll;
+#endif
+
+/* vca_acceptor.c */
+struct sess *vca_accept_sess(int fd);
+void vca_handover(struct sess *sp, int bad);
+
