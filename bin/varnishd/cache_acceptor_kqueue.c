b98d7cd1924afcd9be106f8bee53758112571307
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-08-21 20:23:14 +0000

    Remove unused #includes
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@877 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_acceptor_kqueue.c
+++ bin/varnishd/cache_acceptor_kqueue.c
@@ -10,13 +10,9 @@
 
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <sys/uio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/event.h>
 
 #include "heritage.h"
