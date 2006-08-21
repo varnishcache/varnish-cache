926c6b7520b700c683de0e72451a4b70c1e0868e
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-08-21 20:42:47 +0000

    Flexelint inpired cleanups
    git-svn-id: http://www.varnish-cache.org/svn/trunk@881 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_acceptor.h
+++ bin/varnishd/cache_acceptor.h
@@ -26,8 +26,6 @@ extern struct acceptor acceptor_poll;
 #endif
 
 /* vca_acceptor.c */
-struct sess *vca_accept_sess(int fd);
 void vca_handover(struct sess *sp, int bad);
-void vca_handfirst(struct sess *sp);
 int vca_pollsession(struct sess *sp);
 
