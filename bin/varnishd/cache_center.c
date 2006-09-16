f05c51b0039284a4ba3bce6f538dff324036268a
Poul-Henning Kamp <phk@phk.freebsd.dk>
2006-09-16 21:45:27 +0000

    Duh!  We need to count stats in per workerthread and summarize into session, otherwise we cannot correctly summarize into srcaddr and global.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@1040 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_center.c
+++ bin/varnishd/cache_center.c
@@ -284,7 +284,7 @@ cnt_fetch(struct sess *sp)
 		AZ(sp->vbc);
 		HSH_Ref(sp->obj); /* get another, STP_DELIVER will deref */
 		HSH_Unbusy(sp->obj);
-		sp->acct.fetch++;
+		sp->wrk->acct.fetch++;
 		sp->step = STP_DELIVER;
 		return (0);
 	}
@@ -304,7 +304,7 @@ cnt_first(struct sess *sp)
 	assert(sp->xid == 0);
 	VCA_Prep(sp);
 	sp->wrk->idle = sp->t_open.tv_sec;
-	sp->acct.sess++;
+	sp->wrk->acct.sess++;
 	SES_RefSrcAddr(sp);
 	do 
 		i = http_RecvSome(sp->fd, sp->http);
@@ -583,7 +583,7 @@ static int
 cnt_passbody(struct sess *sp)
 {
 
-	sp->acct.pass++;
+	sp->wrk->acct.pass++;
 	AN(sp->vbc);
 	PassBody(sp);
 	AZ(sp->vbc);
@@ -610,7 +610,7 @@ static int
 cnt_pipe(struct sess *sp)
 {
 
-	sp->acct.pipe++;
+	sp->wrk->acct.pipe++;
 	PipeSession(sp);
 	sp->step = STP_DONE;
 	return (0);
@@ -662,7 +662,7 @@ cnt_recv(struct sess *sp)
 	AZ(sp->obj);
 	AZ(sp->vbc);
 
-	sp->acct.req++;
+	sp->wrk->acct.req++;
 	done = http_DissectRequest(sp->http, sp->fd);
 	if (done != 0) {
 		RES_Error(sp, done, NULL);
