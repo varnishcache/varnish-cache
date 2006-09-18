e0ca148934c98d3e4f47d658f23bf13e367cf8f7
Dag Erling Smørgrav <des@des.no>
2006-09-18 21:50:47 +0000

    Regenerate to revert over-eager mechanical edits.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@1089 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvcl/vcc_fixed_token.c
+++ lib/libvcl/vcc_fixed_token.c
@@ -1,31 +1,4 @@
-/*-
- * Copyright (c) 2006 Verdens Gang AS
- * Copyright (c) 2006 Linpro AS
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * Initial implementation by Poul-Henning Kamp <phk@phk.freebsd.dk>
- *
+/*
  * $Id$
  *
  * NB:  This file is machine generated, DO NOT EDIT!
@@ -493,8 +466,35 @@ vcl_output_lang_h(FILE *f)
 	fputs("	vcl_func_f	*fetch_func;\n", f);
 	fputs("	vcl_func_f	*timeout_func;\n", f);
 	fputs("};\n", f);
-	fputs("/*\n", f);
-	fputs(" * $Id$ \n", f);
+	fputs("/*-\n", f);
+	fputs(" * Copyright (c) 2006 Verdens Gang AS\n", f);
+	fputs(" * Copyright (c) 2006 Linpro AS\n", f);
+	fputs(" * All rights reserved.\n", f);
+	fputs(" *\n", f);
+	fputs(" * Redistribution and use in source and binary forms, with or without\n", f);
+	fputs(" * modification, are permitted provided that the following conditions\n", f);
+	fputs(" * are met:\n", f);
+	fputs(" * 1. Redistributions of source code must retain the above copyright\n", f);
+	fputs(" *    notice, this list of conditions and the following disclaimer.\n", f);
+	fputs(" * 2. Redistributions in binary form must reproduce the above copyright\n", f);
+	fputs(" *    notice, this list of conditions and the following disclaimer in the\n", f);
+	fputs(" *    documentation and/or other materials provided with the distribution.\n", f);
+	fputs(" *\n", f);
+	fputs(" * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND\n", f);
+	fputs(" * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n", f);
+	fputs(" * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n", f);
+	fputs(" * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE\n", f);
+	fputs(" * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n", f);
+	fputs(" * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS\n", f);
+	fputs(" * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)\n", f);
+	fputs(" * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT\n", f);
+	fputs(" * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY\n", f);
+	fputs(" * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF\n", f);
+	fputs(" * SUCH DAMAGE.\n", f);
+	fputs(" *\n", f);
+	fputs(" * Initial implementation by Poul-Henning Kamp <phk@phk.freebsd.dk>\n", f);
+	fputs(" *\n", f);
+	fputs(" * $Id$\n", f);
 	fputs(" *\n", f);
 	fputs(" * Runtime support for compiled VCL programs.\n", f);
 	fputs(" *\n", f);
