2d2265cd49cce321a6f2efc9946cc4772fbcf09f
Dag Erling Smørgrav <des@des.no>
2006-10-05 14:56:50 +0000

    Older versions of automake generate incorrect Makefiles, and automake's own mechanism for specifying a required version doesn't seem to work. Hack autogen.sh to check which version of automake is installed.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@1140 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- autogen.sh
+++ autogen.sh
@@ -3,12 +3,28 @@
 # $Id$
 #
 
-set -ex
-
 if [ -d /usr/local/gnu-autotools/bin ] ; then
 	PATH=/usr/local/gnu-autotools/bin:${PATH}
 	export PATH
 fi
+
+automake_version=$(automake --version | tr ' ' '\n' | egrep '^[0-9]\.[0-9a-z.-]+')
+if [ -z "$automake_version" ] ; then
+    echo "unable to determine automake version"
+    exit 1
+else
+    case $automake_version in
+	1.9*|[23456789].*)
+	    ;;
+	*)
+	    echo "your version of automake ($automake_version) is too old;" \
+		"you need 1.9 or newer."
+	    exit 1
+	    ;;
+    esac
+fi
+
+set -ex
 
 aclocal
 libtoolize --copy --force
