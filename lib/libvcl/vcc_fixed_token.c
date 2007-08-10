09e302b5781c622723f9cd3497bb67819a199cc5
Poul-Henning Kamp <phk@phk.freebsd.dk>
2007-08-10 10:28:08 +0000

    Don't call functions outside the VRT namespace, even if this means that we have to wrap strcmp().
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@1830 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvcl/vcc_fixed_token.c
+++ lib/libvcl/vcc_fixed_token.c
@@ -446,6 +446,10 @@ vcl_output_lang_h(struct vsb *sb)
 	vsb_cat(sb, "void VRT_SetHdr(struct sess *, enum gethdr_e where, const char *, const char *, ...);\n");
 	vsb_cat(sb, "void VRT_handling(struct sess *sp, unsigned hand);\n");
 	vsb_cat(sb, "\n");
+	vsb_cat(sb, "/* Simple stuff */\n");
+	vsb_cat(sb, "int VRT_strcmp(const char *s1, const char *s2);\n");
+	vsb_cat(sb, "\n");
+	vsb_cat(sb, "\n");
 	vsb_cat(sb, "/* Backend related */\n");
 	vsb_cat(sb, "void VRT_init_simple_backend(struct backend **, struct vrt_simple_backend *);\n");
 	vsb_cat(sb, "void VRT_fini_backend(struct backend *);\n");
