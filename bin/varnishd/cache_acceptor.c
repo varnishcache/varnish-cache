b42f59ae34c9a39e4097d8b746bbd3024b9c4c00
Poul-Henning Kamp <phk@phk.freebsd.dk>
2007-09-30 19:54:06 +0000

    We may not have a worker thread here, we could be called from the acceptor code on a timeout.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@2060 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_acceptor.c
+++ bin/varnishd/cache_acceptor.c
@@ -241,7 +241,7 @@ vca_close_session(struct sess *sp, const char *why)
 {
 	int i;
 
-	WSP(sp, SLT_SessionClose, "%s", why);
+	VSL(SLT_SessionClose, sp->id, "%s", why);
 	if (sp->fd >= 0) {
 		i = close(sp->fd);
 		assert(i == 0 || errno != EBADF);	/* XXX EINVAL seen */
