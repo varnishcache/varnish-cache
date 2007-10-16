870fb14c59b46720144988a2e9c1244754c42d01
Poul-Henning Kamp <phk@phk.freebsd.dk>
2007-10-16 10:17:24 +0000

    Silence flexelint
    git-svn-id: http://www.varnish-cache.org/svn/trunk@2104 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_http.c
+++ bin/varnishd/cache_http.c
@@ -89,7 +89,7 @@ http2shmlog(const struct http *hp, int t)
 	CHECK_OBJ_NOTNULL(hp, HTTP_MAGIC);
 	if (t > HTTP_HDR_FIRST)
 		t = HTTP_HDR_FIRST;
-	assert(hp->logtag >= HTTP_Rx && hp->logtag <= HTTP_Obj);
+	assert(hp->logtag >= HTTP_Rx && hp->logtag <= HTTP_Obj); /*lint !e685*/
 	assert(t >= HTTP_HDR_REQ && t <= HTTP_HDR_FIRST);
 	return (logmtx[hp->logtag][t]);
 }
