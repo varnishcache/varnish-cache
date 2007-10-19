c83cfe6cc5ef09718cc0655ae4a725b5d81c6a5c
Dag Erling Smørgrav <des@des.no>
2007-10-19 11:20:15 +0000

    Merged revisions 1733 via svnmerge from svn+ssh://projects.linpro.no/svn/varnish/trunk/varnish-cache
    ........
      r1733 | des | 2007-07-20 12:14:43 +0200 (Fri, 20 Jul 2007) | 2 lines
      
      Style nit.
    ........
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/1.1@2136 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache.h
+++ bin/varnishd/cache.h
@@ -96,8 +96,6 @@ void WS_Assert(struct ws *ws);
 void WS_Reset(struct ws *ws);
 char *WS_Alloc(struct ws *ws, unsigned bytes);
 
-
-
 /*--------------------------------------------------------------------
  * HTTP Request/Response/Header handling structure.
  */
