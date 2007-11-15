07af9eda80d341fb764229ed7adbc3efaa4a4d53
Dag Erling Smørgrav <des@des.no>
2007-11-15 16:03:57 +0000

    Merged revisions 2248,2251 via svnmerge from svn+ssh://projects.linpro.no/svn/varnish/trunk/varnish-cache
    ........
      r2248 | des | 2007-11-09 15:03:13 +0100 (Fri, 09 Nov 2007) | 3 lines
      
      Add our own mkstemp(3) clone which allows the template to contain non-
      placeholder characters after the placeholders.
    ........
      r2251 | des | 2007-11-09 23:59:44 +0100 (Fri, 09 Nov 2007) | 2 lines
      
      Forgot to commit this when vtmpfile.c was added.
    ........
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/1.2@2257 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ lib/libvarnish/vtmpfile.c
@@ -0,0 +1,73 @@
+/*-
+ * Copyright (c) 2006 Verdens Gang AS
+ * Copyright (c) 2006-2007 Linpro AS
+ * All rights reserved.
+ *
+ * Author: Dag-Erling Smørgrav <des@linpro.no>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libvarnish.h"
+
+int
+vtmpfile(char *template)
+{
+	char *b, *e, *p;
+	int fd, ran;
+
+	for (b = template; *b != '#'; ++b)
+		/* nothing */ ;
+	if (*b == '\0') {
+		errno = EINVAL;
+		return (-1);
+	}
+	for (e = b; *e == '#'; ++e)
+		/* nothing */ ;
+
+	for (;;) {
+		for (p = b; p < e; ++p) {
+			ran = random() % 63;
+			if (ran < 10)
+				*p = '0' + ran;
+			else if (ran < 36)
+				*p = 'A' + ran - 10;
+			else if (ran < 62)
+				*p = 'a' + ran - 36;
+			else
+				*p = '_';
+		}
+		fd = open(template, O_RDWR|O_CREAT|O_EXCL, 0600);
+		if (fd >= 0)
+			return (fd);
+		if (errno != EEXIST)
+			return (-1);
+	}
+	/* not reached */
+}
