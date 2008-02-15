25f1ff93bee8d9e899b7e9046c907e66bfc04797
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-02-15 10:34:42 +0000

    Add VLU (Varnish Line Up) a copy of LineUp from my private utility library:
    A set of simple functions for cutting a byte stream into text-lines and
    calling a function for each of them, as the they become available.
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@2471 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ include/vlu.h
@@ -0,0 +1,41 @@
+/*-
+ * Copyright (c) 2005-2008 Poul-Henning Kamp <phk@freebsd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: vpf.h 1410 2007-05-11 11:17:09Z des $
+ *
+ * Functions for assembling a bytestream into text-lines and calling
+ * a function on each.
+ */
+
+#ifndef VLU_H_INCLUDED
+#define VLU_H_INCLUDED
+
+typedef int (vlu_f)(void *, const char *);
+struct vlu *VLU_New(void *priv, vlu_f *func);
+int VLU_Fd(int fd, struct vlu *l);
+int VLU_File(FILE *f, struct vlu *l);
+void VLU_Destroy(struct vlu *l);
+
+#endif
