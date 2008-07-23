4eb4937dc84d19e183f1aa058dd27595e73ae4de
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-07-23 15:20:37 +0000

    use strtod() instead of strtof()
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@2997 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishtest/vtc_http.c
+++ bin/varnishtest/vtc_http.c
@@ -589,7 +589,7 @@ cmd_http_timeout(CMD_ARGS)
 	CAST_OBJ_NOTNULL(hp, priv, HTTP_MAGIC);
 	AN(av[1]);
 	AZ(av[2]);
-	hp->timeout = strtof(av[1], NULL) * 1000.0;
+	hp->timeout = strtod(av[1], NULL) * 1000.0;
 }
 
 /**********************************************************************
