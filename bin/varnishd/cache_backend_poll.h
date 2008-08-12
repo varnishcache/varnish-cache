f0c4d814adcfe8629036ca35c69d8bad3df1c9e6
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-08-12 13:39:27 +0000

    New file with the bitmap definitons for for backend polling.
    git-svn-id: http://www.varnish-cache.org/svn/trunk@3086 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ bin/varnishd/cache_backend_poll.h
@@ -0,0 +1,39 @@
+/*-
+ * Copyright (c) 2008 Linpro AS
+ * All rights reserved.
+ *
+ * Author: Poul-Henning Kamp <phk@phk.freebsd.dk>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+BITMAP(good_ipv4, "4", "Good IPv4", 0)
+BITMAP(good_ipv6, "6", "Good IPv4", 0)
+BITMAP( err_xmit, "x", "Error Xmit", 0)
+BITMAP(good_xmit, "X", "Good Xmit", 0)
+BITMAP( err_shut, "s", "Error Shut", 0)
+BITMAP(good_shut, "S", "Good Shut", 0)
+BITMAP( err_recv, "r", "Error Recv", 0)
+BITMAP(good_recv, "R", "Good Recv", 1)
