849d0c15b89f9ba3182a67a180686a0fce410ead
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-09-26 13:07:51 +0000

    Constify three strings in compiled VCL data structure vrt_backend
    git-svn-id: http://www.varnish-cache.org/svn/trunk@3229 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvcl/vcc_fixed_token.c
+++ lib/libvcl/vcc_fixed_token.c
@@ -340,10 +340,10 @@ vcl_output_lang_h(struct vsb *sb)
 	vsb_cat(sb, " * A backend is a host+port somewhere on the network\n");
 	vsb_cat(sb, " */\n");
 	vsb_cat(sb, "struct vrt_backend {\n");
-	vsb_cat(sb, "	char				*vcl_name;\n");
-	vsb_cat(sb, "	char				*ident;\n");
+	vsb_cat(sb, "	const char			*vcl_name;\n");
+	vsb_cat(sb, "	const char			*ident;\n");
 	vsb_cat(sb, "\n");
-	vsb_cat(sb, "	char				*hosthdr;\n");
+	vsb_cat(sb, "	const char			*hosthdr;\n");
 	vsb_cat(sb, "\n");
 	vsb_cat(sb, "	const unsigned char		*ipv4_sockaddr;\n");
 	vsb_cat(sb, "	const unsigned char		*ipv6_sockaddr;\n");
