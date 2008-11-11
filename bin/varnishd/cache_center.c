9369e4007904414cc65eafd82acaf77ecd2110d0
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-11-11 19:06:55 +0000

    Implement restart in vcl_hit.
    Fixes #365
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@3386 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_center.c
+++ bin/varnishd/cache_center.c
@@ -543,7 +543,9 @@ cnt_hit(struct sess *sp)
 		sp->step = STP_ERROR;
 		return (0);
 	case VCL_RET_RESTART:
-		INCOMPL();
+		sp->director = NULL;
+		sp->restarts++;
+		sp->step = STP_RECV;
 		return (0);
 	default:
 		WRONG("Illegal action in vcl_hit{}");
