d5bc1a1b8b701b1da96d1a37fc5c66cd0dc2a4cd
Poul-Henning Kamp <phk@phk.freebsd.dk>
2008-11-25 11:45:59 +0000

    Neuther the FreeBSD specifics of SHA256 implementation.
    In the end, it comes down to lack of POSIX definition of a way to find
    out byte-endianess, sigh...
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@3438 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/vsha256.h
+++ include/vsha256.h
@@ -29,7 +29,7 @@
 #ifndef _SHA256_H_
 #define _SHA256_H_
 
-#include <sys/types.h>
+#include <stdint.h>
 
 typedef struct SHA256Context {
 	uint32_t state[8];
@@ -41,10 +41,6 @@ __BEGIN_DECLS
 void	SHA256_Init(SHA256_CTX *);
 void	SHA256_Update(SHA256_CTX *, const void *, size_t);
 void	SHA256_Final(unsigned char [32], SHA256_CTX *);
-char   *SHA256_End(SHA256_CTX *, char *);
-char   *SHA256_File(const char *, char *);
-char   *SHA256_FileChunk(const char *, char *, off_t, off_t);
-char   *SHA256_Data(const void *, unsigned int, char *);
 __END_DECLS
 
 #endif /* !_SHA256_H_ */
