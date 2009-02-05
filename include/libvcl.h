6c2de6378ddcb3293a41c0daa5c80332a98aa963
Tollef Fog Heen <tfheen@varnish-software.com>
2009-02-05 12:23:06 +0000

    Merge r3415: Simplify how we manage the -f argument:
    The VCL file specified to -f must be read relative to the directory
    from which varnishd is started, before we chdir to the workdir.
    
    We used to deal with this by opening the file and passing the file
    handle down.  It's simpler to just read the file and pass the actual
    VCL code down.
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.0@3622 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/libvcl.h
+++ include/libvcl.h
@@ -30,7 +30,6 @@
  */
 
 char *VCC_Compile(struct vsb *sb, const char *b, const char *e);
-char *VCC_CompileFile(struct vsb *sb, const char *fn, int fd);
 void VCC_InitCompile(const char *default_vcl);
 
 
