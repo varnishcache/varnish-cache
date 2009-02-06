3df2a6fe1af60db3e6784be6afff906d9af50aee
Tollef Fog Heen <tfheen@varnish-software.com>
2009-02-06 13:55:45 +0000

    Merge r3481: Flexelint adjustments
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.0@3674 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/flint.sh
+++ bin/varnishd/flint.sh
@@ -10,25 +10,4 @@ flexelint \
 	flint.lnt \
 	*.c \
 	../../lib/libvarnish/*.c \
-	../../lib/libvcl/*.c \
-	> $T 2>&1
-
-for t in Error Warning Info Note
-do
-	sed -n "/$t [0-9][0-9][0-9]:/s/.*\($t [0-9][0-9][0-9]\).*/\1/p" $T
-done | awk '
-$2 == 830	{ next }
-$2 == 831	{ next }
-	{
-	i=$2"_"$1
-	h[i]++
-	n++
-	}
-END	{
-	printf "%5d %s\n", n, "Total"
-	for (i in h)
-		printf "%5d %s\n", h[i], i
-	}
-' | sort -rn
-
-cat $T
+	../../lib/libvcl/*.c 
