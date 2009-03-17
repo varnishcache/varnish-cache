4049f93936a9d70cae650db514b68e2038e865e5
Poul-Henning Kamp <phk@phk.freebsd.dk>
2009-03-17 10:20:28 +0000

    Comments, asserts and some locking
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@3944 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/common.h
+++ bin/varnishd/common.h
@@ -31,6 +31,8 @@
 
 struct cli;
 struct sockaddr;
+extern pid_t mgt_pid;
+#define ASSERT_MGT() do { assert(getpid() == mgt_pid);} while (0)
 
 /* cache_acceptor.c */
 void VCA_tweak_waiter(struct cli *cli, const char *arg);
