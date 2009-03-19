8685aaab5cfdfbd3cdb5fe6ec0d5131ef6f8f1bd
Tollef Fog Heen <tfheen@varnish-software.com>
2009-03-19 12:50:02 +0000

    Merge r3771: Correct copyright and attribution.
    Approved by:    phk, ssm
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.0@3958 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishtest/vtc.c
+++ bin/varnishtest/vtc.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2006-2008 Linpro AS
+ * Copyright (c) 2008-2009 Linpro AS
  * All rights reserved.
+ *
+ * Author: Poul-Henning Kamp <phk@phk.freebsd.dk>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
