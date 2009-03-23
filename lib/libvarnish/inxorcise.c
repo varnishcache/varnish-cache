5be4bc240d49aa29155a5aef1e2c81cb51ac439d
Artur Bergman <sky@crucially.net>
2009-03-23 11:29:31 +0000

    it helps if I do svn add
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@3984 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ lib/libvarnish/inxorcise.c
@@ -0,0 +1,56 @@
+/*-
+ * Copyright (c) 2006 Verdens Gang AS
+ * Copyright (c) 2006-2008 Linpro AS
+ * All rights reserved.
+ *
+ * Author: Poul-Henning Kamp <phk@phk.freebsd.dk>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: $
+ *
+ * This file only exists because Apple has decided to break a
+ * function that has an age old tradition and force the user
+ * to use launchd instead.
+ *
+ *
+ */
+ 
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libvarnish.h"
+
+#ifdef __APPLE__ && __MAC_OS_X_VERSION_10_5
+int inxorcise(int nochdir __attribute__((unused)), int noclose __attribute__((unused))) {
+	fprintf(stderr, "Apple does not want programs to use daemon(3) and suggests using launchd(1).  We don't agree, but their dad is bigger than our dad.\n");
+	exit(1);
+}
+#else
+int inxorcise(int nochdir, int noclose) {
+	return (daemon(nochdir, noclose));
+}
+#endif
+
+
