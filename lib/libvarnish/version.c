6c110ac5acb30091557925961ccffdb05141ab44
Tollef Fog Heen <tfheen@varnish-software.com>
2009-03-30 09:42:44 +0000

    Include SVN revision in version string
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@4006 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvarnish/version.c
+++ lib/libvarnish/version.c
@@ -40,8 +40,8 @@
 void
 varnish_version(const char *progname)
 {
-	fprintf(stderr, "%s (%s-%s)\n", progname,
-	    PACKAGE_TARNAME, PACKAGE_VERSION);
+	fprintf(stderr, "%s (%s-%s SVN %s)\n", progname,
+		PACKAGE_TARNAME, PACKAGE_VERSION, svn_version());
 	fprintf(stderr,
 	    "Copyright (c) 2006-2009 Linpro AS / Verdens Gang AS\n");
 }
