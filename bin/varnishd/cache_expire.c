af11c706f87f9c3e43445e2343e427f4890d49cf
Artur Bergman <sky@crucially.net>
2009-07-27 09:54:31 +0000

    reminder to add test case to catch this error
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@4145 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_expire.c
+++ bin/varnishd/cache_expire.c
@@ -334,6 +334,11 @@ EXP_NukeOne(struct sess *sp, struct objcore_head *lru)
 	if (oc == NULL)
 		return (-1);
 
+	/* o is never allocated so this shouldn't work
+	 * making a comment so I don't forget
+	 * why does no test case capture this?
+	 * could be my swine flu riddled mind playing games? / Artur
+	 */
 	WSL(sp->wrk, SLT_ExpKill, 0, "%u LRU", o->xid);
 	HSH_Deref(sp->wrk, &o);
 	return (1);
