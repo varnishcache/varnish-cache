bd7206af66a63b8a9aeb85417fcf7172fad88802
Tollef Fog Heen <tfheen@varnish-software.com>
2009-10-08 12:26:18 +0000

    Merge r4150: Emit more headers when hitting 304
    When hitting 304, ETag, Content-Location, Expires, Cache-control and
    Vary should all be sent to the client.
    
    See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5
    for some more details
    
    Thanks to Rogério Schneider for the patch.
    
    Fixes #529
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.0@4295 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_response.c
+++ bin/varnishd/cache_response.c
@@ -46,6 +46,7 @@ static void
 res_do_304(struct sess *sp)
 {
 	char lm[64];
+	char *p;
 
 	WSP(sp, SLT_Length, "%u", 0);
 
@@ -60,6 +61,19 @@ res_do_304(struct sess *sp)
 		TIM_format(sp->obj->last_modified, lm);
 		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Last-Modified: %s", lm);
 	}
+
+	/* http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5 */
+	if (http_GetHdr(sp->obj->http, H_Cache_Control, &p))
+		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Cache-Control: %s", p);
+	if (http_GetHdr(sp->obj->http, H_Content_Location, &p))
+		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Content-Location: %s", p);
+	if (http_GetHdr(sp->obj->http, H_ETag, &p))
+		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "ETag: %s", p);
+	if (http_GetHdr(sp->obj->http, H_Expires, &p))
+		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Expires: %s", p);
+	if (http_GetHdr(sp->obj->http, H_Vary, &p))
+		http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Vary: %s", p);
+
 	http_PrintfHeader(sp->wrk, sp->fd, sp->http, "Connection: %s",
 	    sp->doclose ? "close" : "keep-alive");
 	sp->wantbody = 0;
