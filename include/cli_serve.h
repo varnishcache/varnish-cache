e58db9b9f3c82afa120e337b7ad1519a8fc62264
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-01-18 20:33:42 +0000

    First attempt at unified, general "serve CLI requests" code. (not used yet)
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@4469 d4fa192b-c00b-0410-8231-f00ffab90ce4

---
+++ include/cli_serve.h
@@ -0,0 +1,38 @@
+/*-
+ * Copyright (c) 2006-2009 Linpro AS
+ * All rights reserved.
+ *
+ * Author: Poul-Henning Kamp <phk@phk.freebsd.dk>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ *
+ */
+
+struct cls;
+typedef void cls_cb_f(void *priv);
+struct cls *CLS_New(cls_cb_f *before, cls_cb_f *after, void *priv);
+int CLS_AddFd(struct cls *cs, int fdi, int fdo, cls_cb_f *closefunc,
+    void *priv);
+int CLS_AddFunc(struct cls *cs, struct cli_proto *clp);
+int CLS_Poll(struct cls *cs, int timeout);
