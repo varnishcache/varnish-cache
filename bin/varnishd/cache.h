d72e28432d0da28cf1388dcb199ebe09a7c87fc6
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-02-01 13:25:01 +0000

    When we detect the parent absconding the CLI pipes, close the acceptor sockets as fast as we can, so that a new copy of varnishd can get at them.
    Fixes	#622
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk@4521 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache.h
+++ bin/varnishd/cache.h
@@ -450,6 +450,7 @@ void vca_return_session(struct sess *sp);
 void vca_close_session(struct sess *sp, const char *why);
 void VCA_Prep(struct sess *sp);
 void VCA_Init(void);
+void VCA_Shutdown(void);
 const char *VCA_waiter_name(void);
 extern pthread_t VCA_thread;
 
