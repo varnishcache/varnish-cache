f9b44aba5bd3d9bdefdd502a19711f33e33f2518
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-02-02 23:37:43 +0000

    When serving ESI to HTTP/1.0 clients, we use EOF mode.  Remeber to properly close the connection when doing so.
    Fixes 524
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk@4529 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_response.c
+++ bin/varnishd/cache_response.c
@@ -151,6 +151,8 @@ RES_BuildHttp(struct sess *sp)
 		if(sp->http->protover >= 1.1)
 			http_PrintfHeader(sp->wrk, sp->fd, sp->wrk->resp,
 			    "Transfer-Encoding: chunked");
+		else 
+			sp->doclose = "ESI EOF";
 	}
 
 	TIM_format(TIM_real(), time_str);
