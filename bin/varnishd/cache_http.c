d076a2775a7d7a3da59e798bcb4101894c5ed1ad
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-03-03 09:12:10 +0000

    Assert on the right HTTP structure.
    Found by:	luc/tom28
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk@4603 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_http.c
+++ bin/varnishd/cache_http.c
@@ -644,7 +644,7 @@ http_copyheader(struct worker *w, int fd, struct http *to,
 
 	CHECK_OBJ_NOTNULL(fm, HTTP_MAGIC);
 	CHECK_OBJ_NOTNULL(to, HTTP_MAGIC);
-	assert(n < to->shd);
+	assert(n < fm->shd);
 	Tcheck(fm->hd[n]);
 	if (to->nhd < to->shd) {
 		to->hd[to->nhd] = fm->hd[n];
