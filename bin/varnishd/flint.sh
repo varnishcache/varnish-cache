f2125096d559d37a34c06a1b546c540c068b6a7f
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-05-20 07:24:18 +0000

    Add a diff facility for the output
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@4832 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/flint.sh
+++ bin/varnishd/flint.sh
@@ -10,4 +10,10 @@ flexelint \
 	*.c \
 	../../lib/libvarnish/*.c \
 	../../lib/libvarnishcompat/execinfo.c \
-	../../lib/libvcl/*.c
+	../../lib/libvcl/*.c 2>&1 | tee _.fl
+
+if [ -f _.fl.old ] ; then
+	diff -u _.fl.old _.fl
+fi
+
+mv _.fl _.fl.old
