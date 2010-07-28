40baed607eeb5dd66bdb94adc21473b216fdc1ff
Tollef Fog Heen <tfheen@varnish-software.com>
2010-07-28 09:54:29 +0000

    Merge r5048: Make Expect 100-continue test case-insensitive
    Candidate for merging into 2.1 branch
    
    
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.1@5050 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_center.c
+++ bin/varnishd/cache_center.c
@@ -1150,7 +1150,7 @@ cnt_start(struct sess *sp)
 	 * Handle Expect headers
 	 */
 	if (http_GetHdr(sp->http, H_Expect, &p)) {
-		if (strcmp(p, "100-continue")) {
+		if (strcasecmp(p, "100-continue")) {
 			sp->err_code = 417;
 			sp->step = STP_ERROR;
 			return (0);
