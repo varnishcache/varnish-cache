de4686d56b5d6f8966f20f0698b72e96945e8645
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-09-08 11:25:50 +0000

    Add an ugly workaround for a bug that causes coredumps in processes which dlclose() objects compiled with gcov support.  (Search "atexit dlclose" for details).
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@5188 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_vrt_vmod.c
+++ bin/varnishd/cache_vrt_vmod.c
@@ -122,7 +122,9 @@ VRT_Vmod_Fini(void **hdl)
 	*hdl = NULL;
 	if (--v->ref != 0)
 		return;
+#ifndef DONT_DLCLOSE_VMODS
 	dlclose(v->hdl);
+#endif
 	free(v->nm);
 	free(v->path);
 	VTAILQ_REMOVE(&vmods, v, list);
