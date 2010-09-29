1bccee28cbc4ad1377581a1f13e234229460a1ae
Tollef Fog Heen <tfheen@varnish-software.com>
2010-09-29 08:31:36 +0000

    Merge r5122: Look for separating space before colon, so that we can grok ":: 50239" as a socketaddr.
    git-svn-id: http://www.varnish-cache.org/svn/branches/2.1@5333 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvarnish/vss.c
+++ lib/libvarnish/vss.c
@@ -104,9 +104,9 @@ VSS_parse(const char *str, char **addr, char **port)
 		}
 	} else {
 		/* IPv4 address of the form 127.0.0.1:80, or non-numeric */
-		p = strchr(str, ':');
+		p = strchr(str, ' ');
 		if (p == NULL)
-			p = strchr(str, ' ');
+			p = strchr(str, ':');
 		if (p == NULL) {
 			*addr = strdup(str);
 			XXXAN(*addr);
