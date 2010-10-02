f508d29a1959aebcdd03fd5cd9cc161ef67b104c
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-10-02 18:14:09 +0000

    Don't list methods manually, we have them in a variable.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@5385 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvcl/generate.py
+++ lib/libvcl/generate.py
@@ -95,7 +95,6 @@ returns =(
 	('error',	('restart', 'deliver',)),
 )
 
-
 #######################################################################
 # Variables available in sessions
 
@@ -696,16 +695,12 @@ struct VCL_conf {
 
 	vcl_init_f	*init_func;
 	vcl_fini_f	*fini_func;
+""")
 
-	vcl_func_f	*recv_func;
-	vcl_func_f	*pipe_func;
-	vcl_func_f	*pass_func;
-	vcl_func_f	*hash_func;
-	vcl_func_f	*miss_func;
-	vcl_func_f	*hit_func;
-	vcl_func_f	*fetch_func;
-	vcl_func_f	*deliver_func;
-	vcl_func_f	*error_func;
+for i in returns:
+	fo.write("\tvcl_func_f\t*" + i[0] + "_func;\n")
+
+fo.write("""
 };
 """)
 
