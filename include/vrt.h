bfb4ccf3337947cbe6e43826c17455c67bc61da2
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-10-04 15:06:23 +0000

    Minor constification
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@5398 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- include/vrt.h
+++ include/vrt.h
@@ -200,7 +200,7 @@ struct vmod_priv {
 typedef int vmod_init_f(struct vmod_priv *,  const struct VCL_conf *);
 
 static inline void 
-vmod_priv_fini(struct vmod_priv *p)
+vmod_priv_fini(const struct vmod_priv *p)
 {
 
 	if (p->priv != (void*)0 && p->free != (void*)0)
