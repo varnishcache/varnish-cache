57834d08d26b113796bd704dd4c2de84ac3780c6
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-12-15 12:03:05 +0000

    Fix a commit to reflect that http_copyh() is only to be used for the fixed location HTTP fields.
    Prodded by: email from anordby, in re r4603
    
    
    
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@5645 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- bin/varnishd/cache_http.c
+++ bin/varnishd/cache_http.c
@@ -661,7 +661,7 @@ static void
 http_copyh(const struct http *to, const struct http *fm, unsigned n)
 {
 
-	assert(n < to->shd);
+	assert(n < HTTP_HDR_FIRST);
 	Tcheck(fm->hd[n]);
 	to->hd[n] = fm->hd[n];
 	to->hdf[n] = fm->hdf[n];
