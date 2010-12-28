d241fc5fe1d41e82d49e1f522b1dde4b9d5d240c
Poul-Henning Kamp <phk@phk.freebsd.dk>
2010-12-28 14:43:15 +0000

    Make sure we have uintptr_t typedef.
    git-svn-id: http://www.varnish-cache.org/svn/trunk/varnish-cache@5665 d4fa192b-c00b-0410-8231-f00ffab90ce4

--- lib/libvgz/zconf.h
+++ lib/libvgz/zconf.h
@@ -8,6 +8,8 @@
 #ifndef ZCONF_H
 #define ZCONF_H
 
+#include <stdint.h>
+
 /*
  * If you *really* need a unique prefix for all types and library functions,
  * compile with -DZ_PREFIX. The "standard" zlib should be compiled without it.
