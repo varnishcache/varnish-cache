76fc17c1852e2a1cb29f4e49a32009a93bb45972
Poul-Henning Kamp <phk@FreeBSD.org>
2011-05-27 20:22:15 +0000

    Make sure that the arguments we pass regsub() came out as strings.
    Fixes #921

--- lib/libvcl/vcc_expr.c
+++ lib/libvcl/vcc_expr.c
@@ -454,6 +454,8 @@ vcc_Eval_Regsub(struct vcc *tl, struct expr **e, const struct symbol *sym)
 	SkipToken(tl, '(');
 
 	vcc_expr0(tl, &e2, STRING);
+	if (e2->fmt != STRING) 
+		vcc_expr_tostring(&e2, STRING);
 
 	SkipToken(tl, ',');
 	ExpectErr(tl, CSTR);
@@ -465,6 +467,8 @@ vcc_Eval_Regsub(struct vcc *tl, struct expr **e, const struct symbol *sym)
 
 	SkipToken(tl, ',');
 	vcc_expr0(tl, &e2, STRING);
+	if (e2->fmt != STRING) 
+		vcc_expr_tostring(&e2, STRING);
 	*e = vcc_expr_edit(STRING, "\v1, \v2)", *e, e2);
 	SkipToken(tl, ')');
 }
