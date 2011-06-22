03fe1a49c5416af5419b2f648ee6c237d4815597
Poul-Henning Kamp <phk@FreeBSD.org>
2011-06-22 10:52:47 +0000

    Don't force a rerun to save reference if we just ran it.

--- bin/varnishd/flint.sh
+++ bin/varnishd/flint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ "x$1" = "x-ok" -a -f _.fl ] ; then
+	echo "Saved as reference"
+	mv _.fl _.fl.old
+	exit 0
+fi
+
 flexelint \
 	../flint.lnt \
 	flint.lnt \
@@ -21,5 +27,6 @@ if [ -f _.fl.old ] ; then
 fi
 
 if [ "x$1" = "x-ok" ] ; then
+	echo "Saved as reference"
 	mv _.fl _.fl.old
 fi
