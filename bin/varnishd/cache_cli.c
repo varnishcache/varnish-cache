bc2a0bd07058013d56fd53237b1ddd8012fb8e89
Poul-Henning Kamp <phk@FreeBSD.org>
2011-08-15 07:28:52 +0000

    ifdef the OFOF development tool

--- bin/varnishd/cache_cli.c
+++ bin/varnishd/cache_cli.c
@@ -140,6 +140,7 @@ cli_debug_sizeof(struct cli *cli, const char * const *av, void *priv)
 	SZOF(struct vbc);
 	SZOF(struct VSC_C_main);
 	SZOF(struct lock);
+#if 0
 #define OFOF(foo, bar)	{ foo __foo; VCLI_Out(cli, \
     "%-30s = 0x%4zx @ 0x%4zx\n", \
 	#foo "." #bar, sizeof(__foo.bar), offsetof(foo, bar)); }
@@ -198,6 +199,8 @@ cli_debug_sizeof(struct cli *cli, const char * const *av, void *priv)
 	OFOF(struct object, store);
 	OFOF(struct object, esidata);
 	OFOF(struct object, last_use);
+#endif
+#undef OFOF
 #endif
 }
 
