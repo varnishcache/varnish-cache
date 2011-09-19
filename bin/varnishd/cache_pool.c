e5975a5f721fa91b954735b54121d54554696cba
Poul-Henning Kamp <phk@FreeBSD.org>
2011-09-19 18:26:20 +0000

    Fix OS/X compilation for good.
    Fixes	#1022

--- bin/varnishd/cache_pool.c
+++ bin/varnishd/cache_pool.c
@@ -59,9 +59,32 @@
 #include "stevedore.h"
 #include "hash_slinger.h"
 
+/*--------------------------------------------------------------------
+ * MAC OS/X is incredibly moronic when it comes to time and such...
+ */
+
 #ifndef CLOCK_MONOTONIC
-#define CLOCK_MONOTONIC CLOCK_REALTIME		/* OS/X is silly */
-#endif
+#define CLOCK_MONOTONIC	0
+
+static int
+clock_gettime(int foo, struct timespec *ts)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	ts->tv_sec = tv.tv_sec;
+	ts->tv_nsec = tv.tv_usec * 1000;
+	return (0);
+}
+
+static int
+pthread_condattr_setclock(pthread_condattr_t *attr, int foo)
+{
+	(void)attr;
+	(void)foo;
+	return (0);
+}
+#endif /* !CLOCK_MONOTONIC */
 
 static void *waiter_priv;
 
