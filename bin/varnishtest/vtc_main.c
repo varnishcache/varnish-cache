5b48f8a64bf663f5a9ac25580ff0a0fb500a6b29
Poul-Henning Kamp <phk@FreeBSD.org>
2011-09-20 09:41:28 +0000

    Missing newline

--- bin/varnishtest/vtc_main.c
+++ bin/varnishtest/vtc_main.c
@@ -228,7 +228,7 @@ tst_cb(const struct vev *ve, int what)
 			    jp->tst->filename, t);
 			if (WIFSIGNALED(stx))
 				printf(" signal=%d", WTERMSIG(stx));
-			printf(" exit=%d", WEXITSTATUS(stx));
+			printf(" exit=%d\n", WEXITSTATUS(stx));
 			if (!vtc_continue) {
 				/* XXX kill -9 other jobs ? */
 				exit(2);
