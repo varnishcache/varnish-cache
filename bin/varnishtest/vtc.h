902bdf49b5465e5336dc6ce51593112c289d1499
Poul-Henning Kamp <phk@FreeBSD.org>
2011-09-20 20:22:09 +0000

    Fix a long-standing bug in varnishtest:
    It used to be that we'd call http processing on a filedescriptor and
    then that was that.  Then we added keywords to do new accepts in the
    server and suddenly the calling code closes a wrong filedesc which
    belongs to somebody else and things get really inconvenient fast.
    
    This made all test-cases which use the "accept" server directive flakey.

--- bin/varnishtest/vtc.h
+++ bin/varnishtest/vtc.h
@@ -64,7 +64,7 @@ extern pthread_t	vtc_thread;
 
 void init_sema(void);
 
-void http_process(struct vtclog *vl, const char *spec, int sock, int sfd);
+int http_process(struct vtclog *vl, const char *spec, int sock, int *sfd);
 
 void cmd_server_genvcl(struct vsb *vsb);
 
--- bin/varnishtest/vtc_client.c
+++ bin/varnishtest/vtc_client.c
@@ -103,7 +103,7 @@ client_thread(void *priv)
 		VTCP_myname(fd, mabuf, sizeof mabuf, mpbuf, sizeof mpbuf);
 		vtc_log(vl, 3, "connected fd %d from %s %s to %s",
 		    fd, mabuf, mpbuf, VSB_data(vsb));
-		http_process(vl, c->spec, fd, -1);
+		fd = http_process(vl, c->spec, fd, NULL);
 		vtc_log(vl, 3, "closing fd %d", fd);
 		VTCP_close(&fd);
 	}
