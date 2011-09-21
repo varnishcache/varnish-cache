656e038e1657c0384d48789833747a228be9bcd6
Poul-Henning Kamp <phk@FreeBSD.org>
2011-09-21 10:40:05 +0000

    More detail on write errors.

--- bin/varnishtest/vtc_http.c
+++ bin/varnishtest/vtc_http.c
@@ -143,7 +143,8 @@ http_write(const struct http *hp, int lvl, const char *pfx)
 	vtc_dump(hp->vl, lvl, pfx, VSB_data(hp->vsb), VSB_len(hp->vsb));
 	l = write(hp->fd, VSB_data(hp->vsb), VSB_len(hp->vsb));
 	if (l != VSB_len(hp->vsb))
-		vtc_log(hp->vl, 0, "Write failed: %s", strerror(errno));
+		vtc_log(hp->vl, 0, "Write failed: (%d vs %d) %s",
+		    l, VSB_len(hp->vsb), strerror(errno));
 }
 
 /**********************************************************************
