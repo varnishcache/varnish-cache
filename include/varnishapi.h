4507c59580f9e5089d9d4e863ca2740daf975fc0
Poul-Henning Kamp <phk@FreeBSD.org>
2011-10-09 16:20:05 +0000

    Also split out the VSC part of the (v)abi

--- include/varnishapi.h
+++ include/varnishapi.h
@@ -32,6 +32,7 @@
 #define VARNISHAPI_H_INCLUDED
 
 #include <vapi/vsm.h>
+#include <vapi/vsc.h>
 
 #include <stdint.h>
 
@@ -42,58 +43,6 @@
  *	All malloc failures will result in assert tripping.
  *	API use failures will trip assert.
  */
-
-
-/*---------------------------------------------------------------------
- * VSC level access functions
- */
-
-void VSC_Setup(struct VSM_data *vd);
-	/*
-	 * Setup vd for use with VSC functions.
-	 */
-
-#define VSC_ARGS	"f:n:"
-#define VSC_n_USAGE	VSM_n_USAGE
-#define VSC_USAGE	VSC_N_USAGE
-
-int VSC_Arg(struct VSM_data *vd, int arg, const char *opt);
-	/*
-	 * Handle standard stat-presenter arguments
-	 * Return:
-	 *	-1 error
-	 *	 0 not handled
-	 *	 1 Handled.
-	 */
-
-int VSC_Open(struct VSM_data *vd, int diag);
-	/*
-	 * Open shared memory for VSC processing.
-	 * args and returns as VSM_Open()
-	 */
-
-struct VSC_C_main *VSC_Main(struct VSM_data *vd);
-	/*
-	 * return Main stats structure
-	 */
-
-struct VSC_point {
-	const char *class;		/* stat struct type		*/
-	const char *ident;		/* stat struct ident		*/
-	const char *name;		/* field name			*/
-	const char *fmt;		/* field format ("uint64_t")	*/
-	int flag;			/* 'a' = counter, 'i' = gauge	*/
-	const char *desc;		/* description			*/
-	const volatile void *ptr;	/* field value			*/
-};
-
-typedef int VSC_iter_f(void *priv, const struct VSC_point *const pt);
-
-int VSC_Iter(struct VSM_data *vd, VSC_iter_f *func, void *priv);
-	/*
-	 * Iterate over all statistics counters, calling "func" for
-	 * each counter not suppressed by any "-f" arguments.
-	 */
 
 /*---------------------------------------------------------------------
  * VSL level access functions
