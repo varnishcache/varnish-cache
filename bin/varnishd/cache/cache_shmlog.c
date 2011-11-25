42d9a31ac0dc63f0ae971974b4a4c36c20f421ef
Poul-Henning Kamp <phk@FreeBSD.org>
2011-11-25 11:40:54 +0000

    Always remember what your pointers point to, when doing pointer addition.
    Fixes	#1065

--- bin/varnishd/cache/cache_shmlog.c
+++ bin/varnishd/cache/cache_shmlog.c
@@ -335,7 +335,7 @@ VSM_Init(void)
 	VWMB();
 
 	vsl_start = vsl_log_start;
-	vsl_end = vsl_start + cache_param->vsl_space;
+	vsl_end = vsl_start + cache_param->vsl_space / sizeof *vsl_end;
 	vsl_ptr = vsl_start + 1;
 
 	VSC_C_main = VSM_Alloc(sizeof *VSC_C_main,
