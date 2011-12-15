b83c68dd51ed522aac12129ecf6e45adbc28ea4f
Poul-Henning Kamp <phk@FreeBSD.org>
2011-12-15 09:21:45 +0000

    Formailize some of the macro-crutches I use to explain stuff to FlexeLint.

--- bin/varnishd/common/common.h
+++ bin/varnishd/common/common.h
@@ -42,6 +42,26 @@
 
 struct cli;
 
+/**********************************************************************
+ * FlexeLint and compiler shutuppery
+ */
+
+/*
+ * In OO-light situations, functions have to match their prototype
+ * even if that means not const'ing a const'able argument.
+ * The typedef should be specified as argument to the macro.
+ */
+#define __match_proto__(xxx)		/*lint -e{818} */
+
+/*
+ * State variables may change value before we use the last value we
+ * set them to.
+ * Pass no argument.
+ */
+#define __state_variable__(xxx)		/*lint -esym(838,xxx) */
+
+/**********************************************************************/
+
 /* Name of transient storage */
 #define TRANSIENT_STORAGE	"Transient"
 
@@ -52,8 +72,6 @@ extern pid_t mgt_pid;
 extern struct vsb *vident;		// XXX: -> heritage ?
 int Symbol_Lookup(struct vsb *vsb, void *ptr);
 
-/* Help shut up FlexeLint */
-#define __match_proto__(xxx) /*lint -e{818} */
 
 /* Really belongs in mgt.h, but storage_file chokes on both */
 void mgt_child_inherit(int fd, const char *what);
