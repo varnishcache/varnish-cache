c9c0572849473c712e46424e717f3267f0e24a1c
Poul-Henning Kamp <phk@FreeBSD.org>
2012-02-13 21:50:32 +0000

    Use VSL_Setup() once more

--- bin/varnishd/cache/cache_wrk.c
+++ bin/varnishd/cache/cache_wrk.c
@@ -141,8 +141,7 @@ wrk_thread_real(void *priv, unsigned shm_workspace, unsigned thread_workspace)
 	memset(w, 0, sizeof *w);
 	w->magic = WORKER_MAGIC;
 	w->lastused = NAN;
-	w->vsl->wlb = w->vsl->wlp = wlog;
-	w->vsl->wle = wlog + (sizeof wlog) / 4;
+	VSL_Setup(w->vsl, wlog, sizeof wlog);
 	AZ(pthread_cond_init(&w->cond, NULL));
 
 	WS_Init(w->aws, "wrk", ws, thread_workspace);
