a636c010cbb767a4610d8d6b11f97e15d027bbce
Poul-Henning Kamp <phk@FreeBSD.org>
2012-02-19 11:05:09 +0000

    Detect client crashing during startup

--- bin/varnishtest/vtc_varnish.c
+++ bin/varnishtest/vtc_varnish.c
@@ -159,6 +159,12 @@ wait_running(const struct varnish *v)
 		if (st != CLIS_OK)
 			vtc_log(v->vl, 0,
 			    "CLI status command failed: %u %s", st, r);
+		if (!strcmp(r, "Child in state stopped")) {
+			vtc_log(v->vl, 0,
+			    "Child stopped before running: %u %s", st, r);
+			free(r);
+			break;
+		}
 		if (!strcmp(r, "Child in state running")) {
 			free(r);
 			break;
