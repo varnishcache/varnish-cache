fde20f7adb6cd51cff84fe4731f3b9da1ed3217f
Poul-Henning Kamp <phk@FreeBSD.org>
2012-08-22 09:31:21 +0000

    A couple of cleanups to get OSX to be more happy.

--- bin/varnishd/storage_persistent.h
+++ bin/varnishd/storage_persistent.h
@@ -42,7 +42,7 @@
 #endif
 
 #define ASSERT_SILO_THREAD(sc) \
-    do {assert(pthread_self() == (sc)->thread);} while (0)
+    do {assert(pthread_equal(pthread_self(), (sc)->thread));} while (0)
 
 #define OC_F_NEEDFIXUP OC_F_PRIV
 
