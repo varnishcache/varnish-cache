042a2d631c5f5a697cd0951eba276401f09bb957
Martin Blix Grydeland <martin@varnish-software.com>
2012-10-03 15:08:14 +0200

    Don't always check the sign after smp_append_sign().
    Remove the XXXAZ(smp_chk_sign) after smp_append_sign(), as it causes
    heavy unnecessary CPU usage after each sign update. No assertion
    reports have come from this test, so there is no reason to expect the
    signs not working.

--- bin/varnishd/storage/storage_persistent_subr.c
+++ bin/varnishd/storage/storage_persistent_subr.c
@@ -128,7 +128,6 @@ smp_append_sign(struct smp_signctx *ctx, const void *ptr, uint32_t len)
 	SHA256_Update(&cx, &ctx->ss->length, sizeof(ctx->ss->length));
 	SHA256_Final(sign, &cx);
 	memcpy(SIGN_END(ctx), sign, sizeof sign);
-XXXAZ(smp_chk_sign(ctx));
 }
 
 /*--------------------------------------------------------------------
