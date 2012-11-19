10590c104c9cc2a03dc5e502f56bee162323d810
Poul-Henning Kamp <phk@FreeBSD.org>
2012-11-19 21:41:49 +0000

    Go over the generation of VSL records with some spit & polish

--- include/vapi/vsl_int.h
+++ include/vapi/vsl_int.h
@@ -46,18 +46,22 @@
  *
  * Each logrecord consist of:
  *	[n]		= ((type & 0xff) << 24) | (length & 0xffff)
- *	[n + 1]		= identifier
+ *	[n + 1]		= ((marker & 0x03) << 30) | (identifier & 0x3fffffff)
  *	[n + 2] ... [m]	= content
+ *
+ * Notice that the constants in these macros cannot be changed without
+ * changing corresponding magic numbers in varnishd/cache/cache_shmlog.c
  */
 
 #define VSL_CLIENTMARKER	(1U<<30)
 #define VSL_BACKENDMARKER	(1U<<31)
 #define VSL_IDENTMASK		(~(3U<<30))
 
+#define VSL_LENMASK		0xffff
 #define VSL_WORDS(len)		(((len) + 3) / 4)
 #define VSL_END(ptr, len)	((ptr) + 2 + VSL_WORDS(len))
 #define VSL_NEXT(ptr)		VSL_END(ptr, VSL_LEN(ptr))
-#define VSL_LEN(ptr)		((ptr)[0] & 0xffff)
+#define VSL_LEN(ptr)		((ptr)[0] & VSL_LENMASK)
 #define VSL_TAG(ptr)		((ptr)[0] >> 24)
 #define VSL_ID(ptr)		(((ptr)[1]) & VSL_IDENTMASK)
 #define VSL_CLIENT(ptr)		(((ptr)[1]) & VSL_CLIENTMARKER)
