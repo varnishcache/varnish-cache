343418dade50e6aa7035005d10532a88b99c7c85
Martin Blix Grydeland <martin@varnish-software.com>
2014-02-04 16:49:42 +0100

    Change varnishtop -> varnishhist

--- bin/varnishhist/varnishhist_options.h
+++ bin/varnishhist/varnishhist_options.h
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * Option definitions for varnishtop
+ * Option definitions for varnishhist
  */
 
 #include "vapi/vapi_options.h"
