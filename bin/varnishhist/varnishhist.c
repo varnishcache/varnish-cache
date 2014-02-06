c37045f9a1d5740a6c4900a77b8f240bc982b8b2
Guillaume Quintard <guillaume.quintard@gmail.com>
2014-02-06 17:37:21 +0100

    Keep only reqs and bereqs

--- bin/varnishhist/varnishhist.c
+++ bin/varnishhist/varnishhist.c
@@ -198,6 +198,8 @@ accumulate(struct VSL_data *vsl, struct VSL_transaction * const pt[],
 	for (tr = pt[0]; tr != NULL; tr = *++pt) {
 		value = -1;
 		hit = 0;
+		if (tr->type != VSL_t_bereq && tr->type != VSL_t_req)
+			continue;
 		while ((1 == VSL_Next(tr->c))) {
 			/* get the value we want, and register if it's a hit*/
 			tag = VSL_TAG(tr->c->rec.ptr);
