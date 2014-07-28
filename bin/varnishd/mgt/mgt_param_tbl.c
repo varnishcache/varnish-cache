63ab0363be2923f1d7984e4483ce0749422028d2
Poul-Henning Kamp <phk@FreeBSD.org>
2014-07-28 07:27:56 +0000

    Remove a couple of #ifdefs that serve no purpose anymore.

--- bin/varnishd/mgt/mgt_param_tbl.c
+++ bin/varnishd/mgt/mgt_param_tbl.c
@@ -534,21 +534,13 @@ struct parspec mgt_parspec[] = {
 		"Directory from which relative VCL filenames (vcl.load and "
 		"include) are opened.",
 		0,
-#ifdef VARNISH_VCL_DIR
 		VARNISH_VCL_DIR,
-#else
-		".",
-#endif
 		NULL },
 	{ "vmod_dir", tweak_string, &mgt_vmod_dir,
 		NULL, NULL,
 		"Directory where VCL modules are to be found.",
 		0,
-#ifdef VARNISH_VMOD_DIR
 		VARNISH_VMOD_DIR,
-#else
-		".",
-#endif
 		NULL },
 
 	{ "vcc_err_unref", tweak_bool, &mgt_vcc_err_unref,
