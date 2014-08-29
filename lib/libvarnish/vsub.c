varnishcache/varnish-cache

commit 33e6fe71743059b912d2b00cdbc08196b003e440

varnishd: streamline exit codes, fix exit codes for vsubs
This should bring us closer to the exit codes now documented in
varnishd.rst

Fixes #1572 for varnishd

lib/libvarnish/vsub.c
@@ -64,7 +64,8 @@ vsub_vlu(void *priv, const char *str)
 	return (0);
 }
 
-int
+/* returns an exit code */
+unsigned
 VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
     int maxlines)
 {
@@ -81,7 +82,7 @@ VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
 	if (pipe(p) < 0) {
 		VSB_printf(sb, "Starting %s: pipe() failed: %s",
 		    name, strerror(errno));
-		return (-1);
+		return (1);
 	}
 	assert(p[0] > STDERR_FILENO);
 	assert(p[1] > STDERR_FILENO);
@@ -90,7 +91,7 @@ VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
 		    name, strerror(errno));
 		AZ(close(p[0]));
 		AZ(close(p[1]));
-		return (-1);
+		return (1);
 	}
 	if (pid == 0) {
 		AZ(close(STDIN_FILENO));
@@ -101,7 +102,12 @@ VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
 		for (sfd = STDERR_FILENO + 1; sfd < 100; sfd++)
 			(void)close(sfd);
 		func(priv);
-		_exit(1);
+		/*
+		 * func should either exec or exit, so getting here should be
+		 * treated like an assertion failure - except that we don't know
+		 * if it's save to trigger an acutal assertion
+		 */
+		_exit(4);
 	}
 	AZ(close(p[1]));
 	vlu = VLU_New(&sp, vsub_vlu, 0);
@@ -117,19 +123,19 @@ VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
 		if (rv < 0 && errno != EINTR) {
 			VSB_printf(sb, "Running %s: waitpid() failed: %s\n",
 			    name, strerror(errno));
-			return (-1);
+			return (1);
 		}
 	} while (rv < 0);
 	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
 		VSB_printf(sb, "Running %s failed", name);
 		if (WIFEXITED(status))
-			VSB_printf(sb, ", exit %d", WEXITSTATUS(status));
+			VSB_printf(sb, ", exited with %d", WEXITSTATUS(status));
 		if (WIFSIGNALED(status))
 			VSB_printf(sb, ", signal %d", WTERMSIG(status));
 		if (WCOREDUMP(status))
 			VSB_printf(sb, ", core dumped");
 		VSB_printf(sb, "\n");
-		return (-1);
+		return (WEXITSTATUS(status));
 	}
 	return (0);
 }

bin/varnishd/mgt/mgt.h
@@ -117,7 +117,7 @@ void STV_Config_Transient(void);
 
 /* mgt_vcc.c */
 void mgt_vcc_init(void);
-int mgt_vcc_default(const char *bflag, const char *f_arg, char *vcl, int Cflag);
+unsigned mgt_vcc_default(const char *bflag, const char *f_arg, char *vcl, int Cflag);
 int mgt_push_vcls_and_start(unsigned *status, char **p);
 int mgt_has_vcl(void);
 extern char *mgt_cc_cmd;

bin/varnishd/mgt/mgt_child.c
@@ -425,7 +425,7 @@ mgt_launch_child(struct cli *cli)
 
 		child_main();
 
-		exit(1);
+		exit(0);
 	}
 	assert(pid > 1);
 	REPORT(LOG_NOTICE, "child (%jd) Started", (intmax_t)pid);
@@ -683,7 +683,7 @@ mgt_sigint(const struct vev *e, int what)
 	(void)fflush(stdout);
 	if (child_pid >= 0)
 		mgt_stop_child();
-	exit (2);
+	exit(0);
 }
 
 /*--------------------------------------------------------------------*/
@@ -753,6 +753,7 @@ MGT_Run(void)
 	else if (!d_flag) {
 		mgt_launch_child(NULL);
 		if (child_state == CH_STOPPED) {
+			// XXX correct? or 0?
 			exit_status = 2;
 			return;
 		}

bin/varnishd/mgt/mgt_cli.c
@@ -499,17 +499,17 @@ mgt_cli_secret(const char *S_arg)
 	fd = open(S_arg, O_RDONLY);
 	if (fd < 0) {
 		fprintf(stderr, "Can not open secret-file \"%s\"\n", S_arg);
-		exit (2);
+		exit(2);
 	}
 	mgt_got_fd(fd);
 	i = read(fd, buf, sizeof buf);
 	if (i == 0) {
 		fprintf(stderr, "Empty secret-file \"%s\"\n", S_arg);
-		exit (2);
+		exit(2);
 	}
 	if (i < 0) {
 		fprintf(stderr, "Can not read secret-file \"%s\"\n", S_arg);
-		exit (2);
+		exit(2);
 	}
 	AZ(close(fd));
 	secret_file = S_arg;
@@ -644,7 +644,7 @@ mgt_cli_master(const char *M_arg)
 	M_nta = VSS_resolve(M_arg, NULL, &M_ta);
 	if (M_nta <= 0) {
 		fprintf(stderr, "Could resolve -M argument to address\n");
-		exit (1);
+		exit(2);
 	}
 	M_nxt = 0;
 	AZ(M_poker);

bin/varnishd/mgt/mgt_main.c
@@ -199,7 +199,7 @@ cli_check(const struct cli *cli)
 	}
 	AZ(VSB_finish(cli->sb));
 	fprintf(stderr, "Error:\n%s\n", VSB_data(cli->sb));
-	exit (2);
+	exit(2);
 }
 
 /*--------------------------------------------------------------------
@@ -559,11 +559,11 @@ main(int argc, char * const *argv)
 		case 'x':
 			if (!strcmp(optarg, "dumprstparam")) {
 				MCF_DumpRstParam();
-				exit (0);
+				exit(0);
 			}
 			if (!strcmp(optarg, "dumprstvsl")) {
 				mgt_DumpRstVsl();
-				exit (0);
+				exit(0);
 			}
 			usage();
 			break;
@@ -651,11 +651,11 @@ main(int argc, char * const *argv)
 		    P_arg, strerror(errno));
 
 	if (b_arg != NULL || f_arg != NULL)
-		if (mgt_vcc_default(b_arg, f_arg, vcl, C_flag))
-			exit (2);
+		if ((o = mgt_vcc_default(b_arg, f_arg, vcl, C_flag)) != 0)
+			exit(o);
 
 	if (C_flag)
-		exit (0);
+		exit(0);
 
 	if (!d_flag) {
 		if (MGT_open_sockets())

bin/varnishd/mgt/mgt_param.c
@@ -175,7 +175,7 @@ mcf_wrap(struct cli *cli, const char *text)
 			if (r == NULL) {
 				fprintf(stderr,
 				    "LINE with just one TAB: <%s>\n", text);
-				exit(2);
+				exit(4);
 			}
 			if (r - q > tw)
 				tw = r - q;
@@ -385,11 +385,11 @@ MCF_AddParams(struct parspec *ps)
 		if (isspace(s[-1])) {
 			fprintf(stderr,
 			    "Param->descr has trailing space: %s\n", pp->name);
-			exit(2);
+			exit(4);
 		}
 		if (mcf_findpar(pp->name) != NULL) {
 			fprintf(stderr, "Duplicate param: %s\n", pp->name);
-			exit(2);
+			exit(4);
 		}
 		if (strlen(pp->name) + 1 > margin2)
 			margin2 = strlen(pp->name) + 1;

bin/varnishd/mgt/mgt_sandbox_solaris.c
@@ -236,7 +236,7 @@ mgt_sandbox_solaris_add_inheritable(priv_set_t *pset, enum sandbox_e who)
 		break;
 	default:
 		REPORT(LOG_ERR, "INCOMPLETE AT: %s(%d)\n", __func__, __LINE__);
-		exit(1);
+		exit(4);
 	}
 }
 
@@ -263,7 +263,7 @@ mgt_sandbox_solaris_add_effective(priv_set_t *pset, enum sandbox_e who)
 		break;
 	default:
 		REPORT(LOG_ERR, "INCOMPLETE AT: %s(%d)\n", __func__, __LINE__);
-		exit(1);
+		exit(4);
 	}
 }
 
@@ -286,7 +286,7 @@ mgt_sandbox_solaris_add_permitted(priv_set_t *pset, enum sandbox_e who)
 		break;
 	default:
 		REPORT(LOG_ERR, "INCOMPLETE AT: %s(%d)\n", __func__, __LINE__);
-		exit(1);
+		exit(4);
 	}
 }
 

bin/varnishd/mgt/mgt_shmem.c
@@ -100,7 +100,7 @@ vsm_n_check(void)
 	struct stat st;
 	pid_t pid;
 	struct VSM_head vsmh;
-	int retval = 2;
+	int retval = 1;
 
 	fd = open(VSM_FILENAME, O_RDWR, 0644);
 	if (fd < 0)
@@ -217,7 +217,7 @@ mgt_SHM_Create(void)
 
 	if (p == MAP_FAILED) {
 		fprintf(stderr, "Mmap error %s: %s\n", fnbuf, strerror(errno));
-		exit (-1);
+		exit(1);
 	}
 
 	mgt_vsm_p = p;
@@ -250,7 +250,7 @@ mgt_SHM_Create(void)
 		fprintf(stderr, "Rename failed %s -> %s: %s\n",
 		    fnbuf, VSM_FILENAME, strerror(errno));
 		(void)unlink(fnbuf);
-		exit (-1);
+		exit(1);
 	}
 
 #ifdef __OpenBSD__
@@ -325,7 +325,7 @@ mgt_SHM_Init(void)
 	/* Collision check with already running varnishd */
 	i = vsm_n_check();
 	if (i)
-		exit(i);
+		exit(2);
 
 	/* Create our static VSM instance */
 	static_vsm = VSM_common_new(static_vsm_buf, sizeof static_vsm_buf);

commit be4def865167e6d20fec7f67aa7cfa245928ea95

Return 2 from VSUB_run() when the sub process dies with a signal.
Fixes a regression from 33e6fe71743059b912d2b00cdbc08196b003e440

Fixes #1585

lib/libvarnish/vsub.c
@@ -127,15 +127,21 @@ VSUB_run(struct vsb *sb, vsub_func_f *func, void *priv, const char *name,
 		}
 	} while (rv < 0);
 	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+		rv = -1;
 		VSB_printf(sb, "Running %s failed", name);
-		if (WIFEXITED(status))
-			VSB_printf(sb, ", exited with %d", WEXITSTATUS(status));
-		if (WIFSIGNALED(status))
+		if (WIFEXITED(status)) {
+			rv = WEXITSTATUS(status);
+			VSB_printf(sb, ", exited with %d", rv);
+		}
+		if (WIFSIGNALED(status)) {
+			rv = 2;
 			VSB_printf(sb, ", signal %d", WTERMSIG(status));
+		}
 		if (WCOREDUMP(status))
 			VSB_printf(sb, ", core dumped");
 		VSB_printf(sb, "\n");
-		return (WEXITSTATUS(status));
+		assert(rv != -1);
+		return (rv);
 	}
 	return (0);
 }