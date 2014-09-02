varnishcache/varnish-cache

commit bb9d78797a8e163f533ccaea3686b5340a19987d

Don't add a trailing dash to URL if missing path.
Adding a dash if missing the path component does not make sense.

Related bug:	#1584

bin/varnishncsa/varnishncsa.c
@@ -300,7 +300,7 @@ format_requestline(const struct format *format)
 		AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_host], NULL));
 	} else
 		AZ(VSB_cat(CTX.vsb, "http://localhost"));
-	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_U], "-"));
+	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_U], NULL));
 	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_q], ""));
 	AZ(VSB_putc(CTX.vsb, ' '));
 	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_H], "HTTP/1.0"));

commit 3ee53259917b9aef37f4be08be273650b9a53968

Fix assertion error introduced in bb9d7879.

bin/varnishncsa/varnishncsa.c
@@ -300,7 +300,7 @@ format_requestline(const struct format *format)
 		AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_host], NULL));
 	} else
 		AZ(VSB_cat(CTX.vsb, "http://localhost"));
-	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_U], NULL));
+	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_U], ""));
 	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_q], ""));
 	AZ(VSB_putc(CTX.vsb, ' '));
 	AZ(vsb_fcat(CTX.vsb, &CTX.frag[F_H], "HTTP/1.0"));