132c0097e3564bc535beef411297b07d18288740
Poul-Henning Kamp <phk@FreeBSD.org>
2015-01-06 09:59:48 +0000

    Slim this table down to 80 char width like all other source files.

--- include/tbl/http_headers.h
+++ include/tbl/http_headers.h
@@ -29,72 +29,89 @@
  * Argument list:
  * ---------------------------------------
  * a	Http header name
- * b	session field name
+ * b	enum name
  * c	Supress header in filter ops
  *
  * see [RFC2616 13.5.1 End-to-end and Hop-by-hop Headers]
  *
  */
 
+
 /*lint -save -e525 -e539 */
 
 #ifndef HTTPH_R_PASS
-#define HTTPH_R_PASS	(1 << 0)	/* Request (c->b) in pass mode */
+#define HTTPH_R_PASS	(1 << 0   )	/* Request (c->b) in pass mode */
 #define HTTPH_R_FETCH	(1 << 1)	/* Request (c->b) for fetch */
 #define HTTPH_A_INS	(1 << 2)	/* Response (b->o) for insert */
 #endif
 
-HTTPH("Keep-Alive",		H_Keep_Alive,		HTTPH_R_PASS | HTTPH_R_FETCH		  )	/* RFC2068 */
-HTTPH("Accept",			H_Accept,		0					  )	/* RFC2616 14.1 */
-HTTPH("Accept-Charset",		H_Accept_Charset,	0					  )	/* RFC2616 14.2 */
-HTTPH("Accept-Encoding",	H_Accept_Encoding,	0					  )	/* RFC2616 14.3 */
-HTTPH("Accept-Language",	H_Accept_Language,	0					  )	/* RFC2616 14.4 */
-HTTPH("Accept-Ranges",		H_Accept_Ranges,		       HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.5 */
-HTTPH("Age",			H_Age,						       HTTPH_A_INS)	/* RFC2616 14.6 */
-HTTPH("Allow",			H_Allow,		0					  )	/* RFC2616 14.7 */
-HTTPH("Authorization",		H_Authorization,	0					  )	/* RFC2616 14.8 */
-HTTPH("Cache-Control",		H_Cache_Control,		       HTTPH_R_FETCH		  )	/* RFC2616 14.9 */
-HTTPH("Connection",		H_Connection,		HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.10 */
-HTTPH("Content-Encoding",	H_Content_Encoding,	0					  )	/* RFC2616 14.11 */
-HTTPH("Content-Language",	H_Content_Language,	0					  )	/* RFC2616 14.12 */
-HTTPH("Content-Length",		H_Content_Length,		       HTTPH_R_FETCH		  )	/* RFC2616 14.13 */
-HTTPH("Content-Location",	H_Content_Location,	0					  )	/* RFC2616 14.14 */
-HTTPH("Content-MD5",		H_Content_MD5,		0					  )	/* RFC2616 14.15 */
-HTTPH("Content-Range",		H_Content_Range,		       HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.16 */
-HTTPH("Content-Type",		H_Content_Type,		0					  )	/* RFC2616 14.17 */
-HTTPH("Cookie",			H_Cookie,		0					  )	/* RFC6265 4.2 */
-HTTPH("Date",			H_Date,			0					  )	/* RFC2616 14.18 */
-HTTPH("ETag",			H_ETag,			0					  )	/* RFC2616 14.19 */
-HTTPH("Expect",			H_Expect,		0					  )	/* RFC2616 14.20 */
-HTTPH("Expires",		H_Expires,		0					  )	/* RFC2616 14.21 */
-HTTPH("From",			H_From,			0					  )	/* RFC2616 14.22 */
-HTTPH("Host",			H_Host,			0					  )	/* RFC2616 14.23 */
-HTTPH("HTTP2-Settings",		H_HTTP2_Settings,	HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* draft-ietf-httpbis-http2-12.txt */
-HTTPH("If-Match",		H_If_Match,			       HTTPH_R_FETCH		  )	/* RFC2616 14.24 */
-HTTPH("If-Modified-Since",	H_If_Modified_Since,		       HTTPH_R_FETCH		  )	/* RFC2616 14.25 */
-HTTPH("If-None-Match",		H_If_None_Match,		       HTTPH_R_FETCH		  )	/* RFC2616 14.26 */
-HTTPH("If-Range",		H_If_Range,			       HTTPH_R_FETCH		  )	/* RFC2616 14.27 */
-HTTPH("If-Unmodified-Since",	H_If_Unmodifed_Since,		       HTTPH_R_FETCH		  )	/* RFC2616 14.28 */
-HTTPH("Last-Modified",		H_Last_Modified,	0					  )	/* RFC2616 14.29 */
-HTTPH("Location",		H_Location,		0					  )	/* RFC2616 14.30 */
-HTTPH("Max-Forwards",		H_Max_Forwards,		0					  )	/* RFC2616 14.31 */
-HTTPH("Pragma",			H_Pragma,		0					  )	/* RFC2616 14.32 */
-HTTPH("Proxy-Authenticate",	H_Proxy_Authenticate,		       HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.33 */
-HTTPH("Proxy-Authorization",	H_Proxy_Authorization,		       HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.34 */
-HTTPH("Range",			H_Range,			       HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.35 */
-HTTPH("Referer",		H_Referer,		0					  )	/* RFC2616 14.36 */
-HTTPH("Retry-After",		H_Retry_After,		0					  )	/* RFC2616 14.37 */
-HTTPH("Server",			H_Server,		0					  )	/* RFC2616 14.38 */
-HTTPH("Set-Cookie",		H_Set_Cookie,		0					  )	/* RFC6265 4.1 */
-HTTPH("TE",			H_TE,			HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.39 */
-HTTPH("Trailer",		H_Trailer,		HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.40 */
-HTTPH("Transfer-Encoding",	H_Transfer_Encoding,	HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.41 */
-HTTPH("Upgrade",		H_Upgrade,		HTTPH_R_PASS | HTTPH_R_FETCH | HTTPH_A_INS)	/* RFC2616 14.42 */
-HTTPH("User-Agent",		H_User_Agent,		0					  )	/* RFC2616 14.43 */
-HTTPH("Vary",			H_Vary,			0					  )	/* RFC2616 14.44 */
-HTTPH("Via",			H_Via,			0					  )	/* RFC2616 14.45 */
-HTTPH("Warning",		H_Warning,		0					  )	/* RFC2616 14.46 */
-HTTPH("WWW-Authenticate",	H_WWW_Authenticate,	0					  )	/* RFC2616 14.47 */
-HTTPH("X-Forwarded-For",	H_X_Forwarded_For,	0					  )	/* Not RFC */
+/* Shorthand for this file only, to keep table narrow */
+
+#if defined(P) || defined(F) || defined(I) || defined(H)
+#error "Macro overloading"  // Trust but verify
+#endif
+
+#define P HTTPH_R_PASS
+#define F HTTPH_R_FETCH
+#define I HTTPH_A_INS
+#define H(s,e,f) HTTPH(s, e, f)
+
+H("Keep-Alive",		H_Keep_Alive,		P|F  )	// 2068
+H("Accept",		H_Accept,		0    )	// 2616 14.1
+H("Accept-Charset",	H_Accept_Charset,	0    )	// 2616 14.2
+H("Accept-Encoding",	H_Accept_Encoding,	0    )	// 2616 14.3
+H("Accept-Language",	H_Accept_Language,	0    )	// 2616 14.4
+H("Accept-Ranges",	H_Accept_Ranges,	  F|I)	// 2616 14.5
+H("Age",		H_Age,			    I)	// 2616 14.6
+H("Allow",		H_Allow,		0    )	// 2616 14.7
+H("Authorization",	H_Authorization,	0    )	// 2616 14.8
+H("Cache-Control",	H_Cache_Control,	  F  )	// 2616 14.9
+H("Connection",		H_Connection,		P|F|I)	// 2616 14.10
+H("Content-Encoding",	H_Content_Encoding,	0    )	// 2616 14.11
+H("Content-Language",	H_Content_Language,	0    )	// 2616 14.12
+H("Content-Length",	H_Content_Length,	  F  )	// 2616 14.13
+H("Content-Location",	H_Content_Location,	0    )	// 2616 14.14
+H("Content-MD5",	H_Content_MD5,		0    )	// 2616 14.15
+H("Content-Range",	H_Content_Range,	  F|I)	// 2616 14.16
+H("Content-Type",	H_Content_Type,		0    )	// 2616 14.17
+H("Cookie",		H_Cookie,		0    )	// 6265 4.2
+H("Date",		H_Date,			0    )	// 2616 14.18
+H("ETag",		H_ETag,			0    )	// 2616 14.19
+H("Expect",		H_Expect,		0    )	// 2616 14.20
+H("Expires",		H_Expires,		0    )	// 2616 14.21
+H("From",		H_From,			0    )	// 2616 14.22
+H("Host",		H_Host,			0    )	// 2616 14.23
+H("HTTP2-Settings",	H_HTTP2_Settings,	P|F|I)	// httpbis-http2-16.txt
+H("If-Match",		H_If_Match,		  F  )	// 2616 14.24
+H("If-Modified-Since",	H_If_Modified_Since,	  F  )	// 2616 14.25
+H("If-None-Match",	H_If_None_Match,	  F  )	// 2616 14.26
+H("If-Range",		H_If_Range,		  F  )	// 2616 14.27
+H("If-Unmodified-Since",H_If_Unmodifed_Since,	  F  )	// 2616 14.28
+H("Last-Modified",	H_Last_Modified,	0    )	// 2616 14.29
+H("Location",		H_Location,		0    )	// 2616 14.30
+H("Max-Forwards",	H_Max_Forwards,		0    )	// 2616 14.31
+H("Pragma",		H_Pragma,		0    )	// 2616 14.32
+H("Proxy-Authenticate",	H_Proxy_Authenticate,	  F|I)	// 2616 14.33
+H("Proxy-Authorization",H_Proxy_Authorization,	  F|I)	// 2616 14.34
+H("Range",		H_Range,		  F|I)	// 2616 14.35
+H("Referer",		H_Referer,		0    )	// 2616 14.36
+H("Retry-After",	H_Retry_After,		0    )	// 2616 14.37
+H("Server",		H_Server,		0    )	// 2616 14.38
+H("Set-Cookie",		H_Set_Cookie,		0    )	// 6265 4.1
+H("TE",			H_TE,			P|F|I)	// 2616 14.39
+H("Trailer",		H_Trailer,		P|F|I)	// 2616 14.40
+H("Transfer-Encoding",	H_Transfer_Encoding,	P|F|I)	// 2616 14.41
+H("Upgrade",		H_Upgrade,		P|F|I)	// 2616 14.42
+H("User-Agent",		H_User_Agent,		0    )	// 2616 14.43
+H("Vary",		H_Vary,			0    )	// 2616 14.44
+H("Via",		H_Via,			0    )	// 2616 14.45
+H("Warning",		H_Warning,		0    )	// 2616 14.46
+H("WWW-Authenticate",	H_WWW_Authenticate,	0    )	// 2616 14.47
+H("X-Forwarded-For",	H_X_Forwarded_For,	0    )	// No RFC
+
+#undef P
+#undef F
+#undef I
+#undef H
 
 /*lint -restore */
