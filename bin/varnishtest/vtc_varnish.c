06faac1b74d2f96a621439d4b062b5f1381abfbc
Poul-Henning Kamp <phk@FreeBSD.org>
2015-01-22 23:21:34 +0000

    Define a debug bit to mark that we are running under varnishtest control and always set it in varnishtest

--- bin/varnishtest/vtc_varnish.c
+++ bin/varnishtest/vtc_varnish.c
@@ -390,6 +390,7 @@ varnish_launch(struct varnish *v)
 	VSB_printf(vsb, " -p syslog_cli_traffic=off");
 	VSB_printf(vsb, " -p sigsegv_handler=on");
 	VSB_printf(vsb, " -p thread_pool_min=10");
+	VSB_printf(vsb, " -p debug=+vtc_mode");
 	VSB_printf(vsb, " -a '%s'", "127.0.0.1:0");
 	VSB_printf(vsb, " -M '%s %s'", abuf, pbuf);
 	VSB_printf(vsb, " -P %s/varnishd.pid", v->workdir);
