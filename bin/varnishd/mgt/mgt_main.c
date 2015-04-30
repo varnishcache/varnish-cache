varnishcache/varnish-cache

commit ac69df7db6ea97dc244df357e14013e720fe9801

varnishd -C on a bad vcl should exit with status 2 as it does without -C

bin/varnishd/mgt/mgt_main.c
@@ -644,6 +644,7 @@ main(int argc, char * const *argv)
 	if (b_arg != NULL || f_arg != NULL) {
 		mgt_vcc_default(cli, b_arg, vcl, C_flag);
 		if (C_flag) {
+			cli_check(cli);
 			AZ(VSB_finish(cli->sb));
 			fprintf(stderr, "%s\n", VSB_data(cli->sb));
 			exit(0);

bin/varnishd/mgt/mgt_vcc.c
@@ -327,7 +327,7 @@ mgt_VccCompile(struct cli *cli, const char *vclname, const char *vclsrc,
 	if (status || C_flag) {
 		(void)unlink(vp.libfile);
 		free(vp.libfile);
-		if (!C_flag) {
+		if (status) {
 			VCLI_Out(cli, "VCL compilation failed");
 			VCLI_SetResult(cli, CLIS_PARAM);
 		}

bin/varnishtest/tests/a00009.vtc
@@ -3,6 +3,8 @@ varnishtest "Code coverage of VCL compiler and RSTdump etc"
 shell "${varnishd} -b 127.0.0.1:80 -C -n ${tmpdir} > /dev/null 2>&1"
 shell "${varnishd} -x dumprstparam > /dev/null 2>&1"
 shell "${varnishd} -x dumprstvsl > /dev/null 2>&1"
+err_shell {VCL version declaration missing} "test -d  ${tmpdir} || mkdir ${tmpdir} ; echo 'bad vcl' >${tmpdir}/t.vcl ; ${varnishd} -f ${tmpdir}/t.vcl -n ${tmpdir} 2>&1"
+err_shell {VCL version declaration missing} "test -d  ${tmpdir} || mkdir ${tmpdir} ; echo 'bad vcl' >${tmpdir}/t.vcl ; ${varnishd} -C -f ${tmpdir}/t.vcl -n ${tmpdir} 2>&1"
 err_shell {-spersistent has been deprecated} "${varnishd} -spersistent 2>&1"
 err_shell {Unknown jail method "xyz"} "${varnishd} -jxyz 2>&1"
 err_shell {-j must be the first argument} "${varnishd} -jnone -jxyz 2>&1"

commit f9909907254d07fa72a842f985cac29d4a28fe95

Fix varnishd -C
Broken in ac69df7db6ea97dc244df357e14013e720fe9801.

bin/varnishd/mgt/mgt_main.c
@@ -674,8 +674,7 @@ main(int argc, char * const *argv)
 
 	if (b_arg != NULL || f_arg != NULL) {
 		mgt_vcc_default(cli, b_arg, vcl, C_flag);
-		if (C_flag) {
-			cli_check(cli);
+		if (C_flag && cli->result == CLIS_OK) {
 			AZ(VSB_finish(cli->sb));
 			fprintf(stderr, "%s\n", VSB_data(cli->sb));
 			exit(0);