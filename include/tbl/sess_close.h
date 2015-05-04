24f88f89f8dcb3394085bff2344e4da64d5681ab
Poul-Henning Kamp <phk@FreeBSD.org>
2015-05-04 21:05:52 +0000

    Add a session close value for clients asking for bogus ranges on streaming objects.

--- include/tbl/sess_close.h
+++ include/tbl/sess_close.h
@@ -44,5 +44,6 @@ SESS_CLOSE(TX_EOF,	  tx_eof,	0,	"EOF transmission")
 SESS_CLOSE(RESP_CLOSE,	  resp_close,	0,	"Backend/VCL requested close")
 SESS_CLOSE(OVERLOAD,	  overload,	1,	"Out of some resource")
 SESS_CLOSE(PIPE_OVERFLOW, pipe_overflow,1,	"Session pipe overflow")
+SESS_CLOSE(RANGE_SHORT,   range_short,  1,	"Insufficient data for range")
 
 /*lint -restore */
