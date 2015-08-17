0dffa87db4bf9d449781d244a00745a23a02803d
Poul-Henning Kamp <phk@FreeBSD.org>
2015-08-17 09:25:19 +0000

    Fix loading of cold VCLs
    Fixes:	#1775

--- bin/varnishd/cache/cache_vcl.c
+++ bin/varnishd/cache/cache_vcl.c
@@ -398,6 +398,10 @@ vcl_set_state(struct vcl *vcl, const char *state)
 	case '0':
 		if (vcl->temp == vcl_temp_cold)
 			break;
+		if (vcl->temp == vcl_temp_init) {
+			vcl->temp = vcl_temp_cold;
+			break;
+		}
 		if (vcl->busy == 0) {
 			vcl->temp = vcl_temp_cold;
 			(void)vcl->conf->event_vcl(&ctx, VCL_EVENT_COLD);
