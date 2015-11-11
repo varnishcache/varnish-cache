383c4497c050d496c33cc6e03754000a6cc74254
Federico G. Schwindt <fgsch@lodoss.net>
2015-11-11 13:33:07 +0000

    Move the pcre jit check to configure
    Output whether pcre jit is usable at configure time.

--- lib/libvarnish/vre.c
+++ lib/libvarnish/vre.c
@@ -36,7 +36,7 @@
 
 #include "vre.h"
 
-#if defined(USE_PCRE_JIT) && PCRE_MAJOR == 8 && PCRE_MINOR >= 32
+#if defined(USE_PCRE_JIT)
 #define VRE_STUDY_JIT_COMPILE PCRE_STUDY_JIT_COMPILE
 #else
 #define VRE_STUDY_JIT_COMPILE 0
