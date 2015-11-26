982e79671d09d3a0661b8ba7e17c1108ca2eb42f
Federico G. Schwindt <fgsch@lodoss.net>
2015-11-26 16:06:04 +0000

    Let other OS set the thread name as well
    Amends 83aa5402 to handle FreeBSD and OpenBSD (at least).

--- bin/varnishd/cache/cache_main.c
+++ bin/varnishd/cache/cache_main.c
@@ -92,7 +92,9 @@ THR_SetName(const char *name)
 {
 
 	AZ(pthread_setspecific(name_key, name));
-#ifdef HAVE_PTHREAD_SETNAME_NP
+#if defined(HAVE_PTHREAD_SET_NAME_NP)
+	pthread_set_name_np(pthread_self(), name);
+#elif defined(HAVE_PTHREAD_SETNAME_NP)
 	pthread_setname_np(pthread_self(), name);
 #endif
 }
