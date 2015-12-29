538f8bb8be96c341935242486f45a2a8ec51fb00
Federico G. Schwindt <fgsch@lodoss.net>
2015-12-29 01:35:21 +0000

    vdef.h is not generated

--- lib/libvcc/generate.py
+++ lib/libvcc/generate.py
@@ -1228,7 +1228,7 @@ vcl_output_lang_h(struct vsb *sb)
 {
 """)
 
-emit_file(fo, buildroot, "include/vdef.h")
+emit_file(fo, srcroot, "include/vdef.h")
 emit_file(fo, buildroot, "include/vcl.h")
 emit_file(fo, srcroot, "include/vrt.h")
 emit_file(fo, buildroot, "include/vrt_obj.h")
