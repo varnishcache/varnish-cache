6694bea3b2c0c4e6ef0cc723f03e07b1d9731502
Federico G. Schwindt <fgsch@lodoss.net>
2016-02-01 18:16:22 +0000

    Cast time_t to work with 32 bit time_t
    Fixes #1851.

--- bin/varnishstat/varnishstat_curses.c
+++ bin/varnishstat/varnishstat_curses.c
@@ -540,8 +540,9 @@ static void
 print_duration(WINDOW *w, time_t t)
 {
 
-	wprintw(w, "%4lu+%02lu:%02lu:%02lu",
-	    t / 86400, (t % 86400) / 3600, (t % 3600) / 60, t % 60);
+	wprintw(w, "%4jd+%02jd:%02jd:%02jd",
+	    (intmax_t)t / 86400, (intmax_t)(t % 86400) / 3600,
+	    (intmax_t)(t % 3600) / 60, (intmax_t)t % 60);
 }
 
 static void
