9f156af54b90167ade015651292eff95feb0a4b0
Nils Goroll <nils.goroll@uplex.de>
2016-02-17 14:17:42 +0100

    VFP_Push: document the place which still need attention for out-of-workspace handling
    We'll need to change a couple of signatures in the call paths getting here
    to propagate up the failure.

--- bin/varnishd/http1/cache_http1_vfp.c
+++ bin/varnishd/http1/cache_http1_vfp.c
@@ -272,16 +272,19 @@ V1F_Setup_Fetch(struct vfp_ctx *vfc, struct http_conn *htc)
 	case BS_EOF:
 		assert(htc->content_length == -1);
 		vfe = VFP_Push(vfc, &v1f_eof, 0);
+		XXXAN(vfe);
 		vfe->priv2 = 0;
 		break;
 	case BS_LENGTH:
 		assert(htc->content_length > 0);
 		vfe = VFP_Push(vfc, &v1f_straight, 0);
+		XXXAN(vfe);
 		vfe->priv2 = htc->content_length;
 		break;
 	case BS_CHUNKED:
 		assert(htc->content_length == -1);
 		vfe = VFP_Push(vfc, &v1f_chunked, 0);
+		XXXAN(vfe);
 		vfe->priv2 = -1;
 		break;
 	default:
