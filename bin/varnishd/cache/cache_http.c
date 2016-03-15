a2b1d039475e0b2298354f9ae63958e26c0da45b
Pål Hermunn Johansen <hermunn@varnish-software.com>
2016-03-15 14:53:16 +0100

    Proper handling of duplicate headers on IMS headers merge
    This fixes a problem when a backend replies with 304 Not Modified
    (after a http conditional request from varnish) and does not supply a
    header that was duplicate in the cached object.
    
    Before this patch, varnish would only supply (by copying from the
    expired object) the first instance of a duplicate header.

--- bin/varnishd/cache/cache_http.c
+++ bin/varnishd/cache/cache_http.c
@@ -992,6 +992,8 @@ HTTP_Merge(struct worker *wrk, struct objcore *oc, struct http *to)
 	const char *ptr;
 	unsigned u;
 	const char *p;
+	unsigned nhd_before_merge;
+	unsigned v;
 
 	CHECK_OBJ_NOTNULL(wrk, WORKER_MAGIC);
 	CHECK_OBJ_NOTNULL(oc, OBJCORE_MAGIC);
@@ -1009,10 +1011,12 @@ HTTP_Merge(struct worker *wrk, struct objcore *oc, struct http *to)
 		http_SetH(to, u, ptr);
 		ptr = strchr(ptr, '\0') + 1;
 	}
+	nhd_before_merge = to->nhd;
 	while (*ptr != '\0') {
 		p = strchr(ptr, ':');
 		AN(p);
-		if (!http_findhdr(to, p - ptr, ptr))
+		v = http_findhdr(to, p - ptr, ptr);
+		if (v == 0 || v >= nhd_before_merge)
 			http_SetHeader(to, ptr);
 		ptr = strchr(ptr, '\0') + 1;
 	}
