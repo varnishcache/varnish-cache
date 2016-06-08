3a263d940cf24d36a85cb835291c3062677dd87b
Guillaume Quintard <guillaume@varnish-software.com>
2016-06-08 12:02:39 +0200

    use Python instead of awk

---
+++ doc/sphinx/vtc-syntax.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#-
+# Copyright (c) 2006-2016 Varnish Software AS
+# All rights reserved.
+#
+# Author: Guillaume Quintard <guillaume.quintard@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# Generate various .c and .h files for the VSL query expression parser
+# and the interfaces for it.
+
+from __future__ import print_function
+import sys;
+import re;
+
+def parse_file(fn):
+	p = False
+	section = ""
+	resec = re.compile("[ /]\* SECTION: ")
+	cl = {}
+	tl = {}
+	sl = []
+
+	f = open(fn, 'r')
+
+	for l in f:
+		if "*/" in l:
+			p = 0
+		if resec.match(l):
+			a = l.split()
+			section = a[2]
+			sl.append(section)
+			cl[section] = []
+			if len(a) > 3:
+				tl[section] = re.sub(r'^[\t ]*\/?\* SECTION: [^ ]+ +',
+						"", l)
+			else:
+				tl[section] = ""
+			p = 1
+		elif p:
+			cl[section].append(re.sub(r'^ \* ?',"", l))
+	f.close()
+
+	sl.sort()
+	for section in sl:
+		print(tl[section], end='');
+		a = section
+		c = section.count('.')
+		if c == 0:
+			r = "-"
+		elif c ==1: 
+			r = "~"
+		else:
+			r = "."
+		print(re.sub(r'.', r, tl[section]), end='')
+		print("".join(cl[section]))
+
+if __name__ == "__main__":
+	for fn in sys.argv[1:]:
+		parse_file(fn)
