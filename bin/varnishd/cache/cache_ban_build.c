varnishcache/varnish-cache

commit 984f145f8d12b68583972bca73d198e08238e011

Align ban lumps
should fix #2050

bin/varnishd/cache/cache_ban_build.c
@@ -101,7 +101,7 @@ BAN_Abandon(struct ban_proto *bp)
 static void
 ban_add_lump(const struct ban_proto *bp, const void *p, uint32_t len)
 {
-	uint8_t buf[sizeof len];
+	uint8_t buf[PRNDUP(sizeof len)];
 
 	buf[0] = 0xff;
 	while (VSB_len(bp->vsb) & PALGN)

bin/varnishd/cache/cache_ban.c
@@ -183,7 +183,8 @@ ban_get_lump(const uint8_t **bs)
 	while (**bs == 0xff)
 		*bs += 1;
 	ln = vbe32dec(*bs);
-	*bs += 4;
+	*bs += PRNDUP(sizeof(uint32_t));
+	assert(PAOK(*bs));
 	r = (const void*)*bs;
 	*bs += ln;
 	return (r);

commit 345eb517ea015c7905f6821b8d669bbbe46b1692

Init the buffer so padding bytes for alignment are always the same
Otherwise ban_equal may not work due to undefined contents of buf

Should fix c19.vtc regression from 984f145f8d12b68583972bca73d198e08238e011

bin/varnishd/cache/cache_ban_build.c
@@ -101,9 +101,8 @@ BAN_Abandon(struct ban_proto *bp)
 static void
 ban_add_lump(const struct ban_proto *bp, const void *p, uint32_t len)
 {
-	uint8_t buf[PRNDUP(sizeof len)];
+	uint8_t buf[PRNDUP(sizeof len)] = { 0xff };
 
-	buf[0] = 0xff;
 	while (VSB_len(bp->vsb) & PALGN)
 		VSB_putc(bp->vsb, buf[0]);
 	vbe32enc(buf, len);