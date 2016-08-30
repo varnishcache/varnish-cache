fb58772dde17ce0a3fc0f1b8059ccf3a39c070ca
Poul-Henning Kamp <phk@FreeBSD.org>
2016-08-30 09:01:31 +0000

    Tag the request with the transport up front

--- bin/varnishd/cache/cache_session.c
+++ bin/varnishd/cache/cache_session.c
@@ -62,6 +62,7 @@ SES_SetTransport(struct worker *wrk, struct sess *sp, struct req *req,
 	assert(xp->number > 0);
 
 	sp->sattr[SA_TRANSPORT] = xp->number;
+	req->transport = xp;
 	wrk->task.func = xp->new_session;
 	wrk->task.priv = req;
 }
