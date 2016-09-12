bd5ade3e542217e99062a4543cd66bd82586d6e4
Nils Goroll <nils.goroll@uplex.de>
2016-09-12 20:34:18 +0200

    Revert "failing crc32 checks on macos + clang - casting the culprit?"
    This reverts commit 4a5ec60af1ff1a0ec81b7c255f5d9ca01299b1be.

--- lib/libvmod_directors/crc32.h
+++ lib/libvmod_directors/crc32.h
@@ -1,3 +1,3 @@
 #include <stdint.h>
-uint32_t crc32(uint32_t crc, const unsigned char *, unsigned);
-uint32_t crc32_l(const unsigned char *, unsigned);
+uint32_t crc32(uint32_t crc, const void *p1, unsigned l);
+uint32_t crc32_l(const void *p1, unsigned l);
