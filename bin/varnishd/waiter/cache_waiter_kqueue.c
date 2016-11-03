ae3f4dca19000a3bdcda9d39886f5a70ad7a38df
Nils Goroll <nils.goroll@uplex.de>
2016-11-03 12:11:54 +0100

    Check if events we received are actually being waited for
    For epoll, we tolerate spurious reports, for all other waiters
    we assert.
    
    fixes #2117

--- bin/varnishd/waiter/cache_waiter_kqueue.c
+++ bin/varnishd/waiter/cache_waiter_kqueue.c
@@ -98,7 +98,7 @@ vwk_thread(void *priv)
 			CHECK_OBJ_NOTNULL(wp, WAITED_MAGIC);
 			EV_SET(ke, wp->fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
 			AZ(kevent(vwk->kq, ke, 1, NULL, 0, NULL));
-			Wait_HeapDelete(w, wp);
+			AN(Wait_HeapDelete(w, wp));
 			Lck_Unlock(&vwk->mtx);
 			Wait_Call(w, wp, WAITER_TIMEOUT, now);
 		}
@@ -118,7 +118,7 @@ vwk_thread(void *priv)
 			}
 			CAST_OBJ_NOTNULL(wp, ke[j].udata, WAITED_MAGIC);
 			Lck_Lock(&vwk->mtx);
-			Wait_HeapDelete(w, wp);
+			AN(Wait_HeapDelete(w, wp));
 			Lck_Unlock(&vwk->mtx);
 			vwk->nwaited--;
 			if (kp->flags & EV_EOF)
