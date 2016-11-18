346d543be36ad1b6ec382857b6b06367b3eaf2d5
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2016-11-18 15:56:02 +0100

    Whitespace OCD

--- tools/vtest.sh
+++ tools/vtest.sh
@@ -4,7 +4,7 @@
 # All rights reserved.
 #
 # Author: Poul-Henning Kamp <phk@phk.freebsd.dk>
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
@@ -13,7 +13,7 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -53,7 +53,7 @@ if [ ! -d varnish-cache ] ; then
 fi
 
 if [ ! -f vt_key.pub ] ; then
-	ssh-keygen -t ed25519 -N "" -f vt_key 
+	ssh-keygen -t ed25519 -N "" -f vt_key
 fi
 
 autogen () (
@@ -136,7 +136,7 @@ do
 	rm -rf _report
 	mkdir _report
 	export LOG=_report/_log
-		
+
 	echo "VTEST 1.01" > ${LOG}
 	echo "DATE `date +%s`" >> ${LOG}
 	echo "BRANCH trunk" >> ${LOG}
