22011e03c5f4b8f895094283ce00ad778f68cc1a
Nils Goroll <nils.goroll@uplex.de>
2017-02-16 11:10:14 +0100

    check errno for failed write

--- bin/varnishd/http1/cache_http1_fsm.c
+++ bin/varnishd/http1/cache_http1_fsm.c
@@ -228,8 +228,10 @@ http1_minimal_response(struct req *req, uint16_t status)
 
 	if (wl > 0)
 		req->acct.resp_hdrbytes += wl;
-	if (! req->doclose && wl != l) {
-		req->doclose = SC_REM_CLOSE;
+	if (wl != l) {
+		VTCP_Assert(1);
+		if (! req->doclose)
+			req->doclose = SC_REM_CLOSE;
 		return (-1);
 	}
 	return (0);
