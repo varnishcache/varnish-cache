f8ea87cb22347ca00fda5d0fcf5b82ed8ff1b2e8
Poul-Henning Kamp <phk@FreeBSD.org>
2017-02-20 23:08:07 +0000

    Forgot this in previous commit.

--- include/tbl/debug_bits.h
+++ include/tbl/debug_bits.h
@@ -44,6 +44,7 @@ DEBUG_BIT(FLUSH_HEAD,		flush_head,	"Flush after http1 head")
 DEBUG_BIT(VTC_MODE,		vtc_mode,	"Varnishtest Mode")
 DEBUG_BIT(WITNESS,		witness,	"Emit WITNESS lock records")
 DEBUG_BIT(VSM_KEEP,		vsm_keep,	"Keep the VSM file on restart")
+DEBUG_BIT(DROP_POOLS,		drop_pools,	"Drop thread pools (testing)")
 #undef DEBUG_BIT
 
 /*lint -restore */
