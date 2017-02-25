987d090a2bcd9961f8085baeae2416c1d8c71e34
Poul-Henning Kamp <phk@FreeBSD.org>
2017-02-25 21:55:36 +0000

    Ignore upper/lower case when comparing header names

--- bin/varnishtest/vtc_http2.c
+++ bin/varnishtest/vtc_http2.c
@@ -884,7 +884,7 @@ find_header(const struct hpk_hdr *h, const char *k)
 
 	int kl = strlen(k);
 	while (h->t) {
-		if (kl == h->key.len  && !memcmp(h->key.ptr, k, kl))
+		if (kl == h->key.len  && !strncasecmp(h->key.ptr, k, kl))
 			return h->value.ptr;
 		h++;
 	}
