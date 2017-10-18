9e5dfacd072302e10ed3b7462226b679074431b0
Dag Haavi Finstad <daghf@varnish-software.com>
2017-10-18 13:07:45 +0200

    whitespace

--- bin/varnishd/http2/cache_http2_proto.c
+++ bin/varnishd/http2/cache_http2_proto.c
@@ -838,7 +838,6 @@ h2_procframe(struct worker *wrk, struct h2_sess *h2,
 	    !(r2 && h2->new_req == r2->req && h2f == H2_F_CONTINUATION))
 		return (H2CE_PROTOCOL_ERROR);	// rfc7540,l,1859,1863
 
-
 	h2e = h2f->rxfunc(wrk, h2, r2);
 	if (h2e == 0)
 		return (0);
