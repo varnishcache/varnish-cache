2694906c91434340dc65a69742af9a1d7a9c1356
Nils Goroll <nils.goroll@uplex.de>
2018-04-27 16:39:06 +0200

    vmodtool: require method names to start with a dot

--- lib/libvcc/vmodtool.py
+++ lib/libvcc/vmodtool.py
@@ -799,6 +799,9 @@ class s_method(stanza):
         assert type(p) == s_object
         self.pfx = p.proto.name
         self.proto = prototype(self, prefix=self.pfx)
+        if not self.proto.bname.startswith("."):
+            err("$Method %s: Method names need to start with . (dot)"
+                % self.proto.bname, warn=False)
         self.proto.obj = "x" + self.pfx
         self.rstlbl = "func_" + self.proto.name
         p.methods.append(self)
