caa7a4e07ef6c820d7e539312b1a09812066304b
Nils Goroll <nils.goroll@uplex.de>
2018-09-03 18:05:09 +0200

    fix comment

--- include/tbl/boc_state.h
+++ include/tbl/boc_state.h
@@ -29,7 +29,7 @@
 /*lint -save -e525 -e539 */
 
 BOC_STATE(INVALID,	invalid)	/* don't touch (yet) */
-BOC_STATE(REQ_DONE,	req_done)	/* beresp.* can be examined */
+BOC_STATE(REQ_DONE,	req_done)	/* bereq.* can be examined */
 BOC_STATE(PREP_STREAM,	prep_stream)	/* Prepare for streaming */
 BOC_STATE(STREAM,	stream)		/* beresp.* can be examined */
 BOC_STATE(FINISHED,	finished)	/* object is complete */
