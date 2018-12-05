2dc0afd17f1e123513c83abbe36d9c9f63df083a
Nils Goroll <nils.goroll@uplex.de>
2018-12-05 11:40:13 +0100

    make two identical comparisons the same
    see 11 lines down
    
    Conflicts:
    	bin/varnishd/cache/cache_hash.c

--- bin/varnishd/cache/cache_hash.c
+++ bin/varnishd/cache/cache_hash.c
@@ -501,9 +501,8 @@ HSH_Lookup(struct req *req, struct objcore **ocp, struct objcore **bocp)
 		if (exp_oc != NULL) {
 			exp_oc->refcnt++;
 			*ocp = exp_oc;
-			if (EXP_Ttl_grace(req, exp_oc) > req->t_req) {
-				if (exp_oc->hits < LONG_MAX)
-					exp_oc->hits++;
+			if (EXP_Ttl_grace(req, exp_oc) >= req->t_req) {
+				exp_oc->hits++;
 				Lck_Unlock(&oh->mtx);
 				return (HSH_GRACE);
 			}
