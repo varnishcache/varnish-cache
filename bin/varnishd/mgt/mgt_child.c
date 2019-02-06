varnishcache/varnish-cache

commit ca76ef579f34394078ce281955b835958d3d8dfe

fix the missing high fd tracking

bin/varnishd/mgt/mgt_child.c
@@ -191,6 +191,8 @@ mch_cli_panic_clear(struct cli *cli, const char * const *av, void *priv)
 static int		mgt_max_fd;
 
 #define CLOSE_FD_UP_TO	(mgt_max_fd + 100)
+// XXX should work now - engage?
+//#define CLOSE_FD_UP_TO	mgt_max_fd
 #define CHECK_FD_UP_TO	(CLOSE_FD_UP_TO + 100)
 
 void
@@ -215,6 +217,7 @@ MCH_Fd_Inherit(int fd, const char *what)
 {
 
 	assert(fd >= 0);
+	MCH_TrackHighFd(fd);
 	if (fd_map == NULL)
 		fd_map = vbit_new(128);
 	AN(fd_map);

commit 1524d95d2f4f8409d7b6507fb93f46b3b4ba46c6

Fix regression from 6bead571da6a2027a518af81e14d87a247f38156
Seen once, and failed to reprocuce (yet), needs to be understood
better

from vmod_blobdigest tests/usage.vtc:

**** v1    1.8 CLI RX|No panic to clear
***  v1    1.8 debug|Info: manager stopping child
***  v1    1.8 debug|Debug: Stopping Child
**** v1    1.9 vsl|          0 CLI             - EOF on CLI connection, worker stops
***  v1    2.8 debug|Info: Child (174227) ended
***  v1    2.8 debug|Info: Child (174227) said Child dies
***  v1    2.8 debug|Debug: Child cleanup complete
***  v1    2.8 debug|Assert error in MCH_TrackHighFd(), mgt/mgt_child.c line 203:
***  v1    2.8 debug|  Condition(fd > 0) not true.
**** v1    2.9 STDOUT poll 0x10
**   v1    2.9 WAIT4 pid=174215 status=0x0086 (user 0.914949 sys 0.567445)
*    v1    2.9 Expected exit: 0x0 signal: 0 core: 0
---- v1    2.9 Bad exit status: 0x0086 exit 0x0 signal 6 core 128
*    top   2.9 failure during reset

On the 6.0 branch the regression appeared in ca76ef579 instead.

bin/varnishd/mgt/mgt_child.c
@@ -217,7 +217,9 @@ MCH_Fd_Inherit(int fd, const char *what)
 {
 
 	assert(fd >= 0);
-	MCH_TrackHighFd(fd);
+	// XXX why?
+	if (fd > 0)
+		MCH_TrackHighFd(fd);
 	if (fd_map == NULL)
 		fd_map = vbit_new(128);
 	AN(fd_map);