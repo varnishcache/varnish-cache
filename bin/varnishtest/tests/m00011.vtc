varnishcache/varnish-cache

commit d7a81fe82ca09c9f3b3a9fd67265de00da7a4315

Introduce a VSS_ResolveOne() function and use it the places where we want a single unique suckaddr.

lib/libvmod_std/vmod_std_conversions.c
@@ -40,6 +40,7 @@
 
 #include "vnum.h"
 #include "vsa.h"
+#include "vss.h"
 #include "vtim.h"
 #include "vcc_if.h"
 
@@ -189,34 +190,6 @@ vmod_integer(VRT_CTX, struct VARGS(integer) *a)
 	return (0);
 }
 
-static VCL_IP
-lookup(VCL_IP *p, const char *s, int resolve)
-{
-	struct addrinfo hints, *res0 = NULL;
-	const struct addrinfo *res;
-	int error;
-	VCL_IP retval = NULL;
-
-	if (s == NULL)
-		return (retval);
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = PF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	if (!resolve)
-		hints.ai_flags |= AI_NUMERICHOST;
-	error = getaddrinfo(s, "80", &hints, &res0);
-	if (!error) {
-		for (res = res0; res != NULL; res = res->ai_next) {
-			retval = VSA_Build(p, res->ai_addr, res->ai_addrlen);
-			if (retval != NULL)
-				break;
-		}
-		freeaddrinfo(res0);
-	}
-	return (retval);
-}
-
 VCL_IP
 vmod_ip(VRT_CTX, struct VARGS(ip) *a)
 {
@@ -234,7 +207,8 @@ vmod_ip(VRT_CTX, struct VARGS(ip) *a)
 		return (NULL);
 	}
 
-	retval = lookup(p, a->s, a->resolve);
+	retval = VSS_ResolveOne(p, a->s, "80", PF_UNSPEC, SOCK_STREAM,
+	    a->resolve ? 0 : AI_NUMERICHOST);
 	if (retval != NULL)
 		return (retval);
 

bin/varnishd/proxy/cache_proxy_proto.c
@@ -40,6 +40,7 @@
 
 #include "vend.h"
 #include "vsa.h"
+#include "vss.h"
 #include "vtcp.h"
 
 struct vpx_tlv {
@@ -61,7 +62,6 @@ vpx_proto1(const struct worker *wrk, const struct req *req)
 	const char *fld[5];
 	int i;
 	char *p, *q;
-	struct addrinfo hints, *res;
 	struct suckaddr *sa;
 	int pfam = -1;
 
@@ -100,58 +100,34 @@ vpx_proto1(const struct worker *wrk, const struct req *req)
 	}
 
 	if (!strcmp(fld[0], "TCP4"))
-		pfam = AF_INET;
+		pfam = PF_INET;
 	else if (!strcmp(fld[0], "TCP6"))
-		pfam = AF_INET6;
+		pfam = PF_INET6;
 	else {
 		VSL(SLT_ProxyGarbage, req->sp->vxid,
 		    "PROXY1: Wrong TCP[46] field");
 		return (-1);
 	}
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV;
-
-	i = getaddrinfo(fld[1], fld[3], &hints, &res);
-	if (i != 0) {
-		VSL(SLT_ProxyGarbage, req->sp->vxid,
-		    "PROXY1: Cannot resolve source address (%s)",
-		    gai_strerror(i));
-		return (-1);
-	}
-	AZ(res->ai_next);
-	if (res->ai_family != pfam) {
-		VSL(SLT_ProxyGarbage, req->sp->vxid,
-		    "PROXY1: %s got wrong protocol (%d)",
-		    fld[0], res->ai_family);
-		freeaddrinfo(res);
-		return (-1);
-	}
 	SES_Reserve_client_addr(req->sp, &sa);
-	AN(VSA_Build(sa, res->ai_addr, res->ai_addrlen));
+	if (VSS_ResolveOne(sa, fld[1], fld[3],
+	    pfam, SOCK_STREAM, AI_NUMERICHOST | AI_NUMERICSERV) == NULL) {
+		VSL(SLT_ProxyGarbage, req->sp->vxid,
+		    "PROXY1: Cannot resolve source address");
+		return (-1);
+	}
 	SES_Set_String_Attr(req->sp, SA_CLIENT_IP, fld[1]);
 	SES_Set_String_Attr(req->sp, SA_CLIENT_PORT, fld[3]);
-	freeaddrinfo(res);
 
-	i = getaddrinfo(fld[2], fld[4], &hints, &res);
-	if (i != 0) {
-		VSL(SLT_ProxyGarbage, req->sp->vxid,
-		    "PROXY1: Cannot resolve destination address (%s)",
-		    gai_strerror(i));
-		return (-1);
-	}
-	AZ(res->ai_next);
-	if (res->ai_family != pfam) {
-		VSL(SLT_ProxyGarbage, req->sp->vxid,
-		    "PROXY1: %s got wrong protocol (%d)",
-		    fld[0], res->ai_family);
-		freeaddrinfo(res);
-		return (-1);
-	}
 	SES_Reserve_server_addr(req->sp, &sa);
-	AN(VSA_Build(sa, res->ai_addr, res->ai_addrlen));
-	freeaddrinfo(res);
+	if (VSS_ResolveOne(sa, fld[2], fld[4],
+	    pfam, SOCK_STREAM, AI_NUMERICHOST | AI_NUMERICSERV) == NULL) {
+		VSL(SLT_ProxyGarbage, req->sp->vxid,
+		    "PROXY1: Cannot resolve destination address");
+		return (-1);
+	}
+	SES_Set_String_Attr(req->sp, SA_CLIENT_IP, fld[1]);
+	SES_Set_String_Attr(req->sp, SA_CLIENT_PORT, fld[3]);
 
 	VSL(SLT_Proxy, req->sp->vxid, "1 %s %s %s %s",
 	    fld[1], fld[3], fld[2], fld[4]);

bin/varnishtest/tests/o00000.vtc
@@ -34,10 +34,10 @@ logexpect l1 -v v1 {
 	expect * 1002	ProxyGarbage	"PROXY1: Too few fields"
 	expect * 1003	ProxyGarbage	"PROXY1: Too many fields"
 	expect * 1004	ProxyGarbage	"PROXY1: Wrong TCP\\[46\\] field"
-	expect * 1005	ProxyGarbage	"PROXY1: Cannot resolve source address \\(.*\\)"
-	expect * 1007	ProxyGarbage	"PROXY1: Cannot resolve destination address \\(.*\\)"
-	expect * 1013	ProxyGarbage	"PROXY1: TCP4 got wrong protocol \\([0-9]*\\)"
-	expect * 1014	ProxyGarbage	"PROXY1: TCP6 got wrong protocol \\([0-9]*\\)"
+	expect * 1005	ProxyGarbage	"PROXY1: Cannot resolve source address"
+	expect * 1007	ProxyGarbage	"PROXY1: Cannot resolve destination address"
+	expect * 1009	ProxyGarbage	"PROXY1: Cannot resolve source address"
+	expect * 1011	ProxyGarbage	"PROXY1: Cannot resolve source address"
 	expect * 1015	Proxy		"1 1.2.3.4 1234 5.6.7.8 5678"
 	expect * 1018	Proxy		"1 1:f::2 1234 5:a::8 5678"
 	expect * 1021	Proxy		"1 1:f::3 1234 5:a::8 5678"
@@ -50,7 +50,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY "
@@ -58,7 +58,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY A B C D\r\n"
@@ -66,7 +66,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY A B C D E F\r\n"
@@ -74,7 +74,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY A B C D E\r\n"
@@ -82,7 +82,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP4 B C D E\r\n"
@@ -90,7 +90,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP4 1.2.3.4 C D E\r\n"
@@ -98,7 +98,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP4 1.2.3.4 D 1234 E\r\n"
@@ -106,7 +106,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP4 1.2.3.4 5.6.7.8 1234 E\r\n"
@@ -114,7 +114,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP6 B C D E\r\n"
@@ -122,7 +122,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP6 1:f::2 C D E\r\n"
@@ -130,7 +130,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP6 1:f::2 1234 D E\r\n"
@@ -138,7 +138,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP6 1:f::2 5:a::8 1234 E\r\n"
@@ -146,7 +146,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP4 1:f::2 5:a::8 1234 5678\r\n"
@@ -154,7 +154,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 {
 	send "PROXY TCP6 1.2.3.4 5.6.7.8 1234 5678\r\n"
@@ -162,7 +162,7 @@ client c1 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 # Finally try something which works...
 client c1 -proxy1 "1.2.3.4:1234 5.6.7.8:5678" {
@@ -179,7 +179,7 @@ client c1 -proxy1 "1.2.3.4:1234 5.6.7.8:5678" {
 	expect resp.http.rp != "1234"
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 client c1 -proxy1 "[1:f::2]:1234 [5:a::8]:5678" {
 	txreq -url /2
@@ -195,7 +195,7 @@ client c1 -proxy1 "[1:f::2]:1234 [5:a::8]:5678" {
 	expect resp.http.rp != "1234"
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 # Try with appended request (See also: #1728)
 client c2 {
@@ -204,13 +204,15 @@ client c2 {
 	expect resp.http.url == "/3"
 } -run
 
+varnish v1 -vsl_catchup
+
 # Malformed (missing \r)
 client c2 {
 	send "PROXY TCP4 1.2.3.4 5.6.7.8 1234 5678\n"
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 # Malformed, too long (106)
 # NB: Should check VSL for proper disposal
@@ -219,7 +221,7 @@ client c2 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 # Malformed, too long (107)
 # NB: Should check VSL for proper disposal
@@ -228,7 +230,7 @@ client c2 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 # Malformed, too long (108)
 # NB: Should check VSL for proper disposal
@@ -237,6 +239,6 @@ client c2 {
 	expect_close
 } -run
 
-delay .1
+varnish v1 -vsl_catchup
 
 logexpect l1 -wait

bin/varnishtest/vtc_client.c
@@ -31,6 +31,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -73,21 +74,11 @@ static VTAILQ_HEAD(, client)	clients =
  * Send the proxy header
  */
 
-static int v_matchproto_(vss_resolved_f)
-proxy_cb(void *priv, const struct suckaddr *sa)
-{
-	struct suckaddr **addr = priv;
-	*addr = VSA_Clone(sa);
-	return (1);
-}
-
 static void
 client_proxy(struct vtclog *vl, int fd, int version, const char *spec)
 {
 	struct suckaddr *sac, *sas;
-	const char *err;
 	char *p, *p2;
-	int error;
 
 	p = strdup(spec);
 	AN(p);
@@ -95,14 +86,12 @@ client_proxy(struct vtclog *vl, int fd, int version, const char *spec)
 	AN(p2);
 	*p2++ = '\0';
 
-	error = VSS_resolver(p, NULL, proxy_cb, &sac, &err);
-	if (err != NULL)
-		vtc_fatal(vl, "Could not resolve client address: %s", err);
-	assert(error == 1);
-	error = VSS_resolver(p2, NULL, proxy_cb, &sas, &err);
-	if (err != NULL)
-		vtc_fatal(vl, "Could not resolve server address: %s", err);
-	assert(error == 1);
+	sac = VSS_ResolveOne(NULL, p, NULL, 0, SOCK_STREAM, AI_PASSIVE);
+	if (sac == NULL)
+		vtc_fatal(vl, "Could not resolve client address");
+	sas = VSS_ResolveOne(NULL, p2, NULL, 0, SOCK_STREAM, AI_PASSIVE);
+	if (sas == NULL)
+		vtc_fatal(vl, "Could not resolve server address");
 	if (vtc_send_proxy(fd, version, sac, sas))
 		vtc_fatal(vl, "Write failed: %s", strerror(errno));
 	free(p);

bin/varnishtest/vtc_main.c
@@ -38,6 +38,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
 
 #include "vtc.h"
 
@@ -550,18 +551,11 @@ i_mode(void)
  * Figure out what IP related magic
  */
 
-static int v_matchproto_(vss_resolved_f)
-bind_cb(void *priv, const struct suckaddr *sa)
-{
-	(void)priv;
-	return (VTCP_bind(sa, NULL));
-}
-
 static void
 ip_magic(void)
 {
-	const char *p;
 	int fd;
+	struct suckaddr *sa;
 	char abuf[VTCP_ADDRBUFSIZE];
 	char pbuf[VTCP_PORTBUFSIZE];
 
@@ -572,7 +566,9 @@ ip_magic(void)
 	 * XXX: "localhost", but that doesn't work out of the box.
 	 * XXX: Things like "prefer_ipv6" parameter complicates things.
 	 */
-	fd = VSS_resolver("127.0.0.1", NULL, bind_cb, NULL, &p);
+	sa = VSS_ResolveOne(NULL, "127.0.0.1", "0", 0, SOCK_STREAM, 0);
+	AN(sa);
+	fd = VTCP_bind(sa, NULL);
 	assert(fd >= 0);
 	VTCP_myname(fd, abuf, sizeof abuf, pbuf, sizeof(pbuf));
 	extmacro_def("localhost", "%s", abuf);

bin/varnishtest/vtc_misc.c
@@ -344,30 +344,19 @@ cmd_delay(CMD_ARGS)
  * DNS services.  This is a basic sanity check for those.
  */
 
-static int v_matchproto_(vss_resolved_f)
-dns_cb(void *priv, const struct suckaddr *sa)
-{
-	char abuf[VTCP_ADDRBUFSIZE];
-	char pbuf[VTCP_PORTBUFSIZE];
-	int *ret = priv;
-
-	VTCP_name(sa, abuf, sizeof abuf, pbuf, sizeof pbuf);
-	if (strcmp(abuf, "192.0.2.255")) {
-		fprintf(stderr, "DNS-test: Wrong response: %s\n", abuf);
-		*ret = -1;
-	} else if (*ret == 0)
-		*ret = 1;
-	return (0);
-}
-
 static int
 dns_works(void)
 {
-	int ret = 0, error;
-	const char *msg;
+	struct suckaddr *sa;
+	char abuf[VTCP_ADDRBUFSIZE];
+	char pbuf[VTCP_PORTBUFSIZE];
 
-	error = VSS_resolver("dns-canary.freebsd.dk", NULL, dns_cb, &ret, &msg);
-	if (error || msg != NULL || ret != 1)
+	sa = VSS_ResolveOne(NULL, "dns-canary.freebsd.dk", NULL, 0, 0, 0);
+	if (sa == NULL)
+		return (0);
+	VTCP_name(sa, abuf, sizeof abuf, pbuf, sizeof pbuf);
+	free(sa);
+	if (strcmp(abuf, "192.0.2.255"))
 		return (0);
 	return (1);
 }

include/vss.h
@@ -34,3 +34,6 @@ int VSS_resolver(const char *addr, const char *def_port, vss_resolved_f *func,
    void *priv, const char **err);
 int VSS_resolver_socktype(const char *addr, const char *def_port,
     vss_resolved_f *func, void *priv, const char **err, int socktype);
+struct suckaddr *VSS_ResolveOne(void *dst,
+    const char *addr, const char *port,
+    int family, int socktype, int flags);

lib/libvarnish/vss.c
@@ -154,3 +154,42 @@ VSS_resolver(const char *addr, const char *def_port, vss_resolved_f *func,
 	return (VSS_resolver_socktype(
 	    addr, def_port, func, priv, err, SOCK_STREAM));
 }
+
+#include <stdio.h>
+
+struct suckaddr *
+VSS_ResolveOne(void *dst, const char *addr, const char *port,
+    int family, int socktype, int flags)
+{
+	struct addrinfo hints, *res = NULL;
+	struct suckaddr *retval = NULL;
+	char *p = NULL, *hp, *pp;
+	int error;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = family;
+	hints.ai_socktype = socktype;
+	hints.ai_flags = flags;
+
+	AN(addr);
+	if (port != NULL) {
+		error = getaddrinfo(addr, port, &hints, &res);
+	} else {
+		p = strdup(addr);
+		AN(p);
+		if (vss_parse(p, &hp, &pp) != NULL || pp == NULL) {
+			free(p);
+			return (NULL);
+		}
+		error = getaddrinfo(hp, pp, &hints, &res);
+		free(p);
+	}
+	if (!error && res != NULL && res->ai_next == NULL) {
+		if (dst == NULL)
+			retval = VSA_Malloc(res->ai_addr, res->ai_addrlen);
+		else
+			retval = VSA_Build(dst, res->ai_addr, res->ai_addrlen);
+		freeaddrinfo(res);
+	}
+	return (retval);
+}

commit c85aa3110c6ab841ef33ba95dfbd962c8ed6c351

Tolerate a null address string in std.ip()
A regression from d7a81fe82ca09c9f3b3a9fd67265de00da7a4315 and the
followup changes from #2993. The new VSS_Resolve{One,First} functions
expect a non-null address string.

lib/libvmod_std/vmod_std_conversions.c
@@ -194,7 +194,7 @@ VCL_IP
 vmod_ip(VRT_CTX, struct VARGS(ip) *a)
 {
 	void *p;
-	VCL_IP retval;
+	VCL_IP retval = NULL;
 
 	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
 	if (a->valid_fallback)
@@ -206,10 +206,11 @@ vmod_ip(VRT_CTX, struct VARGS(ip) *a)
 		return (NULL);
 	}
 
-	retval = VSS_ResolveFirst(
-	    p, a->s, a->valid_p ? a->p : "80",
-	    AF_UNSPEC, SOCK_STREAM,
-	    a->resolve ? 0 : AI_NUMERICHOST|AI_NUMERICSERV);
+	if (a->s != NULL)
+		retval = VSS_ResolveFirst(
+		    p, a->s, a->valid_p ? a->p : "80",
+		    AF_UNSPEC, SOCK_STREAM,
+		    a->resolve ? 0 : AI_NUMERICHOST|AI_NUMERICSERV);
 
 	if (retval != NULL)
 		return (retval);

bin/varnishtest/tests/m00011.vtc
@@ -77,6 +77,11 @@ varnish v1 -vcl+backend {
 		set resp.http.port11 = std.port(debug.get_ip());
 		std.timestamp("1.2.3.4 80, p = 443");
 
+		debug.store_ip(std.ip(req.http.non-existent, server.ip));
+		set resp.http.ip12 = debug.get_ip();
+		set resp.http.port12 = std.port(debug.get_ip());
+		std.timestamp("NULL, server.ip");
+
 	}
 } -start
 
@@ -110,4 +115,6 @@ client c1 {
 	expect resp.http.port10 == ${s1_port}
 	expect resp.http.ip11 == 1.2.3.4
 	expect resp.http.port11 == 80
+	expect resp.http.ip12 == ${v1_addr}
+	expect resp.http.port12 == ${v1_port}
 } -run