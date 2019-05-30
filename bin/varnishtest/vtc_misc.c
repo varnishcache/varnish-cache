21956d6c2ec6067939cbeffe5f7789a8d09c9af8
Poul-Henning Kamp <phk@FreeBSD.org>
2019-05-30 13:33:04 +0000

    Pass correct args to VSS_ResolveOne

--- bin/varnishtest/vtc_misc.c
+++ bin/varnishtest/vtc_misc.c
@@ -29,6 +29,7 @@
 #include "config.h"
 
 #include <sys/wait.h>
+#include <sys/socket.h>
 
 #include <grp.h>
 #include <math.h>
@@ -351,7 +352,7 @@ dns_works(void)
 	char abuf[VTCP_ADDRBUFSIZE];
 	char pbuf[VTCP_PORTBUFSIZE];
 
-	sa = VSS_ResolveOne(NULL, "dns-canary.freebsd.dk", NULL, 0, 0, 0);
+	sa = VSS_ResolveOne(NULL, "dns-canary.freebsd.dk", NULL, AF_UNSPEC, SOCK_STREAM, 0);
 	if (sa == NULL)
 		return (0);
 	VTCP_name(sa, abuf, sizeof abuf, pbuf, sizeof pbuf);
