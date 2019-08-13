08eceda3f20bcdf6c79228489c88f9d8464edbc9
Poul-Henning Kamp <phk@FreeBSD.org>
2019-08-13 11:08:18 +0000

    Try to get the VSM cluster inconsistency fixed for good.

--- bin/varnishtop/varnishtop.c
+++ bin/varnishtop/varnishtop.c
@@ -385,6 +385,7 @@ main(int argc, char **argv)
 		ident = VSM_Dup(vut->vsm, "Arg", "-i");
 	else
 		ident = strdup("");
+	AN(ident);
 	vut->dispatch_f = accumulate;
 	vut->dispatch_priv = NULL;
 	vut->sighup_f = sighup;
