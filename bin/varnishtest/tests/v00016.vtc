varnishcache/varnish-cache

commit 340abd0456e4d018fd1589f406287057ab6740ff

Recognize type-based methods and fix error messages accordingly.
No methods are implemented yet.

lib/libvcc/vcc_symb.c
@@ -238,7 +238,7 @@ struct symbol *
 VCC_SymbolGet(struct vcc *tl, vcc_kind_t kind,
     const struct symmode *e, const struct symxref *x)
 {
-	struct symtab *st, *st2;
+	struct symtab *st, *st2 = NULL;
 	struct symbol *sym = NULL, *sym2 = NULL;
 	struct token *t0, *tn, *tn1, *tn2 = NULL;
 

bin/varnishtest/tests/v00016.vtc
@@ -41,7 +41,7 @@ varnish v1 -errvcl {Operator > not possible on BACKEND} {
 	sub vcl_recv { if (a > b) { } }
 }
 
-varnish v1 -errvcl {Symbol not found: 'req.foo'} {
+varnish v1 -errvcl {Unknown property 'foo' for type HTTP} {
 	backend b { .host = "127.0.0.1"; }
 	sub vcl_hash { if (req.foo != "bar") { } }
 }

bin/varnishtest/tests/v00018.vtc
@@ -38,7 +38,7 @@ varnish v1 -errvcl {Expected ';' got 'if'} {
 	sub vcl_recv { set req.url = "foo" if "bar"; }
 }
 
-varnish v1 -errvcl {Symbol not found: 'req.foo'} {
+varnish v1 -errvcl {Unknown property 'foo' for type HTTP} {
 	backend b { .host = "127.0.0.1"; }
 	sub vcl_hash { hash_data(req.foo); }
 }

lib/libvcc/vcc_expr.c
@@ -688,7 +688,7 @@ vcc_expr5(struct vcc *tl, struct expr **e, vcc_type_t fmt)
 	switch (tl->t->tok) {
 	case ID:
 		t = tl->t;
-		sym = VCC_SymbolGet(tl, SYM_NONE, SYMTAB_EXISTING, XREF_REF);
+		sym = VCC_SymbolGet(tl, SYM_NONE, SYMTAB_PARTIAL, XREF_REF);
 		ERRCHK(tl);
 		AN(sym);
 		if (sym->kind == SYM_FUNC && sym->type == VOID) {
@@ -811,7 +811,16 @@ vcc_expr4(struct vcc *tl, struct expr **e, vcc_type_t fmt)
 
 	*e = NULL;
 	vcc_expr5(tl, e, fmt);
-	return;
+	while (tl->t->tok == '.') {
+		vcc_NextToken(tl);
+		ExpectErr(tl, ID);
+		VSB_printf(tl->sb, "Unknown property ");
+		vcc_ErrToken(tl, tl->t);
+		VSB_printf(tl->sb,
+		 " for type %s\n", (*e)->fmt->name);
+		vcc_ErrWhere(tl, tl->t);
+		return;
+	}
 }
 
 /*--------------------------------------------------------------------

commit 46a6a3af681644f43e2e813ee68f8bd53b4cd1bb

Apply bandaid to missing VMOD symbol errors
The VCL compilation error message when a VMOD symbol does not exist
simply disappeared. When I figured how to bring it back I was lucky
my test case exhibited another quirk for the following case:

    new foo = bar.foo()

Where bar is a successfully imported VMOD, and foo is the missing
constructor. For some reason instance symbols are created with VCL
low and high values, so the missing foo constructor ended up being
confused with the existing foo instance.

The regression I initially hunted down (the lack of error message
in the first place) was introduced by 340abd0456e4. I suggest we
don't give VMOD-induced symbols a VCL low/high since by definition
they are not tied to a VCL version (even though they may do so at
run time) and use that criteria to filter out the spurious error
message:

> Symbol not found: 'directors.foo' (Only available when 4.0 <= VCL
> syntax <= 4.0)

If there is no proper low or high (and 4.0 is legit) then we don't
print the "Only available when" part? For now I needed the bandaid.

Refs 340abd0456e4d018fd1589f406287057ab6740ff

lib/libvcc/vcc_symb.c
@@ -275,6 +275,8 @@ VCC_SymbolGet(struct vcc *tl, vcc_kind_t kind,
 			break;
 		tn = tn1;
 	}
+	if (sym != NULL && sym->kind == SYM_VMOD && e == SYMTAB_PARTIAL)
+		e = SYMTAB_EXISTING;
 	if (sym != NULL && e == SYMTAB_PARTIAL) {
 		st = st2;
 		tn = tn2;
@@ -294,7 +296,8 @@ VCC_SymbolGet(struct vcc *tl, vcc_kind_t kind,
 			VSB_printf(tl->sb, "%.*s", PF(tn1));
 		VSB_cat(tl->sb, "'");
 		sym = vcc_sym_in_tab(tl, st, kind, VCL_LOW, VCL_HIGH);
-		if (sym != NULL) {
+		if (sym != NULL && sym->kind != SYM_OBJECT &&
+		    sym->kind != SYM_INSTANCE) { /* XXX: too specific */
 			VSB_cat(tl->sb, " (Only available when");
 			if (sym->lorev >= VCL_LOW)
 				VSB_printf(tl->sb, " %.1f <=", .1 * sym->lorev);

bin/varnishtest/tests/v00016.vtc
@@ -117,6 +117,19 @@ varnish v1 -syntax 4.0 -errvcl {Undefined acl foo} {
 	}
 }
 
+# NB: The line break in -errvcl is here on purpose, it prevents
+# a spurious "Only available when" addition to be missed when the
+# foo constructor could be confused with the foo instance name.
+varnish v1 -syntax 4.0 -errvcl {Symbol not found: 'directors.foo'
+At:} {
+	import directors;
+	backend b { .host = "127.0.0.1"; }
+
+	sub vcl_init {
+		new foo = directors.foo();
+	}
+}
+
 # 'foo' overloaded
 varnish v1 -syntax 4.0 -errvcl {Symbol not found: 'foo'} {
 	backend b { .host = "127.0.0.1"; }