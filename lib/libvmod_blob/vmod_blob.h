2d0b2f557a33bbfdb223c0591df459ade0c51b0f
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2019-11-22 22:46:40 +0100

    Document the new vmod_blob codec interface
    It's in essence the same as the previous one.

--- lib/libvmod_blob/vmod_blob.h
+++ lib/libvmod_blob/vmod_blob.h
@@ -61,11 +61,59 @@ enum case_e {
 #include "tbl_case.h"
 };
 
+/*
+ * Length estimate interface.
+ */
 typedef size_t blob_len_f(size_t);
-typedef ssize_t blob_encode_f(BLOB_CODEC, enum case_e, blob_dest_t, size_t,
-    blob_src_t, size_t);
-typedef ssize_t blob_decode_f(BLOB_CODEC, blob_dest_t, size_t, ssize_t,
-    VCL_STRANDS);
+
+/*
+ * General interface for a decoder: decode the strings STRANDS into dest,
+ * and return the length of decoded data.
+ *
+ * dest:	destination of the decoded data
+ * destlen:	maximum length available at dest
+ * slen:	maximum length to read or -1 to read up to '\0'
+ * strings:	a string in the form of a STRANDS
+ *
+ * The regions pointed to by dest and strings MUST NOT overlap (per
+ * restrict). Note that any member of strings may be NULL or empty.
+ *
+ * Returns:
+ *        -1, if there is insufficient space at dest, or if the decoding is
+ *            invalid; errno SHALL be set to ENOMEM or EINVALID, respectively
+ *         0, if the length of the decoding is 0 -- the caller should return
+ *            a static constant empty BLOB
+ * otherwise, the number of bytes written
+ */
+typedef ssize_t blob_decode_f(BLOB_CODEC, blob_dest_t dest, size_t destlen,
+    ssize_t slen, VCL_STRANDS strings);
+
+/*
+ * General interface for an encoder: encode the data at src of length
+ * srclen into a null-terminated string at dest, and return the length
+ * of the encoded string.
+ *
+ * kase:	case enum (for encodings with hex digits)
+ * dest:	destination of the encoded string
+ * destlen:	maximum length available at dest
+ * src:		source of data to be encoded
+ * srclen:	length of data to be encoded
+ *
+ * The regions pointed to by dest and src MUST NOT overlap (this is the
+ * contract imposed by restrict).
+ * An encoder SHALL NOT append the terminating null byte (this must
+ * be done by the caller).
+ *
+ * Returns:
+ *        -1, if there is insufficient space at dest, *including* space
+ *            for the terminating null byte
+ *         0, if the length of the encoding is 0 -- the caller should
+ *            return the static constant empty string (literal "")
+ * otherwise, the number of bytes written (note that this does not
+ *            include any terminating null byte)
+ */
+typedef ssize_t blob_encode_f(BLOB_CODEC, enum case_e kase, blob_dest_t dest,
+    size_t destlen, blob_src_t src, size_t srclen);
 
 struct blob_codec {
 	blob_len_f	*decode_len;
@@ -107,63 +155,13 @@ enum encoding {
 
 #define AENC(enc) assert((enc) > __INVALID_ENCODING && (enc) < __MAX_ENCODING)
 
-/*
- * Length estimate interface
- */
 typedef size_t len_f(size_t);
 
-/*
- * General interface for an encoder: encode the data at in of length inlen
- * into a null-terminated string at buf, and return the length of the
- * encoding.
- *
- * enc: encoding enum (from parse_encoding.h)
- * kase: case enum (for encodings with hex digits)
- * buf: destination of the encoded string
- * buflen: maximum length available at buf
- * in: source of data to be encoded
- * inlen: length of data to be encoded
- *
- * The regions pointed to by buf and in MUST NOT overlap (this is the
- * contract imposed by restrict).
- * An encoder SHALL NOT append the terminating null byte (this must
- * be done by the caller).
- *
- * Returns:
- * -1, if there is insufficient space at buf, *including* space for the
- *     terminating null byte
- * 0, if the length of the encoding is 0 -- the caller should return
- *    the static constant empty string (literal "")
- * otherwise, the number of bytes written (note that this does not
- *            include any terminating null byte)
- */
 typedef
 ssize_t encode_f(const enum encoding enc, const enum case_e kase,
 		 blob_dest_t buf, blob_len_t buflen,
 		 blob_src_t in, blob_len_t inlen);
 
-/*
- * General interface for a decoder: decode the concatenation of strings
- * (obtained from STRANDS) into buf, and return the length of decoded
- * data.
- *
- * dec: decoding enum (from parse_encoding.h)
- * buf: destination of the decoded data
- * buflen: maximum length available at buf
- * inlen: maximum length to read or -1 to read up to \0
- * strings: strings obtained from VCL STRANDS
- *
- * The regions pointed to by buf and strings MUST NOT overlap (per
- * restrict).
- * Note that any member of the strings list may be NULL or empty.
- *
- * Returns:
- * -1, if there is insufficient space at buf, or if the decoding is
- *     invalid; errno SHALL be set to ENOMEM or EINVALID, respectively
- * 0, if the length of the decoding is 0 -- the caller should return
- *    a static constant empty BLOB
- * otherwise, the number of bytes written
- */
 typedef ssize_t decode_f(const enum encoding dec, blob_dest_t buf,
     blob_len_t buflen, blob_slen_t inlen, VCL_STRANDS strings);
 
