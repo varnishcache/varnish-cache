bae79c214e6e31ff288cc1eb09c9433556eb3b97
Martin Blix Grydeland <martin@varnish-software.com>
2019-12-16 16:37:27 +0100

    Fix WS_ReserveSize calls when bytes is equal to free workspace
    Currently, with the 505b7bd9643006fa8e3977f920564ce12a2b24a2 patch, when
    calling WS_ReserveSize with bytes equal to the amount of workspace that is
    currently available, it will return zero and mark overflow.
    
    This patch redoes the patch, and changes it to return zero and overflow
    only when the requested number of bytes is larger than what is available.

--- bin/varnishd/cache/cache_ws.c
+++ bin/varnishd/cache/cache_ws.c
@@ -262,7 +262,7 @@ WS_ReserveSize(struct ws *ws, unsigned bytes)
 	if (bytes < b2)
 		b2 = PRNDUP(bytes);
 
-	if (ws->f + b2 >= ws->e) {
+	if (bytes > b2) {
 		WS_MarkOverflow(ws);
 		return (0);
 	}
