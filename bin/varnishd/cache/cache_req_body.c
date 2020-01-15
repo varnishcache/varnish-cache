0644d58abe07a18a972ead14e4aa304c80f354e7
Nils Goroll <nils.goroll@uplex.de>
2020-01-15 18:13:45 +0100

    A request body now might have up to two references
    since d4b6228e1e32cda3014eeff0a5cb60cd7a0b5474 the busyobj might also
    gain one, so depending on who deref's first, there may be one or zero
    references left.

--- bin/varnishd/cache/cache_req_body.c
+++ bin/varnishd/cache/cache_req_body.c
@@ -263,11 +263,17 @@ VRB_Ignore(struct req *req)
 void
 VRB_Free(struct req *req)
 {
+	int r;
 
 	CHECK_OBJ_NOTNULL(req, REQ_MAGIC);
 
-	if (req->body_oc != NULL)
-		AZ(HSH_DerefObjCore(req->wrk, &req->body_oc, 0));
+	if (req->body_oc == NULL)
+		return;
+
+	r = HSH_DerefObjCore(req->wrk, &req->body_oc, 0);
+
+	// a busyobj may have gained a reference
+	assert (r == 0 || r == 1);
 }
 
 /*----------------------------------------------------------------------
