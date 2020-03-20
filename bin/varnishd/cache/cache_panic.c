410354f143fb9f27ad30a3d765827d2849cea1ae
Nils Goroll <nils.goroll@uplex.de>
2020-03-20 16:32:49 +0100

    add pthread_self() to panic output
    This should help locating the panicking thread in a core dump when when
    the principle thread as determined by the debugger is a different one.

--- bin/varnishd/cache/cache_panic.c
+++ bin/varnishd/cache/cache_panic.c
@@ -783,6 +783,8 @@ pan_ic(const char *func, const char *file, int line, const char *cond,
 	if (err)
 		VSB_printf(pan_vsb, "errno = %d (%s)\n", err, vstrerror(err));
 
+	VSB_printf(pan_vsb, "pthread.self = %p\n", (void *)pthread_self());
+
 	q = THR_GetName();
 	if (q != NULL)
 		VSB_printf(pan_vsb, "pthread.name = (%s)\n", q);
