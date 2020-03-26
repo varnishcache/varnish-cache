8222ddfe37f84578ee1a7c624fe39cc43f5c71a2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-03-26 09:48:02 +0200

    Use CircleCI 'machine' executors with QEMU emulation to build packages
    This way CircleCI could be used to build, test and create multiarch packages.
    
    Alpine Linux aarch64 packaging is currently commented out because it fails while installing the dependencies. Needs more debugging, but the issue must be in the base Docker image because the same
    Shell script works just fine on amd64

---
+++ .circleci/make-apk-packages.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+echo "PARAM_RELEASE: $PARAM_RELEASE"
+echo "PARAM_DIST: $PARAM_DIST"
+
+if test "x$PARAM_RELEASE" = "x"; then
+    echo "Env variable PARAM_RELEASE is not set! For example PARAM_RELEASE=8, for CentOS 8"
+    exit 1
+elif test "x$PARAM_DIST" = "x"; then
+    echo "Env variable PARAM_DIST is not set! For example PARAM_DIST=centos"
+    exit 1
+fi
+
+export DIST_DIR=build
+
+mkdir -p /package && cd /package
+tar xazf /workspace/alpine.tar.gz --strip 1
+
+adduser -D builder
+echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
+addgroup builder abuild
+mkdir -p /var/cache/distfiles
+chmod -R a+w /var/cache/distfiles
+chmod -R a+w /var/cache/apk
+
+echo "Generate key"
+su builder -c "abuild-keygen -nai"
+
+echo "Fix APKBUILD's variables"
+ls -la
+tar xavf /workspace/varnish-*.tar.gz
+if [ -e /workspace/.is_weekly ]; then
+    WEEKLY='.weekly'
+else
+    WEEKLY=
+fi
+VERSION=$(varnish-*/configure --version | awk 'NR == 1 {print $NF}')$WEEKLY
+echo "Version: $VERSION"
+sed -i "s/@VERSION@/$VERSION/" APKBUILD
+rm -rf varnish-*/
+
+echo "Fix checksums, build"
+cp /workspace/varnish-*.tar.gz .
+chown builder -R /workspace
+chown builder -R /package
+rm -rf /var/cache/apk/*
+chown builder:abuild -R /var/cache/
+
+su builder -c "abuild checksum"
+su builder -c "abuild -r"
+
+echo "Fix the APKBUILD's version"
+su builder -c "mkdir apks"
+ARCH=`uname -m`
+echo "Arch: $ARCH"
+su builder -c "cp /home/builder/packages/$ARCH/*.apk apks"
+ls -laR apks
+
+echo "Import the packages into the workspace"
+mkdir -p /varnish-cache/packages/apk/$PARAM_DIST/$PARAM_RELEASE/$ARCH/
+mv /home/builder/packages/$ARCH/*.apk /varnish-cache/packages/apk/$PARAM_DIST/$PARAM_RELEASE/$ARCH/
