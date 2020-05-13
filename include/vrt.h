051d1efb766f5fb09134cb29989fa0859b57b540
Nils Goroll <nils.goroll@uplex.de>
2020-05-13 17:48:39 +0200

    vrt.h changelog tlc
    Ref #3311

--- include/vrt.h
+++ include/vrt.h
@@ -53,6 +53,8 @@
  * Whenever something is deleted or changed in a way which is not
  * binary/load-time compatible, increment MAJOR version
  *
+ * NEXT (2020-09-15)
+ *	VRT_VDI_Resolve() added
  * 11.0 (2020-03-16)
  *	Changed type of vsa_suckaddr_len from int to size_t
  *	New prefix_{ptr|len} fields in vrt_backend
