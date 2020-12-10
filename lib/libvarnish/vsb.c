17c148c124a5bbdb50913274a23a461756c6853d
Nils Goroll <nils.goroll@uplex.de>
2020-12-10 19:40:21 +0100

    flexelint
    ... thinks this statement is confusing.
    
    Ref: 15f94d33c9ad7955bde94386e953d19bbf6fb4de

--- lib/libvarnish/vsb.c
+++ lib/libvarnish/vsb.c
@@ -350,7 +350,7 @@ VSB_cat(struct vsb *s, const char *str)
 		return (-1);
 
 	while (s->s_indent > 0 && (nl = strchr(str, '\n')) != NULL) {
-		l = nl - str + 1;
+		l = (nl - str) + 1;
 		if (VSB_bcat(s, str, l) < 0)
 			return (-1);
 		str += l;
