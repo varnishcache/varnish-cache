c56e4a85d1ea3a14c859e4046d8723c9df87711c
Nils Goroll <nils.goroll@uplex.de>
2021-01-07 15:32:30 +0100

    vmod lint: change header format

--- vmod/flint.sh
+++ vmod/flint.sh
@@ -2,10 +2,10 @@
 
 for vmod in vmod_*.vcc ; do
     vmod="${vmod%.vcc}"
-    vmod="${vmod#vmod_}"
     echo "====================="
     echo "${vmod}"
     echo "====================="
+    vmod="${vmod#vmod_}"
     FLOPS="-I../bin/varnishd vcc_${vmod}_if.c vmod_${vmod}*.c" \
 	 ../tools/flint_skel.sh
 done
