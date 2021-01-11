varnishcache/varnish-cache

commit c1352b0b0ece8028317c838751d9042a55347cd5

Use VSA_getsockname() - avoid sockaddr_storage
avoids an second getsockname() in vbp_write_proxy_v1()
avoids a memcpy() in VTCP_my_suckaddr()

bin/varnishd/cache/cache_backend_probe.c
@@ -251,20 +251,22 @@ vbp_write_proxy_v1(struct vbp_target *vt, int *sock)
 	char buf[105]; /* maximum size for a TCP6 PROXY line with null char */
 	char addr[VTCP_ADDRBUFSIZE];
 	char port[VTCP_PORTBUFSIZE];
-	struct sockaddr_storage ss;
+	char vsabuf[vsa_suckaddr_len];
+	struct suckaddr *sua;
+	int proto;
 	struct vsb vsb;
-	socklen_t l;
 
-	VTCP_myname(*sock, addr, sizeof addr, port, sizeof port);
+	sua = VSA_getsockname(*sock, vsabuf, sizeof vsabuf);
+	AN(sua);
+	VTCP_name(sua, addr, sizeof addr, port, sizeof port);
 	AN(VSB_init(&vsb, buf, sizeof buf));
 
-	l = sizeof ss;
-	AZ(getsockname(*sock, (void *)&ss, &l));
-	if (ss.ss_family == AF_INET || ss.ss_family == AF_INET6) {
+	proto = VSA_Get_Proto(sua);
+	if (proto == AF_INET || proto == AF_INET6)
 		VSB_printf(&vsb, "PROXY %s %s %s %s %s\r\n",
-		    ss.ss_family == AF_INET ? "TCP4" : "TCP6",
+		    proto == AF_INET ? "TCP4" : "TCP6",
 		    addr, addr, port, port);
-	} else
+	else
 		VSB_cat(&vsb, "PROXY UNKNOWN\r\n");
 	AZ(VSB_finish(&vsb));
 

lib/libvarnish/vtcp.c
@@ -48,6 +48,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "vdef.h"
 #include "vas.h"
@@ -105,12 +106,8 @@ VTCP_name(const struct suckaddr *addr, char *abuf, unsigned alen,
 struct suckaddr *
 VTCP_my_suckaddr(int sock)
 {
-	struct sockaddr_storage addr_s;
-	socklen_t l;
-
-	l = sizeof addr_s;
-	AZ(getsockname(sock, (void *)&addr_s, &l));
-	return (VSA_Malloc(&addr_s, l));
+	return (VSA_getsockname(sock,
+	    malloc(vsa_suckaddr_len), vsa_suckaddr_len));
 }
 
 /*--------------------------------------------------------------------*/
@@ -118,12 +115,10 @@ VTCP_my_suckaddr(int sock)
 void
 VTCP_myname(int sock, char *abuf, unsigned alen, char *pbuf, unsigned plen)
 {
-	struct sockaddr_storage addr_s;
-	socklen_t l;
+	char buf[vsa_suckaddr_len];
 
-	l = sizeof addr_s;
-	AZ(getsockname(sock, (void *)&addr_s, &l));
-	vtcp_sa_to_ascii(&addr_s, l, abuf, alen, pbuf, plen);
+	VTCP_name(VSA_getsockname(sock, buf, sizeof buf),
+		  abuf, alen, pbuf, plen);
 }
 
 /*--------------------------------------------------------------------*/

commit 05356fb7c6c10d3ddd9766f474da679046922422

varnishd: VTCP_name() is not for UDS
fixes a regression introduced by c1352b0b0ece8028317c838751d9042a55347cd5:

by not using a suckaddr, VTCP_myname() would also work on UDS, but none
of the VTCP was ever intended to (and there are other places where that
is asserted). So one could argue that 9837045a011db7496b6a0680687c48fbbac41186
was already wrong, but I do not want to deflect from my own mistake in
any way.

Fixes #3492

bin/varnishd/cache/cache_backend_probe.c
@@ -258,16 +258,17 @@ vbp_write_proxy_v1(struct vbp_target *vt, int *sock)
 
 	sua = VSA_getsockname(*sock, vsabuf, sizeof vsabuf);
 	AN(sua);
-	VTCP_name(sua, addr, sizeof addr, port, sizeof port);
 	AN(VSB_init(&vsb, buf, sizeof buf));
 
 	proto = VSA_Get_Proto(sua);
-	if (proto == AF_INET || proto == AF_INET6)
+	if (proto == AF_INET || proto == AF_INET6) {
+		VTCP_name(sua, addr, sizeof addr, port, sizeof port);
 		VSB_printf(&vsb, "PROXY %s %s %s %s %s\r\n",
 		    proto == AF_INET ? "TCP4" : "TCP6",
 		    addr, addr, port, port);
-	else
+	} else {
 		VSB_cat(&vsb, "PROXY UNKNOWN\r\n");
+	}
 	AZ(VSB_finish(&vsb));
 
 	VSB_fini(&vsb);

lib/libvarnish/vtcp.c
@@ -100,6 +100,7 @@ VTCP_name(const struct suckaddr *addr, char *abuf, unsigned alen,
 	socklen_t sl;
 
 	sa = VSA_Get_Sockaddr(addr, &sl);
+	AN(sa);
 	vtcp_sa_to_ascii(sa, sl, abuf, alen, pbuf, plen);
 }
 