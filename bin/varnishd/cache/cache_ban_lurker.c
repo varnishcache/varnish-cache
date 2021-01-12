a19e22625caf062a501023cfc4878bbda52422a5
Nils Goroll <nils.goroll@uplex.de>
2021-01-12 12:28:35 +0100

    name the ban lurker thread

--- bin/varnishd/cache/cache_ban_lurker.c
+++ bin/varnishd/cache/cache_ban_lurker.c
@@ -420,6 +420,8 @@ ban_lurker(struct worker *wrk, void *priv)
 	CHECK_OBJ_NOTNULL(wrk, WORKER_MAGIC);
 	AZ(priv);
 
+	THR_SetName("ban_lurker");
+
 	VSL_Setup(&vsl, NULL, 0);
 
 	while (!ban_shutdown) {
