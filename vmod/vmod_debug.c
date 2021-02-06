af8894525e778adb343847de99c00fa23869a5c9
Nils Goroll <nils.goroll@uplex.de>
2021-02-06 14:10:30 +0100

    Add m00054.vtc: panic when calling sub from a wrong vcl
    VMODs must not use SUB references across VCLs. This test checks for a
    panic if they do.

--- vmod/vmod_debug.c
+++ vmod/vmod_debug.c
@@ -1309,3 +1309,25 @@ xyzzy_check_call(VRT_CTX, VCL_SUB sub)
 {
 	return (VRT_check_call(ctx, sub));
 }
+
+/* the next two are to test WRONG vmod behavior:
+ * holding a VCL_SUB reference across vcls
+ */
+
+static VCL_SUB wrong = NULL;
+
+VCL_VOID v_matchproto_(td_xyzzy_bad_memory)
+xyzzy_bad_memory(VRT_CTX, VCL_SUB sub)
+{
+	(void) ctx;
+
+	wrong = sub;
+}
+
+VCL_SUB v_matchproto_(td_xyzzy_total_recall)
+xyzzy_total_recall(VRT_CTX)
+{
+	(void) ctx;
+
+	return (wrong);
+}
