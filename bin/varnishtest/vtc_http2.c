0145e4a7a3dbe1c94da1cc81e506e137394f7b59
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2021-02-23 18:57:55 +0100

    vtc: Properly parse `rxdata -some`

--- bin/varnishtest/vtc_http2.c
+++ bin/varnishtest/vtc_http2.c
@@ -2254,6 +2254,7 @@ cmd_rxdata(CMD_ARGS)
 
 	while (*++av)
 		if (!strcmp(*av, "-some")) {
+			av++;
 			STRTOU32(times, *av, p, vl, "-some");
 			if (!times)
 				vtc_fatal(vl, "-some argument must be more"
