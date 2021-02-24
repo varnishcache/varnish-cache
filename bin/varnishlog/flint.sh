7c2379d53e2fdc990201144e64d1f3128bb87437
Poul-Henning Kamp <phk@FreeBSD.org>
2021-02-24 13:21:06 +0000

    Add copyright & SPDX to flint.sh files

--- bin/varnishlog/flint.sh
+++ bin/varnishlog/flint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# Copyright (c) 2010-2021 Varnish Software AS
+# SPDX-License-Identifier: BSD-2-Clause
+# See LICENSE file for full text of license
 
 FLOPS='
 	-DVARNISH_STATE_DIR=\"foo\"
