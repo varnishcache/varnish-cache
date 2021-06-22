9a96583bf3878b5ddeb69e252c1a53e5fcc4a018
Martin Blix Grydeland <martin@varnish-software.com>
2021-06-22 11:47:45 +0200

    Unbreak varnishtest expect_close
    The change to VTCP_Check() in 58a21da7736295e674ebb7e8ae9eee9529c083b0
    broke expect_close in varnishtest.

--- bin/varnishtest/vtc_http.c
+++ bin/varnishtest/vtc_http.c
@@ -1496,7 +1496,7 @@ cmd_http_expect_close(CMD_ARGS)
 			    "Expected close: poll = %d, revents = 0x%x",
 			    i, fds[0].revents);
 		i = read(hp->sess->fd, &c, 1);
-		if (VTCP_Check(i))
+		if (i <= 0 && VTCP_Check(i))
 			break;
 		if (i == 1 && vct_islws(c))
 			continue;
