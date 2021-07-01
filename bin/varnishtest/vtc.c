eb71eae78cf71412913b66517ab644630e4c21ec
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2021-07-01 08:37:15 +0200

    varnishtest: Replace macro_get() with macro_cat()
    The latter operates on a VSB, which is always what call sites are doing
    anyway. It also takes the responsibility of ignoring unknown macros, in
    preparation for more responsibilities that will also require the ability
    to fail a test case.

--- bin/varnishtest/vtc.c
+++ bin/varnishtest/vtc.c
@@ -204,8 +204,8 @@ macro_undef(struct vtclog *vl, const char *instance, const char *name)
 	AZ(pthread_mutex_unlock(&macro_mtx));
 }
 
-char *
-macro_get(const char *b, const char *e)
+void
+macro_cat(struct vtclog *vl, struct vsb *vsb, const char *b, const char *e)
 {
 	struct macro *m;
 	int l;
@@ -222,7 +222,9 @@ macro_get(const char *b, const char *e)
 		retval = malloc(VTIM_FORMAT_SIZE);
 		AN(retval);
 		VTIM_format(t, retval);
-		return (retval);
+		VSB_cat(vsb, retval);
+		free(retval);
+		return;
 	}
 
 	AZ(pthread_mutex_lock(&macro_mtx));
@@ -232,9 +234,19 @@ macro_get(const char *b, const char *e)
 			break;
 	}
 	if (m != NULL)
-		retval = strdup(m->val);
+		REPLACE(retval, m->val);
 	AZ(pthread_mutex_unlock(&macro_mtx));
-	return (retval);
+
+	if (retval == NULL) {
+		if (!ign_unknown_macro)
+			vtc_fatal(vl, "Macro ${%.*s} not found",
+			    (int)(e - b), b);
+		VSB_printf(vsb, "${%.*s}", (int)(e - b), b);
+		return;
+	}
+
+	VSB_cat(vsb, retval);
+	free(retval);
 }
 
 struct vsb *
@@ -259,7 +271,6 @@ macro_expand(struct vtclog *vl, const char *text)
 {
 	struct vsb *vsb;
 	const char *p, *q;
-	char *m;
 
 	vsb = VSB_new_auto();
 	AN(vsb);
@@ -279,19 +290,7 @@ macro_expand(struct vtclog *vl, const char *text)
 		assert(p[1] == '{');
 		assert(q[0] == '}');
 		p += 2;
-		m = macro_get(p, q);
-		if (m == NULL) {
-			if (!ign_unknown_macro) {
-				VSB_destroy(&vsb);
-				vtc_fatal(vl, "Macro ${%.*s} not found",
-					  (int)(q - p), p);
-				NEEDLESS(return (NULL));
-			}
-			VSB_printf(vsb, "${%.*s}", (int)(q - p), p);
-		} else {
-			VSB_printf(vsb, "%s", m);
-			free(m);
-		}
+		macro_cat(vl, vsb, p, q);
 		text = q + 1;
 	}
 	AZ(VSB_finish(vsb));
@@ -513,7 +512,6 @@ exec_file(const char *fn, const char *script, const char *tmpdir,
 	FILE *f;
 	struct vsb *vsb;
 	const char *p;
-	char *q;
 
 	(void)signal(SIGPIPE, SIG_IGN);
 
@@ -537,12 +535,8 @@ exec_file(const char *fn, const char *script, const char *tmpdir,
 
 	vsb = VSB_new_auto();
 	AN(vsb);
-	if (*fn != '/') {
-		q = macro_get("pwd", NULL);
-		AN(q);
-		VSB_cat(vsb, q);
-		free(q);
-	}
+	if (*fn != '/')
+		macro_cat(vltop, vsb, "pwd", NULL);
 	p = strrchr(fn, '/');
 	if (p != NULL) {
 		VSB_putc(vsb, '/');
