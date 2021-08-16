eb24a4a6e1992924dbfbd2756521b5c4044825f3
Poul-Henning Kamp <phk@FreeBSD.org>
2021-08-16 11:11:07 +0000

    Cover a more lines to a void a pointless red gcov entry

--- lib/libvarnishapi/vxp_test_coverage.sh
+++ lib/libvarnishapi/vxp_test_coverage.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+set -ex
+
 ./vxp_test -q '(*Error) or (BerespStatus >= 500)'
 ./vxp_test -q 'ReqHeader:user-agent ~ "iPod" and Timestamp:Resp[2] > 1'
+
+! ./vxp_test -h 
+
+! ./vxp_test -q 'The head and in frontal attack on an english writer' 
