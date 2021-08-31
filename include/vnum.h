3f6a25170488bb8bfca5f07a81e94cc1c5c35ade
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2021-08-31 11:57:38 +0200

    vnum: New VNUM_uint() and VNUM_hex() functions
    With the hex table suggested by fgs.

--- include/vnum.h
+++ include/vnum.h
@@ -37,6 +37,8 @@ vtim_dur VNUM_duration(const char *p);
 int64_t VNUM_bytes_unit(double r, const char *b, const char *e, uintmax_t rel,
     const char **errtxt);
 const char *VNUM_2bytes(const char *p, uintmax_t *r, uintmax_t rel);
+ssize_t VNUM_uint(const char *b, const char *e, const char **p);
+ssize_t VNUM_hex(const char *b, const char *e, const char **p);
 
 int64_t SF_Parse_Integer(const char **ipp, const char **errtxt);
 double SF_Parse_Decimal(const char **ipp, int strict, const char **errtxt);
