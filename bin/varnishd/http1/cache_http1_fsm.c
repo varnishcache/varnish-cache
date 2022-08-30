e3c39bccb64c4161fcf03bd80d0519e0a5aec9b9
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2022-08-30 16:24:34 +0200

    http1: Emit ReqStart on bad requests
    It makes no sense to emit this just before we end the transaction,
    except to help varnishncsa find the client IP address.

--- bin/varnishd/http1/cache_http1_fsm.c
+++ bin/varnishd/http1/cache_http1_fsm.c
@@ -44,6 +44,7 @@
 #include "cache/cache_transport.h"
 #include "cache_http1.h"
 
+#include "common/heritage.h"
 #include "vtcp.h"
 
 static const char H1NEWREQ[] = "HTTP1::NewReq";
@@ -245,6 +246,7 @@ http1_abort(struct req *req, uint16_t status)
 static int
 http1_dissect(struct worker *wrk, struct req *req)
 {
+	const char *ci, *cp, *endpname;
 
 	CHECK_OBJ_NOTNULL(wrk, WORKER_MAGIC);
 	CHECK_OBJ_NOTNULL(req, REQ_MAGIC);
@@ -271,6 +273,15 @@ http1_dissect(struct worker *wrk, struct req *req)
 		    (int)(req->htc->rxbuf_e - req->htc->rxbuf_b),
 		    req->htc->rxbuf_b);
 		wrk->stats->client_req_400++;
+
+		ci = SES_Get_String_Attr(req->sp, SA_CLIENT_IP);
+		cp = SES_Get_String_Attr(req->sp, SA_CLIENT_PORT);
+		CHECK_OBJ_NOTNULL(req->sp->listen_sock, LISTEN_SOCK_MAGIC);
+		endpname = req->sp->listen_sock->name;
+		AN(endpname);
+
+		VSLb(req->vsl, SLT_ReqStart, "%s %s %s", ci, cp, endpname);
+
 		req->doclose = SC_RX_JUNK;
 		http1_abort(req, 400);
 		return (-1);
