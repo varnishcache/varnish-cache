32af98d0ba935edb254117c48046f953a8c3a890
Poul-Henning Kamp <phk@FreeBSD.org>
2022-09-19 11:39:04 +0000

    Update 'process' docs

--- bin/varnishtest/vtc_process.c
+++ bin/varnishtest/vtc_process.c
@@ -908,10 +908,14 @@ process_close(struct process *p)
  * Output from the stderr-pipe is copied verbatim to ${pNAME_err}, and
  * is always included in the vtc_log.
  *
- *	process pNAME SPEC [-log] [-dump] [-hexdump] [-expect-exit N]
- *		[-start] [-run]
- *		[-write STRING] [-writeln STRING]
- *		[-kill STRING] [-stop] [-wait] [-close] [-screen-dump]
+ *	process pNAME SPEC [-allow-core] [-expect-exit N] [-expect-signal N]
+ *		[-dump] [-hexdump] [-log]
+ *		[-run] [-close] [-kill SIGNAL] [-start] [-stop] [-wait]
+ *		[-write STRING] [-writeln STRING] [-writehex HEXSTRING]
+ *		[-need-bytes [+]NUMBER]
+ *		[-screen-dump] [-winsz LINES COLUMNSS] [-ansi-response]
+ *		[-expect-cursor LINE COLUMN] [-expect-text LINE COLUMN TEXT]
+ *		[-match-text LINE COLUMN REGEXP]
  *
  * pNAME
  *	Name of the process. It must start with 'p'.
@@ -934,6 +938,12 @@ process_close(struct process *p)
  * \-expect-exit N
  *	Expect exit status N
  *
+ * \-expect-signal N
+ *	Expect signal in exit status N
+ *
+ * \-allow-core
+ *	Core dump in exit status is OK
+ *
  * \-wait
  *	Wait for the process to finish.
  *
@@ -954,7 +964,7 @@ process_close(struct process *p)
  *	expression from either output, consider using it if you only need
  *	to match one.
  *
- * \-kill STRING
+ * \-kill SIGNAL
  *	Send a signal to the process. The argument can be either
  *	the string "TERM", "INT", or "KILL" for SIGTERM, SIGINT or SIGKILL
  *	signals, respectively, or a hyphen (-) followed by the signal
@@ -970,7 +980,10 @@ process_close(struct process *p)
  * \-stop
  *	Shorthand for -kill TERM.
  *
- * \-winsz LIN COL
+ * \-close
+ *	Alias for "-kill HUP"
+ *
+ * \-winsz LINES COLUMNS
  *	Change the terminal window size to LIN lines and COL columns.
  *
  * \-write STRING
@@ -986,20 +999,24 @@ process_close(struct process *p)
  *	Wait until at least NUMBER bytes have been received in total.
  *	If '+' is prefixed, NUMBER new bytes must be received.
  *
- * \-expect-text LIN COL PAT
- *	Wait for PAT to appear at LIN,COL on the virtual screen.
+ * \-ansi-response
+ *	Respond to terminal respond-back sequences
+ *
+ * \-expect-cursor LINE COLUMN
+ *	Expect cursors location
+ *
+ * \-expect-text LINE COLUMNS TEXT
+ *	Wait for TEXT to appear at LIN,COL on the virtual screen.
  *	Lines and columns are numbered 1...N
  *	LIN==0 means "on any line"
  *	COL==0 means "anywhere on the line"
  *
- * \-match-text LIN COL PAT
+ * \-match-text LINE COLUMN REGEXP
  *	Wait for the PAT regular expression to match the text at LIN,COL on the virtual screen.
  *	Lines and columns are numbered 1...N
  *	LIN==0 means "on any line"
  *	COL==0 means "anywhere on the line"
  *
- * \-close
- *	Alias for "-kill HUP"
  *
  * \-screen-dump
  *	Dump the virtual screen into vtc_log
