8e2817645ea0683bdffb2c3b2f5965295e00bff5
Poul-Henning Kamp <phk@FreeBSD.org>
2022-10-25 07:59:52 +0000

    Use VXID() when printing vxids

--- bin/varnishd/cache/cache_req_fsm.c
+++ bin/varnishd/cache/cache_req_fsm.c
@@ -1113,7 +1113,7 @@ cnt_diag(struct req *req, const char *state)
 	CHECK_OBJ_NOTNULL(req, REQ_MAGIC);
 
 	VSLb(req->vsl,  SLT_Debug, "vxid %ju STP_%s sp %p vcl %p",
-	    req->vsl->wid.vxid, state, req->sp, req->vcl);	// XXX_VXID
+	    VXID(req->vsl->wid), state, req->sp, req->vcl);
 	VSL_Flush(req->vsl, 0);
 }
 
