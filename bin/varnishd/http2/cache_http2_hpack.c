eeeb8fad01926b188d8f9871f6d3b26107752f9b
Dag Haavi Finstad <daghf@varnish-software.com>
2023-04-03 10:26:51 +0200

    h2: Relax "no //" URLs requirement
    This requirement was dropped in the updated rfc 9113.
    
    Fixes: #3911

--- bin/varnishd/http2/cache_http2_hpack.c
+++ bin/varnishd/http2/cache_http2_hpack.c
@@ -135,10 +135,9 @@ h2h_addhdr(struct http *hp, char *b, size_t namelen, size_t len)
 			n = HTTP_HDR_URL;
 			disallow_empty = 1;
 
-			// rfc7540,l,3060,3071
-			if (((len > 0 && *b != '/') ||
-			    (len > 1 && *(b+1) == '/')) &&
-			    (strncmp(b, "*", len) != 0)) {
+			// rfc9113,l,2693,2705
+			if (len > 0 && *b != '/' &&
+			    strncmp(b, "*", len) != 0) {
 				VSLb(hp->vsl, SLT_BogoHeader,
 				    "Illegal :path pseudo-header %.*s",
 				    (int)len, b);
