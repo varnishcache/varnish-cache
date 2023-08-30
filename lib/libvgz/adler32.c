6fd423560ff3b26fd9dd86479d9908322ee25b62
Poul-Henning Kamp <phk@FreeBSD.org>
2023-08-30 09:42:50 +0000

    zlib 1.3 convergence:  Get rid of OF(...)

--- lib/libvgz/adler32.c
+++ lib/libvgz/adler32.c
@@ -8,7 +8,7 @@
 #include "zutil.h"
 
 #ifdef NOVGZ
-local uLong adler32_combine_ OF((uLong adler1, uLong adler2, z_off64_t len2));
+local uLong adler32_combine_ (uLong adler1, uLong adler2, z_off64_t len2);
 
 #define BASE 65521U     /* largest prime smaller than 65536 */
 #define NMAX 5552
