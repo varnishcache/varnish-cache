38f1febb537e40c017631384aae191ff3a5ee821
Poul-Henning Kamp <phk@FreeBSD.org>
2023-08-30 09:42:50 +0000

    zlib 1.3 convergence:  Get rid of OF(...)

--- lib/libvgz/inflate.c
+++ lib/libvgz/inflate.c
@@ -92,16 +92,16 @@
 #endif
 
 /* function prototypes */
-local int inflateStateCheck OF((z_streamp strm));
-local void fixedtables OF((struct inflate_state FAR *state));
-local int updatewindow OF((z_streamp strm, const unsigned char FAR *end,
-                           unsigned copy));
+local int inflateStateCheck (z_streamp strm);
+local void fixedtables (struct inflate_state FAR *state);
+local int updatewindow (z_streamp strm, const unsigned char FAR *end,
+                           unsigned copy);
 #ifdef BUILDFIXED
-   void makefixed OF((void));
+   void makefixed (void);
 #endif
 #ifdef NOVGZ
-local unsigned syncsearch OF((unsigned FAR *have, const unsigned char FAR *buf,
-                              unsigned len));
+local unsigned syncsearch (unsigned FAR *have, const unsigned char FAR *buf,
+                              unsigned len);
 #endif /* NOVGZ */
 
 local int inflateStateCheck(
