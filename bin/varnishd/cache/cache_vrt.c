8029033416577dea049f9d7837deffe8e07683ff
Walid Boudebouda <walid.boudebouda@gmail.com>
2023-10-14 20:07:44 +0200

    VCL: Introduce new obj_stale variable
    obj_stale variable gives access to the stale object we had in cache
    when doing a 304 revalidation. It is only readable from vcl_backend_refresh
    subroutine.

--- bin/varnishd/cache/cache_vrt.c
+++ bin/varnishd/cache/cache_vrt.c
@@ -259,6 +259,7 @@ VRT_selecthttp(VRT_CTX, enum gethdr_e where)
 	case HDR_RESP:
 		hp = ctx->http_resp;
 		break;
+	case HDR_OBJ_STALE:
 	case HDR_OBJ:
 		hp = NULL;
 		break;
@@ -281,6 +282,13 @@ VRT_GetHdr(VRT_CTX, VCL_HEADER hs)
 		CHECK_OBJ_NOTNULL(ctx->req, REQ_MAGIC);
 		CHECK_OBJ_NOTNULL(ctx->req->objcore, OBJCORE_MAGIC);
 		return (HTTP_GetHdrPack(ctx->req->wrk, ctx->req->objcore,
+		    hs->what));
+	}
+
+	if (hs->where == HDR_OBJ_STALE) {
+		CHECK_OBJ_NOTNULL(ctx->bo, BUSYOBJ_MAGIC);
+		CHECK_OBJ_NOTNULL(ctx->bo->stale_oc, OBJCORE_MAGIC);
+		return (HTTP_GetHdrPack(ctx->bo->wrk, ctx->bo->stale_oc,
 		    hs->what));
 	}
 	hp = VRT_selecthttp(ctx, hs->where);
