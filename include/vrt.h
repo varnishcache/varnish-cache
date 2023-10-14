b4b91fadedb0104c8bbc5c0397962f61b6f6ec44
Walid Boudebouda <walid.boudebouda@gmail.com>
2023-10-14 20:07:44 +0200

    VCL: Introduce new obj_stale variable
    obj_stale variable gives access to the stale object we had in cache
    when doing a 304 revalidation. It is only readable from vcl_backend_refresh
    subroutine.

--- include/vrt.h
+++ include/vrt.h
@@ -58,6 +58,19 @@
  * binary/load-time compatible, increment MAJOR version
  *
  * NEXT (2024-09-15)
+  * 	VRT_r_obj_stale_age() added
+ *	VRT_r_obj_stale_can_esi() added
+ *	VRT_r_obj_stale_grace() added
+ *	VRT_r_obj_stale_hits() added
+ *	VRT_r_obj_stale_keep() added
+ *	VRT_r_obj_stale_proto() added
+ *	VRT_r_obj_stale_reason() added
+ *	VRT_r_obj_stale_status() added
+ *	VRT_r_obj_stale_storage() added
+ *	VRT_r_obj_stale_time() added
+ *	VRT_r_obj_stale_ttl() added
+ *	VRT_r_obj_stale_uncacheable() added
+ *	enum gethdr_e has new value HDR_OBJ_STALE
  *	struct vrt_backend.backend_wait_timeout added
  *	struct vrt_backend.backend_wait_limit  added
  * 19.1 (2024-05-27)
@@ -675,7 +688,8 @@ enum gethdr_e {
 	HDR_RESP,
 	HDR_OBJ,
 	HDR_BEREQ,
-	HDR_BERESP
+	HDR_BERESP,
+	HDR_OBJ_STALE
 };
 
 struct gethdr_s {
