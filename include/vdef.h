c5f616ce207d01b7a5432042ae99f07af47c695e
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2024-03-27 15:19:01 +0100

    txt: New macros to work with strings

--- include/vdef.h
+++ include/vdef.h
@@ -263,6 +263,8 @@ typedef struct {
 
 #define Tcheck(t)	do { (void)pdiff((t).b, (t).e); } while (0)
 #define Tlen(t)		(pdiff((t).b, (t).e))
+#define Tstr(s)		((txt){(s), (s) + strlen(s)})
+#define Tstrcmp(t, s)	(strncmp((t).b, (s), Tlen(t)))
 
 /* #3020 dummy definitions until PR is merged*/
 #define LIKELY(x)	(x)
