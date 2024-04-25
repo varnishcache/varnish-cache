bec48849fba4146aa707d2f3d59089ba24944f21
Dag Haavi Finstad <daghf@varnish-software.com>
2024-04-25 14:36:57 +0200

    h2: Ensure highest_stream is consistent also on Upgrade: h2c
    Conflicts:
    	bin/varnishd/http2/cache_http2_session.c

--- bin/varnishd/http2/cache_http2_session.c
+++ bin/varnishd/http2/cache_http2_session.c
@@ -289,6 +289,8 @@ h2_ou_session(struct worker *wrk, struct h2_sess *h2,
 
 	/* Start req thread */
 	r2 = h2_new_req(wrk, h2, 1, req);
+	AZ(h2->highest_stream);
+	h2->highest_stream = r2->stream;
 	req->transport = &H2_transport;
 	req->req_step = R_STP_TRANSPORT;
 	req->task.func = h2_do_req;
