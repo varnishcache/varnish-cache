ae30fec5a9569fbbdbb82307494e4e140f1cfcbd
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2024-06-17 11:38:45 +0200

    http1: Rename HTTP1_Complete() to HTTP1_Headers()

--- bin/varnishd/http1/cache_http1_fetch.c
+++ bin/varnishd/http1/cache_http1_fetch.c
@@ -197,7 +197,7 @@ V1F_FetchRespHdr(struct busyobj *bo)
 	CHECK_OBJ_NOTNULL(bo->htc, HTTP_CONN_MAGIC);
 
 	t = VTIM_real() + htc->first_byte_timeout;
-	hs = HTC_RxStuff(htc, HTTP1_Complete, NULL, NULL,
+	hs = HTC_RxStuff(htc, HTTP1_Headers, NULL, NULL,
 	    t, NAN, htc->between_bytes_timeout, cache_param->http_resp_size);
 	if (hs != HTC_S_COMPLETE) {
 		bo->acct.beresp_hdrbytes +=
