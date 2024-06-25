6dbb1f2c45530600c57db0e796b4f40248c19c41
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2024-06-25 07:51:01 +0200

    SQUASHME: further simplify emulated WS_Pipeline()

--- bin/varnishd/cache/cache_ws_emu.c
+++ bin/varnishd/cache/cache_ws_emu.c
@@ -224,51 +224,43 @@ WS_Reset(struct ws *ws, uintptr_t pp)
 int
 WS_Pipeline(struct ws *ws, const void *b, const void *e, unsigned rollback)
 {
-	struct ws_emu *we;
-	struct ws_alloc *wa;
-	unsigned l;
+	void *tmp;
+	unsigned r, l;
 
 	WS_Assert(ws);
 	AZ(ws->f);
 	AZ(ws->r);
 
-	/* Nothing to pipeline, this can be implemented as a simple
-	 * reservation.
+	/* NB: the pipeline cannot be moved if it comes from the same
+	 * workspace because a rollback would free the memory. This is
+	 * emulated with two copies instead.
 	 */
-	if (b == NULL) {
+
+	if (b != NULL) {
+		AN(e);
+		l = pdiff(b, e);
+		tmp = malloc(l);
+		AN(tmp);
+		memcpy(tmp, b, l);
+	} else {
 		AZ(e);
-		if (rollback)
-			WS_Rollback(ws, 0);
-		(void)WS_ReserveAll(ws);
-		return (0);
+		l = 0;
+		tmp = NULL;
 	}
-
-	/* In order to emulate a memmove() from the same workspace without
-	 * triggering a use-after-free the sequence of events must start with
-	 * a detached allocation into which the pipeline is copied.
-	 * The allocation is then manually turned into a reservation after
-	 * potentially unwinding (freeing) past allocations.
-	 */
-	we = ws_emu(ws);
-	ALLOC_OBJ(wa, WS_ALLOC_MAGIC);
-	AN(wa);
-	wa->len = we->len;
-	wa->ptr = malloc(wa->len);
-	AN(wa->ptr);
-
-	AN(e);
-	l = pdiff(b, e);
-	if (l <= wa->len)
-		memcpy(wa->ptr, b, l);
 
 	if (rollback)
 		WS_Rollback(ws, 0);
-	ws->f = wa->ptr;
-	ws->r = ws->f + wa->len;
-	VTAILQ_INSERT_TAIL(&we->head, wa, list);
-	WS_Assert(ws);
-	if (l > wa->len)
+
+	r = WS_ReserveAll(ws);
+
+	if (l > r) {
+		free(tmp);
 		return (-1);
+	}
+
+	if (l > 0)
+		memcpy(ws->f, tmp, l);
+	free(tmp);
 	return (l);
 }
 
