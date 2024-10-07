5cb857197d471b4ccb175978b269441e25316dfc
Nils Goroll <nils.goroll@uplex.de>
2024-10-07 17:51:19 +0200

    vmod_debug: split filter sources from rest of the code
    and flexelint

--- vmod/flint.sh
+++ vmod/flint.sh
@@ -10,6 +10,6 @@ for vmod in vmod_*.vcc ; do
     echo "${vmod}"
     echo "====================="
     vmod="${vmod#vmod_}"
-    FLOPS="-I../bin/varnishd vcc_${vmod}_if.c vmod_${vmod}*.c" \
+    FLOPS="-I../bin/varnishd -I../lib/libvgz vcc_${vmod}_if.c vmod_${vmod}*.c" \
 	 ../tools/flint_skel.sh
 done
