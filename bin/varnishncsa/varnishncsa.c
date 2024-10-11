e1fff877d2bb76e1eb474a1e4a404b87146e9777
Walid Boudebouda <walid.boudebouda@gmail.com>
2024-10-11 12:22:08 +0200

    varnishncsa: Replace "force" flag with an update mode enum

--- bin/varnishncsa/varnishncsa.c
+++ bin/varnishncsa/varnishncsa.c
@@ -162,6 +162,11 @@ static struct ctx {
 	int64_t			vxid;
 } CTX;
 
+enum update_mode {
+	UM_IF_NOT_SET,
+	UM_ALWAYS
+};
+
 static void parse_format(const char *format);
 
 static void
@@ -850,7 +855,7 @@ isprefix(const char *prefix, size_t len, const char *b,
 }
 
 static void
-frag_fields(int force, const char *b, const char *e, ...)
+frag_fields(enum update_mode um, const char *b, const char *e, ...)
 {
 	va_list ap;
 	const char *p, *q;
@@ -882,7 +887,7 @@ frag_fields(int force, const char *b, const char *e, ...)
 		assert(p != NULL && q != NULL);
 		if (p >= e || q <= p)
 			continue;
-		if (frag->gen != CTX.gen || force) {
+		if (frag->gen != CTX.gen || um == UM_ALWAYS) {
 			/* We only grab the same matching field once */
 			frag->gen = CTX.gen;
 			frag->b = p;
@@ -893,10 +898,9 @@ frag_fields(int force, const char *b, const char *e, ...)
 }
 
 static void
-frag_line(int force, const char *b, const char *e, struct fragment *f)
+frag_line(enum update_mode um, const char *b, const char *e, struct fragment *f)
 {
-
-	if (f->gen == CTX.gen && !force)
+	if (f->gen == CTX.gen && um == UM_IF_NOT_SET)
 		/* We only grab the same matching record once */
 		return;
 
@@ -923,7 +927,7 @@ process_hdr(const struct watch_head *head, const char *b, const char *e)
 		CHECK_OBJ_NOTNULL(w, WATCH_MAGIC);
 		if (!isprefix(w->key, w->keylen, b, e, &p))
 			continue;
-		frag_line(1, p, e, &w->frag);
+		frag_line(UM_ALWAYS, p, e, &w->frag);
 	}
 }
 
@@ -942,9 +946,9 @@ process_vsl(const struct vsl_watch_head *head, enum VSL_tag_e tag,
 		    !isprefix(w->prefix, w->prefixlen, b, e, &p))
 			continue;
 		if (w->idx == 0)
-			frag_line(0, p, e, &w->frag);
+			frag_line(UM_IF_NOT_SET, p, e, &w->frag);
 		else
-			frag_fields(0, p, e, w->idx, &w->frag, 0, NULL);
+			frag_fields(UM_IF_NOT_SET, p, e, w->idx, &w->frag, 0, NULL);
 	}
 }
 
@@ -993,44 +997,44 @@ dispatch_f(struct VSL_data *vsl, struct VSL_transaction * const pt[],
 				skip = 1;
 				break;
 			case SLT_PipeAcct:
-				frag_fields(0, b, e,
+				frag_fields(UM_IF_NOT_SET, b, e,
 				    3, &CTX.frag[F_I],
 				    4, &CTX.frag[F_O],
 				    0, NULL);
 				break;
 			case SLT_BackendOpen:
-				frag_fields(1, b, e,
+				frag_fields(UM_ALWAYS, b, e,
 				    3, &CTX.frag[F_h],
 				    0, NULL);
 				break;
 			case SLT_ReqStart:
-				frag_fields(0, b, e,
+				frag_fields(UM_IF_NOT_SET, b, e,
 				    1, &CTX.frag[F_h],
 				    0, NULL);
 				break;
 			case SLT_BereqMethod:
 			case SLT_ReqMethod:
-				frag_line(0, b, e, &CTX.frag[F_m]);
+				frag_line(UM_IF_NOT_SET, b, e, &CTX.frag[F_m]);
 				break;
 			case SLT_BereqURL:
 			case SLT_ReqURL:
 				p = memchr(b, '?', e - b);
 				if (p == NULL)
 					p = e;
-				frag_line(0, b, p, &CTX.frag[F_U]);
-				frag_line(0, p, e, &CTX.frag[F_q]);
+				frag_line(UM_IF_NOT_SET, b, p, &CTX.frag[F_U]);
+				frag_line(UM_IF_NOT_SET, p, e, &CTX.frag[F_q]);
 				break;
 			case SLT_BereqProtocol:
 			case SLT_ReqProtocol:
-				frag_line(0, b, e, &CTX.frag[F_H]);
+				frag_line(UM_IF_NOT_SET, b, e, &CTX.frag[F_H]);
 				break;
 			case SLT_BerespStatus:
 			case SLT_RespStatus:
-				frag_line(1, b, e, &CTX.frag[F_s]);
+				frag_line(UM_ALWAYS, b, e, &CTX.frag[F_s]);
 				break;
 			case SLT_BereqAcct:
 			case SLT_ReqAcct:
-				frag_fields(0, b, e,
+				frag_fields(UM_IF_NOT_SET, b, e,
 				    3, &CTX.frag[F_I],
 				    5, &CTX.frag[F_b],
 				    6, &CTX.frag[F_O],
@@ -1039,19 +1043,19 @@ dispatch_f(struct VSL_data *vsl, struct VSL_transaction * const pt[],
 			case SLT_Timestamp:
 #define ISPREFIX(a, b, c, d)	isprefix(a, strlen(a), b, c, d)
 				if (ISPREFIX("Start:", b, e, &p)) {
-					frag_fields(0, p, e, 1,
+					frag_fields(UM_IF_NOT_SET, p, e, 1,
 					    &CTX.frag[F_tstart], 0, NULL);
 
 				} else if (ISPREFIX("Resp:", b, e, &p) ||
 				    ISPREFIX("PipeSess:", b, e, &p) ||
 				    ISPREFIX("BerespBody:", b, e, &p)) {
-					frag_fields(0, p, e, 1,
+					frag_fields(UM_IF_NOT_SET, p, e, 1,
 					    &CTX.frag[F_tend], 0, NULL);
 
 				} else if (ISPREFIX("Process:", b, e, &p) ||
 				    ISPREFIX("Pipe:", b, e, &p) ||
 				    ISPREFIX("Beresp:", b, e, &p)) {
-					frag_fields(0, p, e, 2,
+					frag_fields(UM_IF_NOT_SET, p, e, 2,
 					    &CTX.frag[F_ttfb], 0, NULL);
 				}
 				break;
@@ -1060,10 +1064,10 @@ dispatch_f(struct VSL_data *vsl, struct VSL_transaction * const pt[],
 				process_hdr(&CTX.watch_reqhdr, b, e);
 				if (ISPREFIX("Authorization:", b, e, &p) &&
 				    ISPREFIX("basic ", p, e, &p))
-					frag_line(0, p, e,
+					frag_line(UM_IF_NOT_SET, p, e,
 					    &CTX.frag[F_auth]);
 				else if (ISPREFIX("Host:", b, e, &p))
-					frag_line(0, p, e,
+					frag_line(UM_IF_NOT_SET, p, e,
 					    &CTX.frag[F_host]);
 #undef ISPREFIX
 				break;
@@ -1106,7 +1110,7 @@ dispatch_f(struct VSL_data *vsl, struct VSL_transaction * const pt[],
 					    strncmp(b, w->key, w->keylen))
 						continue;
 					p = b + w->keylen;
-					frag_line(0, p, e, &w->frag);
+					frag_line(UM_IF_NOT_SET, p, e, &w->frag);
 				}
 				break;
 			default:
