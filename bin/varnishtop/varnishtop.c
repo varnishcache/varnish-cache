varnishcache/varnish-cache

commit 7af00146975db17247b7602b0dfce3ccf8090245

vcurses: move AC() and IC() macros to vcurses.h
AC() asserts that curses commands do not fail
IC() ignores return values (just (void)x)

bin/varnishtop/varnishtop.c
@@ -58,12 +58,6 @@
 #include "vut.h"
 #include "vapi/vsig.h"
 
-#if 0
-#define AC(x) assert((x) != ERR)
-#else
-#define AC(x) x
-#endif
-
 static struct VUT *vut;
 
 struct top {

bin/varnishhist/varnishhist.c
@@ -56,14 +56,6 @@
 #include "vut.h"
 #include "vtim.h"
 
-#if 1
-#define AC(x) assert((x) != ERR)
-#define IC(x) (void)x
-#else
-#define AC(x) (void)x
-#define IC(x) (void)x
-#endif
-
 #define HIST_N 2000		/* how far back we remember */
 #define HIST_RES 100		/* bucket resolution */
 

include/vcurses.h
@@ -43,3 +43,11 @@
 #else
 #  error "SysV or X/Open-compatible Curses header file required"
 #endif
+
+#if 1
+#define AC(x) assert((x) != ERR)
+#define IC(x) (void)x
+#else
+#define AC(x) (void)x
+#define IC(x) (void)x
+#endif

commit a39c931f15c8052b0ea7a54f99bd5ea2f08f46a5

vcurses: defuse assertions on *printw()
7af00146975db17247b7602b0dfce3ccf8090245 accidentally enabled previous AC()
checks for varnishtop only.

*printw() is buggy, see #4266

bin/varnishtop/varnishtop.c
@@ -210,16 +210,16 @@ update(unsigned p)
 	q = ident;
 	len = COLS - strlen(q);
 	if (end_of_file)
-		AC(mvprintw(0, len - (1 + 6), "%s (EOF)", q));
+		IC(mvprintw(0, len - (1 + 6), "%s (EOF)", q));
 	else
-		AC(mvprintw(0, len - 1, "%s", q));
-	AC(mvprintw(0, 0, "list length %u", ntop));
+		IC(mvprintw(0, len - 1, "%s", q));
+	IC(mvprintw(0, 0, "list length %u", ntop));
 	for (tp = VRBT_MIN(t_order, &h_order); tp != NULL; tp = tp2) {
 		tp2 = VRBT_NEXT(t_order, &h_order, tp);
 
 		if (++l < LINES) {
 			len = vmin(tp->clen, COLS - 20);
-			AC(mvprintw(l, 0, "%9.2f %-*.*s %*.*s\n",
+			IC(mvprintw(l, 0, "%9.2f %-*.*s %*.*s\n",
 				tp->count, maxfieldlen, maxfieldlen,
 				VSL_tags[tp->tag],
 				len, len, tp->rec_data));