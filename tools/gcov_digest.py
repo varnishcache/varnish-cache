552f5c1ff8453ac45e5aca2775ca0ff728f0fd29
Poul-Henning Kamp <phk@FreeBSD.org>
2025-06-16 13:58:33 +0000

    Renovate the gcov_digest script.

--- tools/gcov_digest.py
+++ tools/gcov_digest.py
@@ -55,102 +55,50 @@ import sys
 import getopt
 import subprocess
 
-counts = {}
-lengths = {}
+source_files = {}
 
-exclude = [".git", ".deps",]
+class SourceFile():
+    ''' GCOV counts for a single source file '''
 
-def process_gcov(fn, sn):
-    """ Sum .gcov file into counts, then delete it """
-    dd = counts.get(sn)
-    if dd is None:
-        dd = {}
-    for ln in open(fn, encoding="UTF-8"):
-        d = ln.split(":")
-        cnt = d[0].strip()
-        ll = d[1]
-        if cnt == "-":
-            continue
-        if cnt == "#####":
-            cnt = 0
-        else:
-            cnt = int(cnt)
-        lno = int(d[1])
-        if lno not in dd:
-            dd[lno] = 0
-        dd[lno] += cnt
-    counts[sn] = dd
-    pl = lengths.get(sn)
-    ll = ll.strip()
-    if d[2] == "/*EOF*/\n":
-        ll = pl
-    elif pl != ll and not pl is None:
-        print("CONFLICT", fn, ll, pl)
-        ll = "-1"
-    lengths[sn] = ll
-    os.remove(fn)
+    def __init__(self, path):
+        self.path = path
+        self.counts = {}
+        self.length = 0
+        source_files[path] = self
 
-def run_gcov(prog, subdir):
-    """ Walk tree, run gcov, process output """
-    for root, dirs, files in os.walk(subdir):
-        for i in exclude:
-            if i in dirs:
-                dirs.remove(i)
-        if " ".join(files).find(".gcda") == -1:
-            continue
-        for fn in files:
-            if fn[-2:] != ".o":
+    def __lt__(self, other):
+        return self.path < other.path
+
+    def ingest(self, fd):
+        ''' Ingest a gcov file '''
+        for line in fd:
+            flds = line.split(':', maxsplit=2)
+            cnt = flds[0].strip()
+            if cnt == "-":
                 continue
-
-            # if we find the .o file in a .../.libs the sources
-            # must be found relative to the parent directory
-
-            if "varnishd" in root:
-                subdir = root.split("/")[-1]
-                cmd = ["cd " + root + "/.. && " + "exec " + prog + " " + subdir + "/" + fn]
-                rpath = "/../"
-            elif root[-6:] == "/.libs":
-                cmd = ["cd " + root + "/.. && " + "exec " + prog + " .libs/" + fn]
-                rpath = "/../"
+            if cnt == "#####":
+                cnt = 0
             else:
-                cmd = ["cd " + root + " && " + "exec " + prog + " " + fn]
-                rpath = "/"
+                cnt = int(cnt)
+            lno = int(flds[1])
+            if lno not in self.counts:
+                self.counts[lno] = cnt
+            else:
+                self.counts[lno] += cnt
+        self.length = lno
 
-            x = subprocess.check_output(
-                    cmd,
-                    stderr=subprocess.STDOUT, shell=True,
-                    universal_newlines=True)
-            pf = ""
+    def summary(self):
+        """
+        Produce compact output file
 
-            for ln in x.split("\n"):
-                if "such file" in ln:
-                    print("LN", ln)
-                    assert "such file" not in ln
-                ln = ln.split()
-                if not ln:
-                    continue
-                if ln[0].find("reating") != -1:
-                    gn = root + rpath + ln[1].strip("'")
-                    gn = os.path.normpath(gn)
-                    assert gn[-5:] == ".gcov"
-                    sn = gn[:-5]
-                    process_gcov(gn, sn)
+        Format:
+            linefm [lineto] count
 
-def produce_output(fdo):
-    """
-    Produce compact output file
-
-    Format:
-        linefm [lineto] count
-
-        "+" in linefm means "previous line + 1"
-        "." in count means "same as previous count"
-    """
-
-    for sn, cnt in counts.items():
-        fdo.write("/" + sn + " " + str(lengths[sn]) + "\n")
-        lnos = list(cnt.items())
-        lnos.sort()
+            "+" in linefm means "previous line + 1"
+            "." in count means "same as previous count"
+        """
+        yield "/" + self.path + " " + str(self.length)
+        lnos = list(sorted(self.counts.items()))
         pln = -1
         pcn = -1
         while lnos:
@@ -178,10 +126,84 @@ def produce_output(fdo):
             else:
                 s += "%d" % cn
                 pcn = cn
-            fdo.write(s + "\n")
+            yield s
 
-if __name__ == "__main__":
+exclude = [".git", ".deps",]
 
+def process_gcov(fn, root):
+    """ Sum .gcov file into counts, then delete it """
+
+    with open(fn, encoding="UTF-8") as gfile:
+        sline = gfile.readline().split(':')
+        assert sline[2] == "Source"
+        sname = os.path.normpath(os.path.join(root, sline[3].strip()))
+        sfile = source_files.get(sname)
+        if not sfile:
+            sfile = SourceFile(sname)
+        sfile.ingest(gfile)
+    os.remove(fn)
+
+def run_gcov(prog, subdir):
+    """ Walk tree, run gcov, process output """
+    for root, dirs, files in os.walk(subdir):
+        for i in exclude:
+            if i in dirs:
+                dirs.remove(i)
+        if " ".join(files).find(".gcda") == -1:
+            continue
+        for fn in files:
+            if fn[-2:] != ".o":
+                continue
+
+            # if we find the .o file in a .../.libs the sources
+            # must be found relative to the parent directory
+
+            if "varnishd" in root:
+                subdir = root.split("/")[-1]
+                cmd = ["cd " + root + "/.. && " + "exec " + prog + " " + subdir + "/" + fn]
+                rpath = "/../"
+            elif "vtest2" in root:
+                cmd = ["cd " + root + "/../.. && " + "exec " + prog + " vtest2/src/" + fn]
+                rpath = "/../../"
+                #print("VT2")
+            elif root[-6:] == "/.libs":
+                cmd = ["cd " + root + "/.. && " + "exec " + prog + " .libs/" + fn]
+                rpath = "/../"
+            else:
+                #print("R", root, "FN", fn)
+                cmd = ["cd " + root + " && " + "exec " + prog + " " + fn]
+                rpath = "/"
+
+            sp = subprocess.run(
+                    cmd,
+                    shell=True,
+                    universal_newlines=True,
+                    capture_output=True,
+            )
+
+            if sp.returncode != 0 or sp.stderr:
+                print("CMD", cmd, "EXIT", sp.returncode)
+                print("SERR", len(sp.stderr), sp.stderr)
+                print("SOUT", sp.stdout)
+                sys.exit(2)
+            x = sp.stdout
+
+            for ln in x.split("\n"):
+                if "such file" in ln:
+                    print("LN", ln)
+                    assert "such file" not in ln
+                ln = ln.split()
+                if not ln:
+                    continue
+                if "reating" in ln[0]:
+                    gn = root + rpath + ln[1].strip("'")
+                    gn = os.path.normpath(gn)
+                    assert gn[-5:] == ".gcov"
+                    #print("GS", gn)
+                    process_gcov(gn, root)
+
+def main():
+    ''' ... '''
     optlist, args = getopt.getopt(sys.argv[1:], "g:o:x:")
 
     fo = sys.stdout
@@ -193,7 +215,7 @@ if __name__ == "__main__":
         if f == '-o' and v == '-':
             fo = sys.stdout
         elif f == '-o':
-            fo = open(v, "w")
+            fo = open(v, "w", encoding="utf8")
         elif f == '-g':
             gcovprog = v
         elif f == '-x':
@@ -205,4 +227,9 @@ if __name__ == "__main__":
     for dn in args:
         run_gcov(gcovprog, dn)
 
-    produce_output(fo)
+    for sf in sorted(source_files.values()):
+        for i in sf.summary():
+            fo.write(i + "\n")
+
+if __name__ == "__main__":
+    main()
