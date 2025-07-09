233f5ab0094bdeae58a1bd941494aad598811d42
Nils Goroll <nils.goroll@uplex.de>
2025-07-09 18:55:29 +0200

    Revert "vmod_vtc: Insignificant polish"
    d-oh, I undid a fix which I applied and explained myself before in
    1157dfdc61211b95375a3ed10f076f2d7875439c
    
    This reverts commit d9d38175a7c3b9e8ab3568e37d4751d37df0814f.

--- vmod/vmod_vtc.c
+++ vmod/vmod_vtc.c
@@ -477,10 +477,10 @@ vsl_line(VRT_CTX, char *str)
 		return;
 
 	str = strtok_r(NULL, "\r\n", &save);
+	// needs to be assigned here because of the compound literal lifetime
+	s = TOSTRAND(str);
 	if (str == NULL)
 		s = vrt_null_strands;
-	else
-		s = TOSTRAND(str);
 
 	vmod_vsl(ctx, id, tag, side, s);
 }
