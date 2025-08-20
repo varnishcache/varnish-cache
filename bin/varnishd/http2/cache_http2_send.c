617d138eb30d0cb866547254b903702dabfe7ca9
Martin Blix Grydeland <martin@varnish-software.com>
2025-08-20 13:21:34 +0200

    H2: Add sanity assertions to h2_send_get_locked()
    These assertions guard against double-registering on the send queue.

--- bin/varnishd/http2/cache_http2_send.c
+++ bin/varnishd/http2/cache_http2_send.c
@@ -105,6 +105,8 @@ h2_send_get_locked(struct worker *wrk, struct h2_sess *h2, struct h2_req *r2)
 	Lck_AssertHeld(&h2->sess->mtx);
 	if (&wrk->cond == h2->cond)
 		ASSERT_RXTHR(h2);
+	AZ(H2_SEND_HELD(h2, r2));
+	AZ(r2->wrk);
 	r2->wrk = wrk;
 	VTAILQ_INSERT_TAIL(&h2->txqueue, r2, tx_list);
 	while (!H2_SEND_HELD(h2, r2))
